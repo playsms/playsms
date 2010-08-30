7f502e94f10660961548e90dcf0840bf5ffac75d
antonraharja <antonraharja@a2ba5a7b-9259-4a92-b3f4-97befcce6881>
2010-08-30 09:28:14 +0000

    forgot to add fn_rate.php
    git-svn-id: http://svn.sourceforge.telkomspeedy.com/svn/playsms/trunk@25 a2ba5a7b-9259-4a92-b3f4-97befcce6881

---
+++ web/lib/fn_rate.php
@@ -0,0 +1,114 @@
+<?php
+function dst2rate($dst)
+{
+    if ($dst)
+    {
+	$db_query = "SELECT rate FROM "._DB_PREF_."_tblRate WHERE dst='$dst'";
+	$db_result = dba_query($db_query);
+	$db_row = dba_fetch_array($db_result);
+	$rate = $db_row[rate];
+    }
+    return $rate;
+}
+
+function prefix2rate($prefix)
+{
+    if ($prefix)
+    {
+	$db_query = "SELECT rate FROM "._DB_PREF_."_tblRate WHERE prefix='$prefix'";
+	$db_result = dba_query($db_query);
+	$db_row = dba_fetch_array($db_result);
+	$rate = $db_row[rate];
+    }
+    return $rate;
+}
+
+function rateid2dst($id)
+{
+    if ($id)
+    {
+	$db_query = "SELECT dst FROM "._DB_PREF_."_tblRate WHERE id='$id'";
+	$db_result = dba_query($db_query);
+	$db_row = dba_fetch_array($db_result);
+	$dst = $db_row[dst];
+    }
+    return $dst;
+}
+
+function rateid2prefix($id)
+{
+    if ($id)
+    {
+	$db_query = "SELECT prefix FROM "._DB_PREF_."_tblRate WHERE id='$id'";
+	$db_result = dba_query($db_query);
+	$db_row = dba_fetch_array($db_result);
+	$prefix = $db_row[prefix];
+    }
+    return $prefix;
+}
+
+function rateid2rate($id)
+{
+    if ($id)
+    {
+	$db_query = "SELECT rate FROM "._DB_PREF_."_tblRate WHERE id='$id'";
+	$db_result = dba_query($db_query);
+	$db_row = dba_fetch_array($db_result);
+	$rate = $db_row[rate];
+    }
+    return $rate;
+}
+
+function setsmscredit($smslog_id) {
+    $db_query = "SELECT * FROM "._DB_PREF_."_tblSMSOutgoing WHERE smslog_id='$smslog_id'";
+    $db_result = dba_query($db_query);
+    $db_row = dba_fetch_array($db_result);
+    $p_dst = $db_row['p_dst'];
+    $p_msg = $db_row['p_msg'];
+    $uid = $db_row['uid'];
+    $count = ceil(strlen($p_msg) / 140);
+    $rate = getsmsrate($p_dst);
+    $username = uid2username($uid);
+    $credit = username2credit($username);
+    $remaining = $credit - ($rate*$count);
+    setusersmscredit($uid, $remaining);
+    return;
+}
+
+function setusersmscredit($uid, $remaining) {
+    $db_query = "UPDATE "._DB_PREF_."_tblUser SET c_timestamp=NOW(),credit='$remaining' WHERE uid='$uid'";
+    $db_result = @dba_affected_rows($db_query);
+}
+
+function getsmsrate($p_dst) {
+    global $default_rate;
+    $rate = $default_rate;
+    $prefix = $p_dst;
+    $m = ( strlen($prefix) > 10 ? 10 : strlen($prefix) );
+    for ($i=$m+1;$i>0;$i--) {
+	$prefix = substr($prefix, 0, $i);
+	$db_query = "SELECT rate FROM "._DB_PREF_."_tblRate WHERE prefix='$prefix'";
+	$db_result = dba_query($db_query);
+	if ($db_row = dba_fetch_array($db_result)) {
+	    $rate = $db_row['rate'];
+	    break;
+	}
+    }
+    return $rate;
+}
+
+function getmaxsmsrate() {
+    global $default_rate;
+    $rate = 0;
+    $db_query = "SELECT rate FROM "._DB_PREF_."_tblRate ORDER BY rate DESC LIMIT 1";
+    $db_result = dba_query($db_query);
+    if ($db_row = dba_fetch_array($db_result)) {
+	$rate = $db_row['rate'];
+    }
+    if ($default_rate > $rate) {
+	$rate = $default_rate;
+    }
+    return $rate;
+}
+
+?>
