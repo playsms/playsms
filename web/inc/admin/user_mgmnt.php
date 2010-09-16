e0b5af54920da8dfbf2440e248e41463eb374720
antonraharja <antonraharja@a2ba5a7b-9259-4a92-b3f4-97befcce6881>
2010-09-16 03:41:54 +0000

    user_mgmnt.php translated (emmanuel)
    git-svn-id: http://svn.sourceforge.telkomspeedy.com/svn/playsms/trunk@77 a2ba5a7b-9259-4a92-b3f4-97befcce6881

--- web/inc/admin/user_mgmnt.php
+++ web/inc/admin/user_mgmnt.php
@@ -8,22 +8,22 @@ switch ($op)
 	$db_result = dba_query($db_query);
 	if ($err)
 	{
-	    $content = "<p><font color=red>$err</font><p>";
+	    $content = "<p><font color='red'>$err</font><p>";
 	}
 	$content .= "
 	    <h2>Manage user</h2>
 	    <p>
-	    <input type=button value=\"Add user\" onClick=\"javascript:linkto('menu.php?inc=user_mgmnt&op=user_add')\" class=\"button\" />
-	    <p>Status: <b>Administrator</b><br>
-    <table cellpadding=1 cellspacing=2 border=0 width=100%>
+	    <input type='button' value=\""._('Add user')."\" onClick=\"javascript:linkto('menu.php?inc=user_mgmnt&op=user_add')\" class=\"button\" />
+	    <p>"._('Status').": <b>"._('Administrator')."</b><br>
+    <table cellpadding='1' cellspacing='2' border='0' width='100%'>
     <tr>
-        <td class=box_title width=25>*</td>
-        <td class=box_title width=150>Username</td>
-        <td class=box_title>Name</td>	
-        <td class=box_title width=200>Email</td>
-        <td class=box_title width=200>Mobile</td>
-        <td class=box_title width=100>Credit</td>
-        <td class=box_title width=75>Action</td>
+        <td class='box_title' width='25'>*</td>
+        <td class='box_title' width='150'>"._('Username')."</td>
+        <td class='box_title'>"._('Name')."</td>	
+        <td class='box_title' width='200'>"._('Email')."</td>
+        <td class='box_title' width='200'>"._('Mobile')."</td>
+        <td class='box_title' width='100'>"._('Credit')."</td>
+        <td class='box_title' width='75'>"._('Action')."</td>
     </tr>		    
 	";
 	$i=0;
@@ -32,16 +32,16 @@ switch ($op)
 	    $i++;
             $td_class = ($i % 2) ? "box_text_odd" : "box_text_even";
 	    $action = "<a href=menu.php?inc=user_mgmnt&op=user_edit&uname=".$db_row['username'].">$icon_edit</a>";
-	    $action .= "<a href=\"javascript: ConfirmURL('Are you sure you want to delete user `".$db_row['username']."` ?','menu.php?inc=user_mgmnt&op=user_del&uname=".$db_row['username']."')\">$icon_delete</a>";
+	    $action .= "<a href=\"javascript: ConfirmURL("._('Are you sure you want to delete user')." `".$db_row['username']."` ?','menu.php?inc=user_mgmnt&op=user_del&uname=".$db_row['username']."')\">$icon_delete</a>";
 	    $content .= "
     <tr>
-	<td class=$td_class>&nbsp;$i.</td>
-	<td class=$td_class>".$db_row['username']."</td>
-	<td class=$td_class>".$db_row['name']."</td>
-	<td class=$td_class>".$db_row['email']."</td>	
-	<td class=$td_class>".$db_row['mobile']."</td>	
-	<td class=$td_class>".$db_row['credit']."</td>	
-	<td class=$td_class align=center>$action</td>
+	<td class='$td_class'>&nbsp;$i.</td>
+	<td class='$td_class'>".$db_row['username']."</td>
+	<td class='$td_class'>".$db_row['name']."</td>
+	<td class='$td_class'>".$db_row['email']."</td>	
+	<td class='$td_class'>".$db_row['mobile']."</td>	
+	<td class='$td_class'>".$db_row['credit']."</td>	
+	<td class='$td_class' align='center'>$action</td>
     </tr>
     ";
 	}
@@ -49,15 +49,15 @@ switch ($op)
 	$db_query = "SELECT * FROM "._DB_PREF_."_tblUser WHERE status='3' ORDER BY username";
 	$db_result = dba_query($db_query);
 	$content .= "<p>Status: <b>Normal User</b><br>
-    <table cellpadding=1 cellspacing=2 border=0 width=100%>
+    <table cellpadding='1' cellspacing='2' border='0' width='100%'>
     <tr>
-        <td class=box_title width=25>*</td>
-        <td class=box_title width=150>Username</td>
-        <td class=box_title>Name</td>	
-        <td class=box_title width=200>Email</td>
-        <td class=box_title width=200>Mobile</td>
-        <td class=box_title width=100>Credit</td>
-        <td class=box_title width=75>Action</td>
+        <td class='box_title' width='25'>*</td>
+        <td class='box_title' width='150'>"._('Username')."</td>
+        <td class='box_title'>"._('Name')."</td>	
+        <td class='box_title' width='200'>"._('Email')."</td>
+        <td class='box_title' width='200'>"._('Mobile')."</td>
+        <td class='box_title' width='100'>"._('Credit')."</td>
+        <td class='box_title' width='75'>"._('Action')."</td>
     </tr>		    
 	";
 	$i=0;	
@@ -66,16 +66,16 @@ switch ($op)
 	    $i++;
             $td_class = ($i % 2) ? "box_text_odd" : "box_text_even";
 	    $action = "<a href=menu.php?inc=user_mgmnt&op=user_edit&uname=".$db_row['username'].">$icon_edit</a>";
-	    $action .= "<a href=\"javascript: ConfirmURL('Are you sure you want to delete user `".$db_row['username']."` ?','menu.php?inc=user_mgmnt&op=user_del&uname=".$db_row['username']."')\">$icon_delete</a>";
+	    $action .= "<a href=\"javascript: ConfirmURL('"._('Are you sure you want to delete user')." `".$db_row['username']."` ?','menu.php?inc=user_mgmnt&op=user_del&uname=".$db_row['username']."')\">$icon_delete</a>";
 	    $content .= "
     <tr>
-	<td class=$td_class>&nbsp;$i.</td>
-	<td class=$td_class>".$db_row['username']."</td>
-	<td class=$td_class>".$db_row['name']."</td>
-	<td class=$td_class>".$db_row['email']."</td>	
-	<td class=$td_class>".$db_row['mobile']."</td>	
-	<td class=$td_class>".$db_row['credit']."</td>	
-	<td class=$td_class align=center>$action</td>
+	<td class='$td_class'>&nbsp;$i.</td>
+	<td class='$td_class'>".$db_row['username']."</td>
+	<td class='$td_class'>".$db_row['name']."</td>
+	<td class='$td_class'>".$db_row['email']."</td>	
+	<td class='$td_class'>".$db_row['mobile']."</td>	
+	<td class='$td_class'>".$db_row['credit']."</td>	
+	<td class='$td_class' align='center'>$action</td>
     </tr>
     ";
 	}
@@ -89,22 +89,22 @@ switch ($op)
     case "user_del":
 	$uname = $_REQUEST['uname'];
 	$del_uid = username2uid($uname);
-	$error_string = "Fail to delete user `$uname`!";
+	$error_string = _('Fail to delete user')." `$uname`!";
 	if (($del_uid > 1) && ($del_uid != $uid))
 	{
 	    $db_query = "DELETE FROM "._DB_PREF_."_tblUser WHERE uid='$del_uid'";
 	    if (@dba_affected_rows($db_query))
 	    {
-		$error_string = "User `$uname` has been deleted!";
+		$error_string = _('User')." `$uname` "._('has been deleted!');
 	    }
 	}
 	if (($del_uid == 1) || ($uname == "admin"))
 	{
-	    $error_string = "User `$uname` is immune to deletion!";
+	    $error_string = _('User')." `$uname` "._('is immune to deletion!');
 	}
 	else if ($del_uid == $uid)
 	{
-	    $error_string = "Current logged in user is immune to deletion!";
+	    $error_string = _('Current logged in user is immune to deletion!');
 	}
 	header ("Location: menu.php?inc=user_mgmnt&op=user_list&err=".urlencode($error_string));
 	break;
@@ -119,50 +119,50 @@ switch ($op)
 	$credit = username2credit($uname);
 	if ($err)
 	{
-	    $content = "<p><font color=red>$err</font><p>";
+	    $content = "<p><font color='red'>$err</font><p>";
 	}
 	// if ($status == 1) { $selected_1 = "selected"; }
 	if ($status == 2) { $selected_2 = "selected"; }
 	if ($status == 3) { $selected_3 = "selected"; }
 	$option_status = "
-	    <option value=2 $selected_2>Administrator</option>
+	    <option value='2' $selected_2>"._('Administrator')."</option>
 	    <!--
-	    <option value=1 $selected_1>Advertiser</option>
+	    <option value='1' $selected_1>"._('Advertiser')."</option>
 	    -->
-	    <option value=3 $selected_3>Normal User</option>
+	    <option value='3' $selected_3>"._('Normal User')."</option>
 	";
 	$content .= "
-	    <h2>Preferences: $uname</h2>
+	    <h2>"._('Preferences').": $uname</h2>
 	    <p>
-	    <form action=menu.php?inc=user_mgmnt&op=user_edit_save method=post>
-	    <input type=hidden name=uname value=\"$uname\">
-	<table width=100% cellpadding=1 cellspacing=2 border=0>
+	    <form action='menu.php?inc=user_mgmnt&op=user_edit_save' method='post'>
+	    <input type='hidden' name='uname' value=\"$uname\">
+	<table width='100%' cellpadding='1' cellspacing='2' border='0'>
 	    <tr>
-		<td width=175>Username</td><td width=5>:</td><td><b>$uname</b></td>
+		<td width='175'>"._('Username')."</td><td width='5'>:</td><td><b>$uname</b></td>
 	    </tr>
 	    <tr>
-		<td>Email</td><td>:</td><td><input type=text size=30 maxlength=30 name=up_email value=\"$email\"></td>
+		<td>"._('Email')."</td><td>:</td><td><input type='text' size='30' maxlength='30' name='up_email' value=\"$email\"></td>
 	    </tr>
 	    <tr>
-		<td>Full name</td><td>:</td><td><input type=text size=30 maxlength=30 name=up_name value=\"$name\"></td>
+		<td>"._('Full name')."</td><td>:</td><td><input type='text' size='30' maxlength='30' name='up_name' value=\"$name\"></td>
 	    </tr>	    	    
 	    <tr>
-		<td>Mobile (sender number)</td><td>:</td><td><input type=text size=16 maxlength=16 name=up_mobile value=\"$mobile\"> (Max. 16 numeric or 11 alphanumeric char.)</td>
+		<td>"._('Mobile (sender number').")</td><td>:</td><td><input type='text' size='16' maxlength='16' name='up_mobile' value=\"$mobile\"> "._('(Max. 16 numeric or 11 alphanumeric char.)')."</td>
 	    </tr>
 	    <tr>
-		<td>SMS Sender ID (SMS footer)</td><td>:</td><td><input type=text size=35 maxlength=30 name=up_sender value=\"$sender\"> (Max. 30 Alphanumeric char.)</td>
+		<td>"._('SMS Sender ID (SMS footer)')."</td><td>:</td><td><input type='text' size='35' maxlength='30' name='up_sender' value=\"$sender\"> "._('(Max. 30 Alphanumeric char.)')."</td>
 	    </tr>	    
 	    <tr>
-		<td>Password</td><td>:</td><td><input type=text size=30 maxlength=30 name=up_password> (Fill to change password for username `$uname`)</td>
+		<td>"._('Password')."</td><td>:</td><td><input type='text' size='30' maxlength='30' name='up_password'> ("._('Fill to change password for username')." `$uname`)</td>
 	    </tr>	    
 	    <tr>
-		<td>Credit</td><td>:</td><td><input type=text size=16 maxlength=30 name=up_credit value=\"$credit\"></td>
+		<td>"._('Credit')."</td><td>:</td><td><input type='text' size='16' maxlength='30' name='up_credit' value=\"$credit\"></td>
 	    </tr>	    
 	    <tr>
-		<td>User level</td><td>:</td><td><select name=up_status>$option_status</select></td>
+		<td>"._('User level')."</td><td>:</td><td><select name='up_status'>$option_status</select></td>
 	    </tr>
 	</table>	    
-	    <p><input type=submit class=button value=save>
+	    <p><input type='submit' class='button' value='save'>
 	    </form>
 	";
 	echo $content;
@@ -177,14 +177,14 @@ switch ($op)
 	$up_status = $_POST['up_status'];
 	$up_credit = $_POST['up_credit'];
 //	$status = username2status($uname);
-	$error_string = "No changes made!";
+	$error_string = _('No changes made!');
 	if ($up_name && $up_mobile && $up_email)
 	{
 	    $db_query = "SELECT email FROM "._DB_PREF_."_tblUser WHERE email='$up_email' AND NOT username='$uname'";
 	    $db_result = dba_num_rows($db_query);
 	    if ($db_result > 0)
 	    {
-		$error_string = "Email `$email` already in use by other username";
+		$error_string = _('Email')." `$email` "._('already in use by other username');
 	    }
 	    else
 	    {
@@ -195,63 +195,63 @@ switch ($op)
 		$db_query = "UPDATE "._DB_PREF_."_tblUser SET c_timestamp='".mktime()."',name='$up_name',email='$up_email',mobile='$up_mobile',sender='$up_sender',status='$up_status'".$chg_pwd.",credit='$up_credit' WHERE username='$uname'";
 		if (@dba_affected_rows($db_query))
 		{
-		    $error_string = "Preferences for user `$uname` has been saved";
+		    $error_string = _('Preferences for user')." `$uname` "._('has been saved');
 		}
 		else
 		{
-		    $error_string = "Fail to save preferences for `$uname`";
+		    $error_string = _('Fail to save preferences for')." `$uname`";
 		}
 	    }
 	}
 	else
 	{
-	    $error_string = "Empty field is not allowed";
+	    $error_string = _('Empty field is not allowed');
 	}
 	header ("Location: menu.php?inc=user_mgmnt&op=user_edit&uname=$uname&err=".urlencode($error_string));
 	break;
     case "user_add":
 	if ($err)
 	{
-	    $content = "<p><font color=red>$err</font><p>";
+	    $content = "<p><font color='red'>$err</font><p>";
 	}
 	$option_status = "
-	    <option value=2>Administrator</option>
+	    <option value='2'>"._('Administrator')."</option>
 	    <!--
-	    <option value=1>Advertiser</option>
+	    <option value='1'>"._('Advertiser')."</option>
 	    -->
-	    <option value=3 selected>Normal User</option>
+	    <option value='3' selected>"._('Normal User')."</option>
 	";
 	$content .= "
-	    <h2>Add user</h2>
+	    <h2>"._('Add user')."</h2>
 	    <p>
-	    <form action=menu.php?inc=user_mgmnt&op=user_add_yes method=post>
-	<table width=100% cellpadding=1 cellspacing=2 border=0>
+	    <form action='menu.php?inc=user_mgmnt&op=user_add_yes' method='post'>
+	<table width='100%' cellpadding='1' cellspacing='2' border='0'>
 	    <tr>
-		<td width=175>Username</td><td width=5>:</td><td><input type=text size=30 maxlength=30 name=add_username value=\"$add_username\"></td>
+		<td width='175'>"._('Username')."</td><td width='5'>:</td><td><input type='text' size='30' maxlength='30' name='add_username' value=\"$add_username\"></td>
 	    </tr>
 	    <tr>
-		<td>Email</td><td>:</td><td><input type=text size=30 maxlength=30 name=add_email value=\"$add_email\"></td>
+		<td>"._('Email')."</td><td>:</td><td><input type='text' size='30' maxlength='30' name='add_email' value=\"$add_email\"></td>
 	    </tr>
 	    <tr>
-		<td>Full name</td><td>:</td><td><input type=text size=30 maxlength=30 name=add_name value=\"$add_name\"></td>
+		<td>"._('Full name')."</td><td>:</td><td><input type='text' size='30' maxlength='30' name='add_name' value=\"$add_name\"></td>
 	    </tr>
 	    <tr>
-		<td>Mobile (sender number)</td><td>:</td><td><input type=text size=16 maxlength=16 name=add_mobile value=\"$add_mobile\"> (Max. 16 numeric char.)</td>
+		<td>"._('Mobile (sender number)')."</td><td>:</td><td><input type='text' size='16' maxlength='16' name='add_mobile' value=\"$add_mobile\"> (Max. 16 numeric char.)</td>
 	    </tr>
 	    <tr>
-		<td>SMS Sender ID (SMS footer)</td><td>:</td><td><input type=text size=35 maxlength=30 name=add_sender value=\"$add_sender\"> (Max. 30 Alphanumeric char.)</td>
+		<td>"._('SMS Sender ID (SMS footer)')."</td><td>:</td><td><input type='text' size='35' maxlength='30' name='add_sender' value=\"$add_sender\"> "._('(Max. 30 Alphanumeric char.)')."</td>
 	    </tr>	    	    	    
 	    <tr>
-		<td>Password</td><td>:</td><td><input type=text size=30 maxlength=30 name=add_password value=\"$add_password\"></td>
+		<td>"._('Password')."</td><td>:</td><td><input type='text' size='30' maxlength='30' name='add_password' value=\"$add_password\"></td>
 	    </tr>
 	    <tr>
-		<td>Credit</td><td>:</td><td><input type=text size=16 maxlength=30 name=add_credit value=\"$add_credit\"></td>
+		<td>"._('Credit')."</td><td>:</td><td><input type='text' size='16' maxlength='30' name='add_credit' value=\"$add_credit\"></td>
 	    </tr>
 	    <tr>
-		<td>User level</td><td>:</td><td><select name=add_status>$option_status</select></td>
+		<td>"._('User level')."</td><td>:</td><td><select name='add_status'>$option_status</select></td>
 	    </tr>
 	</table>	    
-	    <p><input type=submit class=button value=Add>
+	    <p><input type='submit' class='button' value='"._('Add')."'>
 	    </form>
 	";
 	echo $content;
@@ -281,13 +281,13 @@ switch ($op)
 		";
 		if ($new_uid = @dba_insert_id($db_query))
 		{
-		    $error_string = "User with username `$add_username` has been added";
+		    $error_string = _('User with username')." `$add_username` "._('has been added');
 		}
 	    }
 	}
 	else
 	{
-	    $error_string = "You must fill all fields!";
+	    $error_string = _('You must fill all fields!');
 	}
 	header ("Location: menu.php?inc=user_mgmnt&op=user_add&err=".urlencode($error_string));
 	break;
