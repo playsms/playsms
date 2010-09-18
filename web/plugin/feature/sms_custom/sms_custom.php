4a0d1ca121d151fd7fe44e1ba4a91ca546cc2f34
antonraharja <antonraharja@a2ba5a7b-9259-4a92-b3f4-97befcce6881>
2010-09-18 09:43:57 +0000

    gettext-ing sms_custom
    git-svn-id: http://svn.sourceforge.telkomspeedy.com/svn/playsms/trunk@85 a2ba5a7b-9259-4a92-b3f4-97befcce6881

--- web/plugin/feature/sms_custom/sms_custom.php
+++ web/plugin/feature/sms_custom/sms_custom.php
@@ -9,9 +9,9 @@ switch ($op)
 	    $content = "<p><font color=red>$err</font><p>";
 	}
 	$content .= "
-	    <h2>Manage custom</h2>
+	    <h2>"._('Manage custom')."</h2>
 	    <p>
-	    <input type=button value=\"Add SMS custom\" onClick=\"javascript:linkto('menu.php?inc=feature_sms_custom&op=sms_custom_add')\" class=\"button\" />
+	    <input type=button value=\""._('Add SMS custom')."\" onClick=\"javascript:linkto('menu.php?inc=feature_sms_custom&op=sms_custom_add')\" class=\"button\" />
 	    <p>
 	";
 	if (!isadmin())
@@ -24,10 +24,10 @@ switch ($op)
     <table cellpadding=1 cellspacing=2 border=0 width=100%>
     <tr>
         <td class=box_title width=25>*</td>
-        <td class=box_title width=100>Keyword</td>
-        <td class=box_title>Exec</td>
-        <td class=box_title width=100>User</td>	
-        <td class=box_title width=75>Action</td>
+        <td class=box_title width=100>"._('Keyword')."</td>
+        <td class=box_title>"._('Exec')."</td>
+        <td class=box_title width=100>"._('User')."</td>	
+        <td class=box_title width=75>"._('Action')."</td>
     </tr>
 	";
 	$i=0;
@@ -38,7 +38,7 @@ switch ($op)
             $td_class = ($i % 2) ? "box_text_odd" : "box_text_even";
 	    $owner = uid2username($db_row['uid']);
 	    $action = "<a href=menu.php?inc=feature_sms_custom&op=sms_custom_edit&custom_id=".$db_row['custom_id'].">$icon_edit</a>&nbsp;";
-	    $action .= "<a href=\"javascript: ConfirmURL('Are you sure you want to delete SMS custom keyword `".$db_row['custom_keyword']."` ?','menu.php?inc=feature_sms_custom&op=sms_custom_del&custom_id=".$db_row['custom_id']."')\">$icon_delete</a>";
+	    $action .= "<a href=\"javascript: ConfirmURL('"._('Are you sure you want to delete SMS custom keyword ?')." ("._('keyword').": `".$db_row['custom_keyword']."`)','menu.php?inc=feature_sms_custom&op=sms_custom_del&custom_id=".$db_row['custom_id']."')\">$icon_delete</a>";
 	    $custom_url = ( (strlen($db_row['custom_url']) > $maxlen) ? substr($db_row['custom_url'],0,$maxlen)."..." : $db_row['custom_url'] );
 	    $content .= "
     <tr>
@@ -53,7 +53,7 @@ switch ($op)
 	echo $content;
 	echo "
 	    <p>
-	    <input type=button value=\"Add SMS custom\" onClick=\"javascript:linkto('menu.php?inc=feature_sms_custom&op=sms_custom_add')\" class=\"button\" />
+	    <input type=button value=\""._('Add SMS custom')."\" onClick=\"javascript:linkto('menu.php?inc=feature_sms_custom&op=sms_custom_add')\" class=\"button\" />
 	";
 	break;
     case "sms_custom_edit":
@@ -69,19 +69,19 @@ switch ($op)
 	    $content = "<p><font color=red>$err</font><p>";
 	}
 	$content .= "
-	    <h2>Edit SMS custom</h2>
+	    <h2>"._('Edit SMS custom')."</h2>
 	    <p>
 	    <form action=menu.php?inc=feature_sms_custom&op=sms_custom_edit_yes method=post>
 	    <input type=hidden name=edit_custom_id value=$custom_id>
 	    <input type=hidden name=edit_custom_keyword value=$edit_custom_keyword>
-	    <p>SMS custom keyword: <b>$edit_custom_keyword</b>
-	    <p>Pass these parameter to custom URL field:
-	    <p><b>{SMSDATETIME}</b> replaced by SMS incoming date/time
-	    <p><b>{SMSSENDER}</b> replaced by sender number
-	    <p><b>{CUSTOMKEYWORD}</b> replaced by custom keyword 
-	    <p><b>{CUSTOMPARAM}</b> replaced by custom parameter passed to server from SMS
-	    <p>SMS custom URL: <input type=text size=60 name=edit_custom_url value=\"$edit_custom_url\">
-	    <p><input type=submit class=button value=Save>
+	    <p>"._('SMS custom keyword').": <b>$edit_custom_keyword</b>
+	    <p>"._('Pass these parameter to custom URL field').":
+	    <p><b>{SMSDATETIME}</b> "._('will be replaced by SMS incoming date/time')."
+	    <p><b>{SMSSENDER}</b> "._('will be replaced by sender number')."
+	    <p><b>{CUSTOMKEYWORD}</b> "._('will be replaced by custom keyword')."
+	    <p><b>{CUSTOMPARAM}</b> "._('will be replaced by custom parameter passed to server from SMS')."
+	    <p>"._('SMS custom URL').": <input type=text size=60 name=edit_custom_url value=\"$edit_custom_url\">
+	    <p><input type=submit class=button value=\""._('Save')."\">
 	    </form>
 	";
 	echo $content;
@@ -95,16 +95,16 @@ switch ($op)
 	    $db_query = "UPDATE "._DB_PREF_."_featureCustom SET c_timestamp='".mktime()."',custom_url='$edit_custom_url' WHERE custom_keyword='$edit_custom_keyword' AND uid='$uid'";
 	    if (@dba_affected_rows($db_query))
 	    {
-		$error_string = "SMS custom keyword `$edit_custom_keyword` has been saved";
+		$error_string = _('SMS custom has been saved')." ("._('keyword')." `$edit_custom_keyword`)";
 	    }
 	    else
 	    {
-	        $error_string = "Fail to save SMS custom keyword `$edit_custom_keyword`";
+	        $error_string = _('Fail to save SMS custom')." ("._('keyword').": `$edit_custom_keyword`)";
 	    }
 	}
 	else
 	{
-	    $error_string = "You must fill all fields!";
+	    $error_string = _('You must fill all fields');
 	}
 	header ("Location: menu.php?inc=feature_sms_custom&op=sms_custom_edit&custom_id=$edit_custom_id&err=".urlencode($error_string));
 	break;
@@ -119,11 +119,11 @@ switch ($op)
 	    $db_query = "DELETE FROM "._DB_PREF_."_featureCustom WHERE custom_keyword='$keyword_name'";
 	    if (@dba_affected_rows($db_query))
 	    {
-		$error_string = "SMS custom keyword `$keyword_name` has been deleted!";
+		$error_string = _('SMS custom has been deleted')." ("._('keyword')." `$keyword_name`)";
 	    }
 	    else
 	    {
-		$error_string = "Fail to delete SMS custom keyword `$keyword_name`";
+		$error_string = _('Fail to delete SMS custom')." ("._('keyword').": `$keyword_name`)";
 	    }
 	}
 	header ("Location: menu.php?inc=feature_sms_custom&op=sms_custom_list&err=".urlencode($error_string));
@@ -134,17 +134,17 @@ switch ($op)
 	    $content = "<p><font color=red>$err</font><p>";
 	}
 	$content .= "
-	    <h2>Add SMS custom</h2>
+	    <h2>"._('Add SMS custom')."</h2>
 	    <p>
 	    <form action=menu.php?inc=feature_sms_custom&op=sms_custom_add_yes method=post>
-	    <p>SMS custom keyword: <input type=text size=10 maxlength=10 name=add_custom_keyword value=\"$add_custom_keyword\">
-	    <p>Pass these parameter to custom URL field:
-	    <p><b>{SMSDATETIME}</b> replaced by SMS incoming date/time
-	    <p><b>{SMSSENDER}</b> replaced by sender number
-	    <p><b>{CUSTOMKEYWORD}</b> replaced by custom keyword 
-	    <p><b>{CUSTOMPARAM}</b> replaced by custom parameter passed to server from SMS
-	    <p>SMS custom URL: <input type=text size=60 maxlength=200 name=add_custom_url value=\"$add_custom_url\">
-	    <p><input type=submit class=button value=Add>
+	    <p>"._('SMS custom keyword').": <input type=text size=10 maxlength=10 name=add_custom_keyword value=\"$add_custom_keyword\">
+	    <p>"._('Pass these parameter to custom URL field').":
+	    <p><b>{SMSDATETIME}</b> "._('will be replaced by SMS incoming date/time')."
+	    <p><b>{SMSSENDER}</b> "._('will be replaced by sender number')."
+	    <p><b>{CUSTOMKEYWORD}</b> "._('will be replaced by custom keyword')."
+	    <p><b>{CUSTOMPARAM}</b> "._('will be replaced by custom parameter passed to server from SMS')."
+	    <p>"._('SMS custom URL').": <input type=text size=60 maxlength=200 name=add_custom_url value=\"$add_custom_url\">
+	    <p><input type=submit class=button value=\""._('Add')."\">
 	    </form>
 	";
 	echo $content;
@@ -159,21 +159,21 @@ switch ($op)
 		$db_query = "INSERT INTO "._DB_PREF_."_featureCustom (uid,custom_keyword,custom_url) VALUES ('$uid','$add_custom_keyword','$add_custom_url')";
 		if ($new_uid = @dba_insert_id($db_query))
 		{
-	    	    $error_string = "SMS custom keyword `$add_custom_keyword` has been added";
+	    	    $error_string = _('SMS custom has been added')." ("._('keyword').": `$add_custom_keyword`)";
 		}
 		else
 		{
-	    	    $error_string = "Fail to add SMS custom keyword `$add_custom_keyword`";
+	    	    $error_string = _('Fail to add SMS custom')." ("._('keyword').": `$add_custom_keyword`)";
 		}
 	    }
 	    else
 	    {
-		$error_string = "SMS keyword `$add_custom_keyword` already exists, reserved or use by other feature!";
+		$error_string = _('SMS custom already exists, reserved or use by other feature')." ("._('keyword').": `$add_custom_keyword`)";
 	    }
 	}
 	else
 	{
-	    $error_string = "You must fill all fields!";
+	    $error_string = _('You must fill all fields');
 	}
 	header ("Location: menu.php?inc=feature_sms_custom&op=sms_custom_add&err=".urlencode($error_string));
 	break;
