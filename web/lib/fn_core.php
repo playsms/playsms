21781b5d9c710e978dacda2cdafb6c27a228771a
Anton Raharja <antonrd@users.sourceforge.net>
2010-12-06 20:14:54 +0700

    use username instead sender number in forwarded message from PV

--- web/lib/fn_core.php
+++ web/lib/fn_core.php
@@ -266,7 +266,11 @@ function insertsmstoinbox($sms_datetime,$sms_sender,$target_user,$message,$sms_r
 			    $unicode = 1;
 			}
 		    }
-		    $message = '@'.$sms_sender.' '.$message;
+		    $c_uid = mobile2uid($sms_sender);
+		    $c_user = user_getdatabyuid($c_uid);
+		    $c_username = $c_user['username'];
+		    $c_sender = $c_username ? $c_username : $sms_sender;
+		    $message = '@'.$c_sender.' '.$message;
 		    list($ok,$to,$smslog_id) = sendsms_pv($target_user,$mobile,$message,'text',$unicode);
 		    logger_print("send to sender:".$sms_sender." receiver:".$sms_receiver." target:".$target_user, 3, "insertsmstoinbox");
 		    if ($cek_ok = $ok[0]) {
