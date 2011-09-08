a72fc9e5a563f1586b69885139b7b52fa5bf587b
antonraharja <antonrd@gmail.com>
2011-09-09 02:50:41 +0700

    sms_survey added

---
+++ web/plugin/feature/sms_survey/config.php
@@ -0,0 +1,7 @@
+<?php
+
+// insert to left menu array
+$menutab_feature = $core_config['menu']['main_tab']['feature'];
+$arr_menu[$menutab_feature][] = array("index.php?app=menu&inc=feature_sms_survey&op=list", _('Manage survey'));
+
+?>
