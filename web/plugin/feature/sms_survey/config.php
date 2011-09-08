e825ba67e65193eca41b952d5bffc7511c955c51
antonraharja <antonrd@gmail.com>
2011-09-09 03:00:38 +0700

    removed sms_survey, it was already added and disabled, my bad, I forgot :). also add reportxls.php to _sms_survey

--- web/plugin/feature/sms_survey/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// insert to left menu array
-$menutab_feature = $core_config['menu']['main_tab']['feature'];
-$arr_menu[$menutab_feature][] = array("index.php?app=menu&inc=feature_sms_survey&op=list", _('Manage survey'));
-
-?>
