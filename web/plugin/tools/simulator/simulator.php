fb1a81206607ef1e33a6140cf59caf61e678926e
Anton Raharja <antonrd@users.sourceforge.net>
2011-11-18 21:40:35 +0700

    Send SMS simulator for testing purposes

---
+++ web/plugin/tools/simulator/simulator.php
@@ -0,0 +1,33 @@
+<?php if(!(defined('_SECURE_'))){die('Intruder alert');}; ?>
+<?php
+if(!valid()){forcenoaccess();};
+
+// error messages
+$error_content = '';
+if ($errid) {
+	$err = logger_get_error_string($errid);
+}
+if ($err) {
+	$error_content = "<div class=error_string>$err</div>";
+}
+
+switch ($op) {
+	case 'simulator_list':
+		$content = '<h2>'._('Simulator').'</h2><p />';
+		if ($error_content) {
+			$content .= '<p>'.$error_content.'</p>';
+		}
+		$content .= "
+			<form method='post' action='index.php?app=menu&inc=tools_simulator&op=submit'>
+                        <p>Message: <input type='text' name='message'></p>
+			<p><input class='button' type='submit' value='"._('Submit')."'></p>
+			</form>
+		";
+		echo $content;
+		break;
+        case 'submit':
+                $content = '<h2>'._('Simulator').'</h2><p />';
+                break;
+}
+
+?>
