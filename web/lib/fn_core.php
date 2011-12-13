e32a77ed31c94092ec057e666ea4c7a120db4b7f
antonraharja <antonrd@gmail.com>
2011-12-14 00:23:16 +0700

    fix insertsmstoinbox() fwd_to_mobile

--- web/lib/fn_core.php
+++ web/lib/fn_core.php
@@ -266,12 +266,11 @@ function insertsmstoinbox($sms_datetime,$sms_sender,$target_user,$message,$sms_r
 							$unicode = 1;
 						}
 					}
-					$mobile = username2mobile($target_user);
-					$message = '@'.$c_username.' '.$message;
-					logger_print("sending to mobile:".$mobile." user:".$target_user, 3, "insertsmstoinbox");
+					$message = $sender.' '.$message;
+					logger_print("send to mobile:".$mobile." from:".$sender." user:".$target_user, 3, "insertsmstoinbox");
 					list($ok, $to, $smslog_id) = sendsms_pv($target_user, $mobile, $message, 'text', $unicode);
-					if ($ok) {
-						logger_print("forwarded to:".$to." smslog_id:".$smslog_id." user:".$target_user, 3, "insertsmstoinbox");
+					if ($ok[0]) {
+                                                logger_print("sent to mobile:".$mobile." from:".$sender." user:".$target_user, 3, "insertsmstoinbox");
 					}
 				}
 			}
