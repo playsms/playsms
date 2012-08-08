085a8e0aac8d79ab8826923b9818f64f9a781458
Anton Raharja <antonrd@gmail.com>
2012-08-08 15:02:42 +0700

    add ability to process single queue in sendsmsd(), no need to check if theres any destination hasn't been deliver

--- web/lib/fn_sendsms.php
+++ web/lib/fn_sendsms.php
@@ -112,9 +112,12 @@ function sendsms_queue_push($queue_code,$sms_to) {
 	return $ok;
 }
 
-function sendsmsd() {
+function sendsmsd($single_queue='') {
 	global $core_config;
-	$db_query = "SELECT * FROM "._DB_PREF_."_tblSMSOutgoing_queue WHERE flag='0'";
+	if ($single_queue) {
+		$queue_sql = "AND queue_code='".$single_queue."'";
+	}
+	$db_query = "SELECT * FROM "._DB_PREF_."_tblSMSOutgoing_queue WHERE flag='0' ".$queue_sql;
 	$db_result = dba_query($db_query);
 	while ($db_row = dba_fetch_array($db_result)) {
 		$c_queue_id = $db_row['id'];
@@ -142,17 +145,12 @@ function sendsmsd() {
 			$db_query3 = "UPDATE "._DB_PREF_."_tblSMSOutgoing_queue_dst SET flag='$c_flag' WHERE id='$c_id'";
 			$db_result3 = dba_query($db_query3);
 		}
-		$c_flag = 0;
-		$db_query4 = "SELECT count(*) AS rows FROM "._DB_PREF_."_tblSMSOutgoing_queue_dst WHERE queue_id='$c_queue_id' AND flag='0'";
-		$db_result4 = dba_query($db_query4);
-		$db_row4 = dba_fetch_array($db_result4);
-		$unprocessed_rows = $db_row4['rows'];
-		if ($unprocessed_rows < 1) {
-			$c_flag = 1;
+		$db_query5 = "UPDATE "._DB_PREF_."_tblSMSOutgoing_queue SET flag='1', datetime_update='".$core_config['datetime']['now']."' WHERE id='$c_queue_id'";
+		if ($db_result5 = dba_affected_rows($db_query5)) {
+			logger_print("finish processing queue_code:".$c_queue_code." uid:".$c_uid." sender_id:".$c_sender_id, 3, "sendsmsd");
+		} else {
+			logger_print("fail to finalize process queue_code:".$c_queue_code." uid:".$c_uid." sender_id:".$c_sender_id, 3, "sendsmsd");
 		}
-		$db_query5 = "UPDATE "._DB_PREF_."_tblSMSOutgoing_queue SET flag='$c_flag', datetime_update='".$core_config['datetime']['now']."' WHERE id='$c_queue_id'";
-		$db_result5 = dba_query($db_query5);
-		logger_print("finish processing queue_code:".$c_queue_code." uid:".$c_uid." sender_id:".$c_sender_id, 3, "sendsmsd");
 	}
 }
 
