f52f24fe021e322e0b2644ceb788b8874b75e9da
Anton Raharja <antonrd@gmail.com>
2012-08-29 09:09:24 +0700

    gw smstools: change term spool_path to spool_dir

--- web/plugin/gateway/smstools/config.php
+++ web/plugin/gateway/smstools/config.php
@@ -2,7 +2,7 @@
 if(!(defined('_SECURE_'))){die('Intruder alert');};
 
 $smstools_param['name'] = "smstools";
-$smstools_param['spool_path'] = "/var/spool/sms";
+$smstools_param['spool_dir'] = "/var/spool/sms";
 
 // save plugin's parameters or options in $core_config
 $core_config['plugin']['smstools'] = $smstools_param;
