cd979e128d55a0c15bdd728f36a530cf79652926
Anton Raharja <antonrd@gmail.com>
2012-09-17 00:33:17 +0700

    add config for playsmsd and sendsmsd in windows, playsms.bat

---
+++ bin/windows/playsms.bat
@@ -0,0 +1,10 @@
+@echo off
+SET PLAYSMS_PATH=C:\xampp\htdocs\playsms\web
+SET PLAYSMS_BIN=C:\playsms\bin
+SET PLAYSMS_SMSD=\usr\bin
+SET PLAYSMS_LOG=C:\playsms\logs
+SET PLAYSMS_SPOOL=C:\playsms\spool
+SET REFRESH_RATE=2
+SET PHP_CLI=C:\xampp\php\php
+SET GAMMU_BIN=C:\gammu\bin
+
