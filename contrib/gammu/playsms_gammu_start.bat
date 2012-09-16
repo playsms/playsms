00b8a676728e25fdeb7c0d4ace01f6d819c0380f
Anton Raharja <antonrd@gmail.com>
2012-09-17 00:35:23 +0700

    add bat files to start and stop gammu, playsmsd, sendsmsd in windows

---
+++ contrib/gammu/playsms_gammu_start.bat
@@ -0,0 +1,7 @@
+@echo off
+
+call C:\playsms\etc\playsms
+
+start "gammu" %GAMMU_BIN%\gammu-smsd -c C:\playsms\etc\gammu-smsdrc
+start "playsmsd_start" %PLAYSMS_BIN%\playsmsd_start.bat
+TASKKILL /F /FI "WINDOWTITLE eq playsmsd_start*"
---
+++ contrib/gammu/playsms_gammu_stop.bat
@@ -0,0 +1,5 @@
+@echo off
+
+TASKKILL /F /FI "WINDOWTITLE eq gammu*"
+TASKKILL /F /FI "WINDOWTITLE eq playsmsd*"
+TASKKILL /F /FI "WINDOWTITLE eq sendsmsd*"
