6cbb1f13560b430edc38d3e6377545e986154e5d
Anton Raharja <antonrd@gmail.com>
2012-09-30 07:52:33 +0700

    update language tools, add msgattrib to remove fuzzy and omit headers in .pot files

--- contrib/tools/language/1-update-pot-files.sh
+++ contrib/tools/language/1-update-pot-files.sh
@@ -18,8 +18,9 @@ find inc/ -iname "*.php" -exec xgettext --from-code=utf-8 -j -o plugin/language/
 ##Themes,plugins and tools strings
 cd $PLAYSMS
 find . -type d -name "language" | grep -v "plugin/language" | sed -e "s/\/[^\/]*$//" > /tmp/.lang_folders
+for i in `cat /tmp/.lang_folders` ; do rm -f "$i/language/messages.pot" ; done
 for i in `cat /tmp/.lang_folders` ; do touch "$i/language/messages.pot" ; done
-for i in `cat /tmp/.lang_folders` ; do find $i -iname '*.php' -exec xgettext --from-code=utf-8 -j -o $i/language/messages.pot {} \; ; done
+for i in `cat /tmp/.lang_folders` ; do find $i -iname '*.php' -exec xgettext -L PHP --omit-header --from-code=utf-8 -j -o $i/language/messages.pot {} \; ; done
 rm /tmp/.lang_folders
 
 cd $CWD
--- contrib/tools/language/2-merge-existing-po-files.sh
+++ contrib/tools/language/2-merge-existing-po-files.sh
@@ -15,6 +15,7 @@ for i in `cat /tmp/.lang_folders` ; do
 	for j in `ls -1 $i/language/ | grep '_'` ; do
 		msgmerge $i/language/$j/LC_MESSAGES/messages.po $i/language/messages.pot > $i/language/$j/LC_MESSAGES/messages.po.tmp
 		mv $i/language/$j/LC_MESSAGES/messages.po.tmp $i/language/$j/LC_MESSAGES/messages.po
+		msgattrib --no-fuzzy --no-obsolete $i/language/$j/LC_MESSAGES/messages.po
 	done
 done
 
