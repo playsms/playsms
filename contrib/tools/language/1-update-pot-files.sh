a2a3a4a3329ab7929381b11adfad2cc5b1c77c44
Anton Raharja <antonrd@gmail.com>
2012-10-13 01:30:50 +0700

    update language tools to create .pot without location and headers, easier to read and work with

--- contrib/tools/language/1-update-pot-files.sh
+++ contrib/tools/language/1-update-pot-files.sh
@@ -12,15 +12,15 @@ CWD=$(pwd)
 ##Common strings
 cd $PLAYSMS
 touch plugin/language/messages.pot
-find lib/ -iname "*.php" -exec xgettext -L PHP --omit-header --from-code=utf-8 -j -o plugin/language/messages.pot {} \;
-find inc/ -iname "*.php" -exec xgettext -L PHP --omit-header --from-code=utf-8 -j -o plugin/language/messages.pot {} \;
+find lib/ -iname "*.php" -exec xgettext -L PHP --omit-header --no-location --sort-output --from-code=utf-8 -j -o plugin/language/messages.pot {} \;
+find inc/ -iname "*.php" -exec xgettext -L PHP --omit-header --no-location --sort-output --from-code=utf-8 -j -o plugin/language/messages.pot {} \;
 
 ##Themes,plugins and tools strings
 cd $PLAYSMS
 find . -type d -name "language" | grep -v "plugin/language" | sed -e "s/\/[^\/]*$//" > /tmp/.lang_folders
 for i in `cat /tmp/.lang_folders` ; do rm -f "$i/language/messages.pot" ; done
 for i in `cat /tmp/.lang_folders` ; do touch "$i/language/messages.pot" ; done
-for i in `cat /tmp/.lang_folders` ; do find $i -iname '*.php' -exec xgettext -L PHP --omit-header --from-code=utf-8 -j -o $i/language/messages.pot {} \; ; done
+for i in `cat /tmp/.lang_folders` ; do find $i -iname '*.php' -exec xgettext -L PHP --omit-header --no-location --sort-output --from-code=utf-8 -j -o $i/language/messages.pot {} \; ; done
 rm /tmp/.lang_folders
 
 cd $CWD
