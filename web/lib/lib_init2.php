20a30deb75237ea7132feb275cb0be5d563548a8
Anton Raharja <antonrd@gmail.com>
2012-10-15 10:39:40 +0700

    insert var $plugin, soon to be all plugin's variables, into $core_config

--- web/lib/lib_init2.php
+++ web/lib/lib_init2.php
@@ -6,7 +6,10 @@ defined('_SECURE_') or die('Forbidden');
 // load menus into core_config
 $core_config['menu'] = $arr_menu;
 
+// load plugin's config into core_config
+$core_config['plugins'] = $plugin;
+
 // fixme anton - uncomment this if you want to know what are available in $core_config
-//print_r($core_config); die();
+print_r($core_config); die();
 
 ?>
