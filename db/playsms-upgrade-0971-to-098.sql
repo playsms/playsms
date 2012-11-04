1b9f6e6d594695e7975a29adba860550e1d1bb0d
Anton Raharja <antonrd@gmail.com>
2012-11-05 00:16:36 +0700

    change password column datatype to varchar(32) to hold md5 string only

--- db/playsms-upgrade-0971-to-098.sql
+++ db/playsms-upgrade-0971-to-098.sql
@@ -4,3 +4,5 @@ ALTER TABLE `playsms_tblSMSTemplate` RENAME TO `playsms_toolsMsgtemplate` ;
 
 ALTER TABLE `playsms_tblUser` ADD `register_datetime` varchar(20) NOT NULL DEFAULT '0000-00-00 00:00:00' AFTER `plus_sign_add` ;
 ALTER TABLE `playsms_tblUser` ADD `lastupdate_datetime` varchar(20) NOT NULL DEFAULT '0000-00-00 00:00:00' AFTER `register_datetime` ;
+
+ALTER TABLE `playsms_tblUser` MODIFY `password` varchar(32) NOT NULL ;
