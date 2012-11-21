aebf211ffbd77a9b0131b7b26c32d6e63b47dab7
Anton Raharja <antonrd@gmail.com>
2012-11-21 07:02:51 +0700

    code cleanups in sms_command_handle()

--- web/plugin/feature/sms_command/fn.php
+++ web/plugin/feature/sms_command/fn.php
@@ -54,35 +54,35 @@ function sms_command_hook_setsmsincomingaction($sms_datetime,$sms_sender,$comman
 	return $ret;
 }
 
-function sms_command_handle($c_uid,$sms_datetime,$sms_sender,$sms_receiver,$command_keyword,$command_param='',$raw_message='')
-{
+function sms_command_handle($c_uid,$sms_datetime,$sms_sender,$sms_receiver,$command_keyword,$command_param='',$raw_message='') {
 	global $datetime_now, $plugin_config;
 	$ok = false;
 	$db_query = "SELECT command_exec,uid,command_return_as_reply FROM "._DB_PREF_."_featureCommand WHERE command_keyword='$command_keyword'";
 	$db_result = dba_query($db_query);
 	$db_row = dba_fetch_array($db_result);
 	$command_exec = $db_row['command_exec'];
-	$sms_datetime = core_display_datetime($sms_datetime);
-	$command_exec = str_replace("{SMSDATETIME}","\"$sms_datetime\"",$command_exec);
-	$command_exec = str_replace("{SMSSENDER}","\"$sms_sender\"",$command_exec);
-	$command_exec = str_replace("{COMMANDKEYWORD}","\"$command_keyword\"",$command_exec);
-	$command_exec = str_replace("{COMMANDPARAM}","\"$command_param\"",$command_exec);
-	$command_exec = str_replace("{COMMANDRAW}","\"$raw_message\"",$command_exec);
-	$command_exec = $plugin_config['feature']['sms_command']['bin']."/".$command_exec;
-	$command_output = shell_exec(stripslashes($command_exec));
-        $username   = uid2username($db_row['uid']);
-        if ($db_row['command_return_as_reply'] == 1) {
-                sendsms_pv($username, $sms_sender, $command_output, 'text', 0);
-        }
-	$db_query = "
-	INSERT INTO "._DB_PREF_."_featureCommand_log
-	(sms_sender,command_log_datetime,command_log_keyword,command_log_exec) 
-	VALUES
-	('$sms_sender','$datetime_now','$command_keyword','$command_exec')
-    ";
-	if ($new_id = @dba_insert_id($db_query))
-	{
-		$ok = true;
+	$command_return_as_reply = $db_row['command_return_as_reply'];
+	$username   = uid2username($db_row['uid']);
+	if ($command_keyword && $command_exec && $username) {
+		$sms_datetime = core_display_datetime($sms_datetime);
+		$command_exec = str_replace("{SMSDATETIME}","\"$sms_datetime\"",$command_exec);
+		$command_exec = str_replace("{SMSSENDER}","\"$sms_sender\"",$command_exec);
+		$command_exec = str_replace("{COMMANDKEYWORD}","\"$command_keyword\"",$command_exec);
+		$command_exec = str_replace("{COMMANDPARAM}","\"$command_param\"",$command_exec);
+		$command_exec = str_replace("{COMMANDRAW}","\"$raw_message\"",$command_exec);
+		$command_exec = $plugin_config['feature']['sms_command']['bin']."/".$command_exec;
+		$command_output = shell_exec(stripslashes($command_exec));
+		if ($command_return_as_reply == 1) {
+			sendsms_pv($username, $sms_sender, $command_output, 'text', 0);
+		}
+		$db_query = "
+			INSERT INTO "._DB_PREF_."_featureCommand_log
+			(sms_sender,command_log_datetime,command_log_keyword,command_log_exec) 
+			VALUES
+			('$sms_sender','$datetime_now','$command_keyword','$command_exec')";
+		if ($new_id = @dba_insert_id($db_query)) {
+			$ok = true;
+		}
 	}
 	return $ok;
 }
