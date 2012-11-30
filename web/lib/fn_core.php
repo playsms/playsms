71827271d82d044b81f8d6bc4213bbaa6cd6690b
Anton Raharja <antonrd@gmail.com>
2012-11-30 14:30:26 +0700

    allow A-Z a-z 0-9 . - for username

--- web/lib/fn_core.php
+++ web/lib/fn_core.php
@@ -220,7 +220,7 @@ function core_get_random_string($length = 8) {
  *
  */
 function core_sanitize_username($username) {
-	$username = preg_replace("/[^A-Za-z0-9 ]/", '', $username);
+	$username = preg_replace("/[^A-Za-z0-9\.\-]/", '', $username);
 	return $username;
 }
 
