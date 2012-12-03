51b0f0bcf58be0d58225ebb75bcfcfbeb13cdd42
Anton Raharja <antonrd@gmail.com>
2012-12-04 00:06:56 +0700

    lines per page is 30 (prev. 50), max pages per display is 10 (prev. 15)

--- web/lib/fn_themes.php
+++ web/lib/fn_themes.php
@@ -31,8 +31,8 @@ function themes_navbar($num, $nav, $max_nav, $url, $page) {
 
 function themes_nav($count, $url) {
 	$ret = false;
-	$lines_per_page = 2;
-	$max_nav = 15;
+	$lines_per_page = 30;
+	$max_nav = 10;
 	$num = ceil($count / $lines_per_page);
 	$nav = ( $_REQUEST['nav'] ? $_REQUEST['nav'] : 1 );
 	$page = ( $_REQUEST['page'] ? $_REQUEST['page'] : 1 );
