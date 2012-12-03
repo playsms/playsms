8b3fc7b2d89d3e6e600fb007c10b35c44694a5f1
Anton Raharja <antonrd@gmail.com>
2012-12-03 16:42:07 +0700

    add functions to add, update, remove users, this model will be used for other stuff mimicking CRUD

--- web/lib/fn_user.php
+++ web/lib/fn_user.php
@@ -1,15 +1,105 @@
 <?php
 defined('_SECURE_') or die('Forbidden');
 
-function user_getall() {
+function user_getall($fields=array()) {
 	$ret = array();
-	$db_query = "SELECT * FROM "._DB_PREF_."_tblUser";
+	if (is_array($fields)) {
+		foreach ($fields as $key => $val) {
+			$q_condition .= " AND ".$key."='".$val."'";
+		}
+		if ($q_condition) {
+			$q_condition = " WHERE 1=1 ".$q_condition;
+		}
+	}
+	$db_query = "SELECT * FROM "._DB_PREF_."_tblUser ".$q_condition;
 	$db_result = dba_query($db_query);
 	while ($db_row = dba_fetch_array($db_result)) {
 		$ret[] = $db_row;
 	}
 	return $ret;
 }
+
+function user_add($item) {
+	$ret = false;
+	if (is_array($item)) {
+		foreach ($item as $key => $val) {
+			$sets .= $key.",";
+			$vals .= "'".$val."',";
+		}
+		if ($sets && $vals) {
+			$sets = substr($sets, 0, -1);
+			$vals = substr($sets, 0, -1);
+			$db_query = "INSERT INTO "._DB_PREF_."_tblUser (".$sets.") VALUES (".$vals.")";
+			if ($c_id = dba_insert_id($db_query)) {
+				$ret = $c_id;
+			}
+		}
+	}
+	return $ret;
+}
+
+function user_update($item, $condition=array()) {
+	$ret = false;
+	if (is_array($item)) {
+		foreach ($item as $key => $val) {
+			$sets .= $key."='".$val."',";
+		}
+		if ($sets) {
+			$sets = substr($sets, 0, -1);
+			if (is_array($condition)) {
+				foreach ($condition as $key => $val){ 
+					$q_condition .= " AND ".$key."='".$val."'";
+				}
+				if ($q_condition) {
+					$q_condition = " WHERE 1=1 ".$q_condition;
+				}
+			}
+			$db_query = "UPDATE "._DB_PREF_."_tblUser SET ".$sets." ".$q_condition;
+			if ($c_rows = dba_affected_rows($db_query)) {
+				$ret = $c_rows;
+			}
+		}
+	}
+	return $ret;
+}
+
+function user_remove($condition=array()) {
+	$ret = false;
+	if (is_array($condition)) {
+		foreach ($condition as $key => $val){ 
+			$q_condition .= " AND ".$key."='".$val."'";
+		}
+		if ($q_condition) {
+			$q_condition = " WHERE 1=1 ".$q_condition;
+		}
+		$db_query = "DELETE FROM "._DB_PREF_."_tblUser ".$q_condition;
+		if ($c_rows = dba_affected_rows($db_query)) {
+			$ret = $c_rows;
+		}
+	}
+	return $ret;
+}
+
+function user_isavail($fields=array()) {
+	$ret = false;
+	if (is_array($fields)) {
+		foreach ($fields as $key => $val) {
+			$q_condition .= " AND ".$key."='".$val."'";
+		}
+		if ($q_condition) {
+			$q_condition = " WHERE 1=1 ".$q_condition;
+		}
+	}
+	$db_query = "SELECT * FROM "._DB_PREF_."_tblUser ".$q_condition." LIMIT 1";
+	$db_result = dba_query($db_query);
+	if ($db_row = dba_fetch_array($db_result)) {
+		$ret = 0;
+	} else {
+		$ret = 1;
+	}
+	return $ret;
+}
+
 
 function user_getallwithstatus($status) {
 	$ret = array();
