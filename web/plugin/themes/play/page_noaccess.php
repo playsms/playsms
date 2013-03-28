eb0cc4afaad1854bcf62dddd14d5e6e104b982a3
Anton Raharja <antonrd@gmail.com>
2013-03-28 12:08:58 +0700

    add Home link to page_noaccess

--- web/plugin/themes/play/page_noaccess.php
+++ web/plugin/themes/play/page_noaccess.php
@@ -1,4 +1,7 @@
 <?php defined('_SECURE_') or die('Forbidden'); ?>
 <?php include $apps_path['themes']."/".$themes_module."/header.php"; ?>
+<?php echo "<div align='center'>"; ?>
 <?php echo $error_content; ?>
+<?php echo "<p><a href='".$http_path['base']."'>"._('Home')."</a></p>"; ?>
+<?php echo "</div>"; ?>
 <?php include $apps_path['themes']."/".$themes_module."/footer.php"; ?>
