1bed145ebf1d2355b9fe0a2c7f5b58dc20960242
Anton Raharja <antonrd@gmail.com>
2013-04-11 13:02:50 +0700

    add new function core_net_match() to check if certain IP belongs to certain IP RANGE

--- web/lib/fn_core.php
+++ web/lib/fn_core.php
@@ -224,4 +224,59 @@ function core_sanitize_username($username) {
 	return $username;
 }
 
+/**
+ * Function: core_net_match()
+ * ref: http://stackoverflow.com/a/10422605 (Volomike)
+ *
+ * This function returns a boolean value.
+ * Usage: core_net_match("IP RANGE", "IP ADDRESS")
+ */
+function core_net_match($network, $ip) {
+	$network=trim($network);
+	$orig_network = $network;
+	$ip = trim($ip);
+	if ($ip == $network) {
+		// echo "used network ($network) for ($ip)\n";
+		return TRUE;
+	}
+	$network = str_replace(' ', '', $network);
+	if (strpos($network, '*') !== FALSE) {
+		if (strpos($network, '/') !== FALSE) {
+			$asParts = explode('/', $network);
+			$network = @ $asParts[0];
+		}
+		$nCount = substr_count($network, '*');
+		$network = str_replace('*', '0', $network);
+		if ($nCount == 1) {
+			$network .= '/24';
+		} else if ($nCount == 2) {
+			$network .= '/16';
+		} else if ($nCount == 3) {
+			$network .= '/8';
+		} else if ($nCount > 3) {
+			return TRUE; // if *.*.*.*, then all, so matched
+		}
+	}
+
+	// echo "from original network($orig_network), used network ($network) for ($ip)\n";
+
+	$d = strpos($network, '-');
+	if ($d === FALSE) {
+		$ip_arr = explode('/', $network);
+		if (!preg_match("@\d*\.\d*\.\d*\.\d*@", $ip_arr[0], $matches)) {
+			$ip_arr[0].=".0";// Alternate form 194.1.4/24
+		}
+		$network_long = ip2long($ip_arr[0]);
+		$x = ip2long($ip_arr[1]);
+		$mask = long2ip($x) == $ip_arr[1] ? $x : (0xffffffff << (32 - $ip_arr[1]));
+		$ip_long = ip2long($ip);
+		return ($ip_long & $mask) == ($network_long & $mask);
+	} else {
+		$from = trim(ip2long(substr($network, 0, $d)));
+		$to = trim(ip2long(substr($network, $d+1)));
+		$ip = ip2long($ip);
+		return ($ip>=$from and $ip<=$to);
+	}
+}
+
 ?>
