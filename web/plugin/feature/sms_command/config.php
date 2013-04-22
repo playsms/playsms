a53f08b86d6423685f017b5a13015580006c0808
Anton Raharja <antonrd@gmail.com>
2013-04-22 23:30:28 +0700

    sms_command bin path set to /var/lib/playsms/sms_command by default, change this from sms_command/config.php

--- web/plugin/feature/sms_command/config.php
+++ web/plugin/feature/sms_command/config.php
@@ -5,6 +5,6 @@ defined('_SECURE_') or die('Forbidden');
 $menutab_feature = $core_config['menutab']['feature'];
 $arr_menu[$menutab_feature][] = array("index.php?app=menu&inc=feature_sms_command&op=sms_command_list", _('Manage command'));
 
-$plugin_config['feature']['sms_command']['bin']	= $apps_path['base'].'/bin';
+$plugin_config['feature']['sms_command']['bin']	= '/var/lib/playsms/sms_command';
 
 ?>
