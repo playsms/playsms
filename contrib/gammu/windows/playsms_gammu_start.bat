34a9cf62112ad00a5b03c6838ef517e65e0407f0
Anton Raharja <antonrd@gmail.com>
2013-04-22 21:31:12 +0700

    update gammu for windows

--- contrib/gammu/windows/playsms_gammu_start.bat
+++ contrib/gammu/windows/playsms_gammu_start.bat
@@ -1,6 +1,6 @@
 @echo off
 
-call C:\playsms\etc\playsms
+call C:\playsms\etc\playsms.bat
 
 start "gammu" %GAMMU_BIN%\gammu-smsd -c C:\playsms\etc\gammu-smsdrc
 start "playsmsd_start" %PLAYSMS_BIN%\playsmsd_start.bat
