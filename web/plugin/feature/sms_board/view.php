49c603e05ba8e347326aef9ed8e84b7800f1948f
Anton Raharja <antonrd@gmail.com>
2013-04-25 16:05:01 +0700

    use correct $board_keyword

--- web/plugin/feature/sms_board/view.php
+++ web/plugin/feature/sms_board/view.php
@@ -11,21 +11,22 @@ switch ($op) {
 			$conditions['uid'] = $uid;
 		}
 		$list = dba_search(_DB_PREF_.'_featureBoard', '*', $conditions);
-		$output_serialize = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=VR&type=serialize";
-		$output_json = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=VR&type=json";
-		$output_xml = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=VR&type=xml";
-		$output_rss091 = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=VR&type=feed&format=rss0.91";
-		$output_rss10 = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=VR&type=feed&format=1.0";
-		$output_rss20 = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=VR&type=feed&format=2.0";
-		$output_atom = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=VR&type=feed&format=atom";
-		$output_mbox = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=VR&type=feed&format=mbox";
-		$output_html = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=VR&type=html";
+		$board_keyword = $list[0]['board_keyword'];
+		$output_serialize = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=".urlencode($board_keyword)."&type=serialize";
+		$output_json = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=".urlencode($board_keyword)."&type=json";
+		$output_xml = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=".urlencode($board_keyword)."&type=xml";
+		$output_rss091 = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=".urlencode($board_keyword)."&type=feed&format=rss0.91";
+		$output_rss10 = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=".urlencode($board_keyword)."&type=feed&format=1.0";
+		$output_rss20 = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=".urlencode($board_keyword)."&type=feed&format=2.0";
+		$output_atom = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=".urlencode($board_keyword)."&type=feed&format=atom";
+		$output_mbox = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=".urlencode($board_keyword)."&type=feed&format=mbox";
+		$output_html = $core_config['http_path']['base']."/index.php?app=webservices&ta=sms_board&keyword=".urlencode($board_keyword)."&type=html";
 		if ($err = $_SESSION['error_string']) {
 			$content = "<div class=error_string>$err</div>";
 		}
 		$content .= "
 			<h2>"._('Manage board')."</h2>
-			<h3>"._('View board')." : ".$list[0]['board_keyword']."</h3>
+			<h3>"._('View board')." : ".$board_keyword."</h3>
 			<table cellpadding=1 cellspacing=2 border=0 width=100%>
 				<tr><td width=100>"._('PHP serialize output')."</td><td>:</td><td><a href=\"".$output_serialize."\" target=_blank>".$output_serialize."</a></td></tr>
 				<tr><td>"._('JSON output')."</td><td>:</td><td><a href=\"".$output_json."\" target=_blank>".$output_json."</a></td></tr>
