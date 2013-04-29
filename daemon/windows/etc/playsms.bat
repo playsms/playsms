20dad8d8396a13b1c7ba716351ddd72345468891
Anton Raharja <antonrd@gmail.com>
2013-04-29 13:34:25 +0700

    remove variable PLAYSMS_DAEMON (only used by gnokii)

--- daemon/windows/etc/playsms.bat
+++ daemon/windows/etc/playsms.bat
@@ -1,7 +1,6 @@
 @echo off
 SET PLAYSMS_PATH=C:\xampp\htdocs\playsms\web
 SET PLAYSMS_BIN=C:\playsms\bin
-SET PLAYSMS_SMSD=\usr\bin
 SET PLAYSMS_LOG=C:\playsms\logs
 SET PLAYSMS_SPOOL=C:\playsms\spool
 SET REFRESH_RATE=2
