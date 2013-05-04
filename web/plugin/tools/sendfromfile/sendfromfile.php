17b93d4d3c52433a126c2c3705742cb4ff053657
Anton Raharja <antonrd@gmail.com>
2013-05-05 06:28:10 +0700

    sendfromfile cleanups, limit rows up to max 500 entries

--- web/plugin/tools/sendfromfile/sendfromfile.php
+++ web/plugin/tools/sendfromfile/sendfromfile.php
@@ -7,18 +7,12 @@ if (($route = $_REQUEST['route']) && ($route == 'user')) {
 	exit();
 }
 
+$sendfromfile_row_limit = 500;
+
 switch ($op) {
 	case 'list':
 		$content = '<h2>'._('Send from file').'</h2><p />';
-		// error messages
-		$error_content = '';
-		if ($err = $_SESSION['error_string']) {
-			$error_content = "<div class=error_string>$err</div>";
-		}
-		if ($error_content) {
-			$content .= '<p>'.$error_content.'</p>';
-		}
-		if ($core_config['user']['status']==2) {
+		if (isadmin()) {
 			$content .= "
 				<form action=\"index.php?app=menu&inc=tools_sendfromfile&op=upload_confirm\" enctype=\"multipart/form-data\" method=\"post\">
 					"._('Please select CSV file')." ("._('format : destination number, message, username').")<br>
@@ -33,105 +27,119 @@ switch ($op) {
 					<p><input type=\"submit\" value=\""._('Upload file')."\" class=\"button\">
 				</form>";
 		}
+		if ($err = $_SESSION['error_string']) {
+			echo "<div class=error_string>$err</div>";
+		}
 		echo $content;
 		break;
 	case 'upload_confirm':
-		$filename = $_FILES['fncsv']['name'];		
-		$fn = $_FILES['fncsv']['tmp_name'];		
+		$filename = $_FILES['fncsv']['name'];
+		$fn = $_FILES['fncsv']['tmp_name'];
 		$fs = $_FILES['fncsv']['size'];
-		$row = 0;		
+		$row = 0;
 		$valid = 0;
 		$invalid = 0;
 		if (($fs == filesize($fn)) && file_exists($fn)) {
 			if (($fd = fopen($fn, 'r')) !== FALSE) {
 				$sid = $core_config['user']['uid'].mktime().md5($fn);
-				while (($data = fgetcsv($fd, $fs, ',')) !== FALSE) {
+				$continue = true;
+				while ((($data = fgetcsv($fd, $fs, ',')) !== FALSE) && $continue) {
 					$row++;
-					$sms_to = $data[0];
-					$sms_msg = $data[1];
-					if ($core_config['user']['status']==2) {
-						$sms_username = $data[2];
+					$sms_to = trim($data[0]);
+					$sms_msg = trim($data[1]);
+					if (isadmin()) {
+						$sms_username = trim($data[2]);
+						$uid = username2uid($sms_username);
 					} else {
 						$sms_username = $core_config['user']['username'];
+						$uid = $core_config['user']['uid'];
 						$data[2] = $sms_username;
 					}
-					if ($sms_to && $sms_msg) {
-						if ($uid = username2uid($sms_username)) {
-							$db_query = "INSERT INTO "._DB_PREF_."_toolsSendfromfile (uid,sid,sms_datetime,sms_to,sms_msg,sms_username) ";
-							$db_query .= "VALUES ('$uid','$sid','".$core_config['datetime']['now']."','$sms_to','".addslashes($sms_msg)."','$sms_username')";
-							if ($db_result = dba_insert_id($db_query)) {
-								$valid++;
-								$item_valid[$valid-1] = $data;
-							}
+					if ($sms_to && $sms_msg && $uid) {
+						$db_query = "INSERT INTO "._DB_PREF_."_toolsSendfromfile (uid,sid,sms_datetime,sms_to,sms_msg,sms_username) ";
+						$db_query .= "VALUES ('$uid','$sid','".$core_config['datetime']['now']."','$sms_to','".addslashes($sms_msg)."','$sms_username')";
+						if ($db_result = dba_insert_id($db_query)) {
+							$valid++;
+							$item_valid[$valid-1] = $data;
 						} else {
 							$invalid++;
 							$item_invalid[$invalid-1] = $data;
 						}
+					} else if ($sms_to || $sms_msg) {
+						$invalid++;
+						$item_invalid[$invalid-1] = $data;
+					}
+					$num_of_rows = $valid + $invalid;
+					if ($num_of_rows >= $sendfromfile_row_limit) {
+						$continue = false;
 					}
 				}
 			}
 		} else {
-                        $_SESSION['error_string'] = _('Invalid CSV file');
-                        header("Location: index.php?app=menu&inc=tools_sendfromfile&op=list");
-                        exit();
-                        break;
-                }
+			$_SESSION['error_string'] = _('Invalid CSV file');
+			header("Location: index.php?app=menu&inc=tools_sendfromfile&op=list");
+			exit();
+			break;
+		}
 		$content = '<h2>'._('Send from file').'</h2><p />';
 		$content .= '<h3>'._('Confirmation').'</h3><p />';
 		$content .= _('Uploaded file').': '.$filename.'<p />';
-		$content .= '<p /><br />';
 		if ($valid) {
-			$content .= _('Found valid entries in uploaded file').' ('._('valid entries').': '.$valid.')<p />';
+			$content .= _('Found valid entries in uploaded file').' ('._('valid entries').': '.$valid.' '._('of').' '.$num_of_rows.')<p />';
 			$content .= '<h3>'._('Valid entries').'</h3><p />';
 			$content .= "
 				<table cellpadding=1 cellspacing=2 border=0 width=100% class=\"sortable\">
-					<tr>
-						<td class=box_title width=4>*</td>
-						<td class=box_title width='150'>"._('Destination number')."</td>
-						<td class=box_title>"._('Message')."</td>
-						<td class=box_title width='100' align=right>"._('Username')."</td>
-					</tr>
-			";
+				<thead><tr>
+					<th width=4>*</th>
+					<th width='20%'>"._('Destination number')."</th>
+					<th width='60%'>"._('Message')."</th>
+					<th width='20%'>"._('Username')."</th>
+				</tr></thead>
+				<tbody>";
+			$j = 0;
 			for ($i=0;$i<count($item_valid);$i++) {
-				$tr_class = ($i % 2) ? "box_text_odd" : "box_text_even";
-				$content .= "
-					<tr class='".$tr_class."'>
-						<td>".($i+1).".</td>
-						<td>".$item_valid[$i][0]."</td>
-						<td>".$item_valid[$i][1]."</td>
-						<td>".$item_valid[$i][2]."</td>
-					</tr>
-				";
+				if ($item_valid[$i][0] && $item_valid[$i][1] && $item_valid[$i][2]) {
+					$j++;
+					$tr_class = ($j % 2) ? "box_text_odd" : "box_text_even";
+					$content .= "
+						<tr class='".$tr_class."'>
+							<td>&nbsp;".$j.".</td>
+							<td align='center'>".$item_valid[$i][0]."</td>
+							<td>".$item_valid[$i][1]."</td>
+							<td align='center'>".$item_valid[$i][2]."</td>
+						</tr>";
+				}
 			}
-			$content .= "</table>";
+			$content .= "</tbody></table>";
 		}
-		$content .= '<p /><br />';
 		if ($invalid) {
-			$content .= _('Found invalid entries in uploaded file').' ('._('invalid entries').': '.$invalid.')<p />';
+			$content .= '<p /><br />';
+			$content .= _('Found invalid entries in uploaded file').' ('._('invalid entries').': '.$invalid.' '._('of').' '.$num_of_rows.')<p />';
 			$content .= '<h3>'._('Invalid entries').'</h3><p />';
 			$content .= "
 				<table cellpadding=1 cellspacing=2 border=0 width=100% class=\"sortable\">
-					<tr>
-						<td class=box_title width=4>*</td>
-						<td class=box_title width='150'>"._('Destination number')."</td>
-						<td class=box_title>"._('Message')."</td>
-						<td class=box_title width='100' align=right>"._('Username')."</td>
-					</tr>
-			";
+				<thead><tr>
+					<th width=4>*</th>
+					<th width='20%'>"._('Destination number')."</th>
+					<th width='60%'>"._('Message')."</th>
+					<th width='20%'>"._('Username')."</th>
+				</tr></thead>";
+			$j = 0;
 			for ($i=0;$i<count($item_invalid);$i++) {
-				$tr_class = ($i % 2) ? "box_text_odd" : "box_text_even";
-				$content .= "
-					<tr class='".$tr_class."'>
-						<td>".($i+1).".</td>
-						<td>".$item_invalid[$i][0]."</td>
-						<td>".$item_invalid[$i][1]."</td>
-						<td>".$item_invalid[$i][2]."</td>
-					</tr>
-				";
+				if ($item_invalid[$i][0] || $item_invalid[$i][1] || $item_invalid[$i][2]) {
+					$j++;
+					$tr_class = ($j % 2) ? "box_text_odd" : "box_text_even";
+					$content .= "
+						<tr class='".$tr_class."'>
+							<td>&nbsp;".$j.".</td>
+							<td align='center'>".$item_invalid[$i][0]."</td>
+							<td>".$item_invalid[$i][1]."</td>
+							<td align='center'>".$item_invalid[$i][2]."</td>
+						</tr>";
+				}
 			}
-			$content .= "</table>";
+			$content .= "</tbody></table>";
 		}
-		$content .= '<p /><br />';
 		$content .= '<h3>'._('Your choice').': </h3><p />';
 		$content .= "<form action=\"index.php?app=menu&inc=tools_sendfromfile&op=upload_cancel\" method=\"post\">";
 		$content .= "<input type=hidden name=sid value='".$sid."'>";
