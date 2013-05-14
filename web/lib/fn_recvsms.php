fb699ca257104384d751763fedacd0017524f234
Anton Raharja <antonrd@gmail.com>
2013-05-15 00:02:30 +0700

    comment logger_print() on setsmsdeliverystatus(), too noisy

--- web/lib/fn_recvsms.php
+++ web/lib/fn_recvsms.php
@@ -279,11 +279,11 @@ function setsmsdeliverystatus($smslog_id,$uid,$p_status) {
 	// $p_status = 1 --> sent
 	// $p_status = 2 --> failed
 	// $p_status = 3 --> delivered
-	logger_print("smslog_id:".$smslog_id." uid:".$uid." p_status:".$p_status, 2, "setsmsdeliverystatus");
+	//logger_print("smslog_id:".$smslog_id." uid:".$uid." p_status:".$p_status, 2, "setsmsdeliverystatus");
 	$ok = false;
 	$db_query = "UPDATE "._DB_PREF_."_tblSMSOutgoing SET c_timestamp='".mktime()."',p_update='$datetime_now',p_status='$p_status' WHERE smslog_id='$smslog_id' AND uid='$uid'";
 	if ($aff_id = @dba_affected_rows($db_query)) {
-		logger_print("saved smslog_id:".$smslog_id, 2, "setsmsdeliverystatus");
+		//logger_print("saved smslog_id:".$smslog_id, 2, "setsmsdeliverystatus");
 		$ok = true;
 		if ($p_status > 0) {
 			for ($c=0;$c<count($core_config['toolslist']);$c++) {
