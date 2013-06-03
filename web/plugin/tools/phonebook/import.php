ff3eeb1522cc5306572e6521c785e34cb8ebd0a2
Anton Raharja <antonrd@gmail.com>
2013-06-03 21:10:59 +0700

    add $phonebook_row_limit and set it to 20k entries per upload, fix the method to import from CSV file

--- web/plugin/tools/phonebook/import.php
+++ web/plugin/tools/phonebook/import.php
@@ -41,7 +41,9 @@ switch ($op) {
 			$file_content = fread($fp, filesize($fnpb_tmpname));
 			fclose($fp);
 			$parse_phonebook = explode("\n", $file_content);
-			$row_num = ( count($parse_phonebook) <= 100 ? count($parse_phonebook) : 100 );
+			$row_num = ( count($parse_phonebook) <= $phonebook_row_limit ? count($parse_phonebook) : $phonebook_row_limit );
+			$session_import = 'phonebook_'._PID_;
+			unset($_SESSION['tmp'][$session_import]);
 			$j = 0;
 			for ($i = 0; $i < $row_num; $i++) {
 				if (!empty($parse_phonebook) && strlen($parse_phonebook[$i]) > 1) {
@@ -59,11 +61,8 @@ switch ($op) {
 							<td align=center class=$td_class>$parse_param[2]</td>
 							<td align=center class=$td_class>$parse_param[3]</td>
 							</tr>";
-						$phonebook_post .= "
-							<input type=\"hidden\" name=\"data_name$j\" value=\"$parse_param[0]\">
-							<input type=\"hidden\" name=\"data_mobile$j\" value=\"$parse_param[1]\">
-							<input type=\"hidden\" name=\"data_email$j\" value=\"$parse_param[2]\">
-							<input type=\"hidden\" name=\"data_code$j\" value=\"$parse_param[3]\">";
+						$k = $j - 1;
+						$_SESSION['tmp'][$session_import][$k] = $parse_param;
 					}
 					unset($parse_param);
 				}
@@ -72,9 +71,9 @@ switch ($op) {
 				</tbody></table>
 				<p>"._('Import above phonebook entries ?')."
 				<form action=\"index.php?app=menu&inc=tools_phonebook&route=import&op=import_yes\" method=\"post\">
-				<input type=\"submit\" value=\""._('Import')."\" class=\"button\">
-				".$phonebook_post."
-				<input type=\"hidden\" name=\"num\" value=\"$j\">
+				<input type=\"hidden\" name=\"number_of_row\" value=\"$j\">
+				<input type=\"hidden\" name=\"session_import\" value=\"".$session_import."\">
+				<input type=\"submit\" class=\"button\" value=\""._('Import')."\">
 				</form>
 				<p>"._b('index.php?app=menu&inc=tools_phonebook&route=import&op=list');
 			echo $content;
@@ -85,17 +84,19 @@ switch ($op) {
 		}
 		break;
 	case "import_yes":
-		$num = $_POST['num'];
-		for ($i=1; $i<=$num; $i++) {
-			$data_name[$i] = trim($_POST['data_name'.$i]);
-			$data_mobile[$i] = trim($_POST['data_mobile'.$i]);
-			$data_email[$i] = trim($_POST['data_email'.$i]);
-			if ($data_code[$i] = trim($_POST['data_code'.$i])) {
-				$data_gpid[$i] = phonebook_groupcode2id($uid, $data_code[$i]);
+		$num = $_POST['number_of_row'];
+		$session_import = $_POST['session_import'];
+		$data = $_SESSION['tmp'][$session_import];
+		foreach ($data as $d) {
+			$name = trim($d[0]);
+			$mobile = trim($d[1]);
+			$email = trim($d[2]);
+			if ($group_code = trim($d[3])) {
+				$gpid = phonebook_groupcode2id($uid, $group_code);
 			}
-			if ($data_name[$i] && $data_mobile[$i] && $data_gpid[$i]) {
-				dba_remove(_DB_PREF_.'_toolsPhonebook', array('name' => $data_name[$i], 'gpid' => $data_gpid[$i], 'uid' => $uid));
-				dba_add(_DB_PREF_.'_toolsPhonebook', array('uid' => $uid, 'name' => $data_name[$i], 'mobile' => $data_mobile[$i], 'email' => $data_email[$i], 'gpid' => $data_gpid[$i]));
+			if ($name && $mobile && $gpid) {
+				dba_remove(_DB_PREF_.'_toolsPhonebook', array('name' => $name, 'gpid' => $gpid, 'uid' => $uid));
+				dba_add(_DB_PREF_.'_toolsPhonebook', array('uid' => $uid, 'name' => $name, 'mobile' => $mobile, 'email' => $email, 'gpid' => $gpid));
 			}
 		}
 		$_SESSION['error_string'] = _('Contacts has been imported');
