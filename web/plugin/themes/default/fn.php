1320044d2bed6169df62a640be1470fd9a317f3e
Anton Raharja <antonrd@gmail.com>
2013-06-08 14:16:35 +0700

    add useful comments on themes/default/fn.php as hints to build your own menus

--- web/plugin/themes/default/fn.php
+++ web/plugin/themes/default/fn.php
@@ -1,56 +1,63 @@
 <?php
 defined('_SECURE_') or die('Forbidden');
 
-/*
-
-// custom functions for this theme
-function themes_default_set_title($title) {
-}
-
-function themes_default_get_title() {
-}
-
-*/
-
 function default_hook_themes_buildmenu($menu_config) {
 	global $core_config;
 	$content_tree = "";
-	$tree_index = 1;
-	$open = 0;
-	foreach($menu_config as $key=>$value) {
-		if($tree_index==1){$open = 1;}else{$open = 0;};
-		$content_tree .= "\t\t d.add(".$tree_index.",0,'".$key."','','','','','',".$open.");\n";
-		$tree_index++;
-	}
 	$tree_index_top = 1;
-	foreach($menu_config as $key=>$value) {
-		foreach($value as $sub_key1=>$sub_value1) {
-			$menu_title = $sub_value1[1];
-			$menu_url = $sub_value1[0];
-			$content_tree .= "\t\t d.add(".$tree_index.",".$tree_index_top.",'".$menu_title."','".$menu_url."','','');\n";
+	$tree_index = 101;
+	$open = 0;
+
+	/*
+	$menu_config = Array
+	(
+		[My Account] => Array	<--- $menu_title
+		(
+			[0] => Array	<--- $array_menu
+			(
+				[0] => index.php?app=menu&inc=send_sms&op=sendsmstopv	<--- $sub_menu_url
+				[1] => Send SMS						<--- $sub_menu_title
+			) */
+
+	// javascript menu tree (left navigation menu)
+	// d.add(id, pid, name, url, title, target, icon, iconOpen, open)
+
+	// Note: login and then view source, see LEFT NAVIGATION MENU block in the source
+
+	foreach ($menu_config as $menu_title => $array_menu) {
+		$open = ( $tree_index == 1 ? 1 : 0 );
+		$content_tree .= "d.add(".$tree_index_top.",0,'".$menu_title."','','".$menu_title."','_top','','','',".$open.");\n";
+		foreach ($array_menu as $sub_menu) {
+			$sub_menu_url = $sub_menu[0];
+			$sub_menu_title = $sub_menu[1];
+			$content_tree .= "\td.add(".$tree_index.",".$tree_index_top.",'".$sub_menu_title."','".$sub_menu_url."','".$sub_menu_title."','_top','','','');\n";
 			$tree_index++;
 		}
 		$tree_index_top++;
 	}
-	$tree_index_top++;
+
+	$home_url = $core_config['http_path']['base'];
 	$logout_url = "index.php?app=page&op=auth_logout";
-	$content = "
-		<script type=\"text/javascript\">
-		<!--
-		d = new dTree('d');
-		d.add(0,-1,'<b>"._('Home')."</b>','".$core_config['http_path']['base']."','','_top');
-		".$content_tree."
-		d.add(".$tree_index_top.",0,'"._('Logout')."','".$logout_url."','','_top');
-		document.write(d);
-		//-->
-		</script>";
+
+	$content = "\n\n<!-- BEGIN LEFT NAVIGATION MENU -->\n\n";
+	$content .= "<script type=\"text/javascript\">\n";
+	$content .= "<!--\n";
+	$content .= "d = new dTree('d');\n";
+	$content .= "d.add(0,-1,'"._('Home')."','".$home_url."','"._('Home')."','_top','','','');\n";
+	$content .= $content_tree;
+	$content .= "d.add(".$tree_index_top.",0,'"._('Logout')."','".$logout_url."','"._('Logout')."','_top','','','');\n";
+	$content .= "document.write(d);\n";
+	$content .= "//-->\n";
+	$content .= "</script>";
+	$content .= "\n\n<!-- END LEFT NAVIGATION MENU -->\n\n";
+
 	return $content;
 }
 
 function default_hook_themes_navbar($num, $nav, $max_nav, $url, $page) {
 	global $core_config;
 	$nav_pages = "";
-	if($num) {
+	if ($num) {
 		$nav_start = ((($nav-1) * $max_nav)+1);
 		$nav_end = (($nav) * $max_nav);
 		$start = 1;
