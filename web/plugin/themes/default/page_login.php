6a65b90f735714c475bc401d6aeb2b41b7490faa
Anton Raharja <antonrd@gmail.com>
2013-08-05 08:26:56 +0700

    update login page on themes default

--- web/plugin/themes/default/page_login.php
+++ web/plugin/themes/default/page_login.php
@@ -1,37 +1,33 @@
 <?php defined('_SECURE_') or die('Forbidden'); ?>
 <?php include $apps_path['themes']."/".$themes_module."/header.php"; ?>
 
-<TABLE WIDTH="100%" height="100%" BORDER=0 CELLPADDING=0 CELLSPACING=0>
+<table width="100%" height="100%" border=0 cellpadding=0 cellspacing=0>
 	<tbody>
-	<TR>
-		<td align="center" valign="middle" bgcolor="#fcfcfc">
-		<TABLE WIDTH=370 BORDER=0 CELLPADDING=0 CELLSPACING=0>
+	<tr>
+		<td align="center" valign="middle">
+		<table width=470 border=0 cellpadding=0 cellspacing=0 bgcolor=#2C3E50>
 			<tbody>
-                        <TR>
-                                <TD COLSPAN=3><a href="<?php echo $http_path['base']; ?>"><IMG
-                                        SRC="<?php echo $http_path['themes']; ?>/<?php echo $themes_module; ?>/images/default_logo.png"
-                                        border=0></a></TD>
-                        </TR>
-			<TR>
-				<TD COLSPAN=3><a href="<?php echo $http_path['base']; ?>"><IMG
-					SRC="<?php echo $http_path['themes']; ?>/<?php echo $themes_module; ?>/images/login_00.png"
-					WIDTH=370 border=0></a></TD>
-			</TR>
-			<TR>
-				<TD WIDTH=7
-					background="<?php echo $http_path['themes']; ?>/<?php echo $themes_module; ?>/images/login_02.png"><IMG
-					SRC="<?php echo $http_path['themes']; ?>/<?php echo $themes_module; ?>/images/login_02.png"
-					WIDTH=7 HEIGHT=16></TD>
-				<TD WIDTH=348 bgcolor="#FDFCFC"><br />
-				<div align=center><?php echo $error_content?></div>
-				<form action="index.php" method=POST><input type=hidden name=app
-					value=page> <input type=hidden name=inc value=login> <input
-					type=hidden name=op value=auth_login>
+			<tr>
+				<td>
+				<form action="index.php" method=POST>
+				<input type=hidden name=app value=page>
+				<input type=hidden name=inc value=login>
+				<input type=hidden name=op value=auth_login>
 				<table width="100%" border="0" cellpadding="2" cellspacing="2">
 					<tbody>
+					<tr><td>&nbsp;</td></tr>
 					<tr>
-						<td width="140" align="right"><?php echo _('Username'); ?> &nbsp;
+						<td align='center' valign='middle' colspan='2'>
+							<?php echo $web_title?>
 						</td>
+					</tr>
+					<tr>
+						<td align='center' valign='middle' colspan='2'>
+							<?php echo $error_content?>
+						</td>
+					</tr>
+					<tr>
+						<td width="170" align="right"><?php echo _('Username'); ?> &nbsp;</td>
 						<td>&nbsp;<input type=text name=username maxlength=100 size=20></td>
 					</tr>
 					<tr>
@@ -40,8 +36,7 @@
 					</tr>
 					<tr>
 						<td>&nbsp;</td>
-						<td>&nbsp;<input type=submit class=button
-							value="<?php echo _('Login'); ?>"></td>
+						<td>&nbsp;<input type=submit class=button value=<?php echo _('Login'); ?>></td>
 					</tr>
 
 					<?php if ($core_config['main']['cfg_enable_register']) { ?>
@@ -61,25 +56,13 @@
 					</tbody>
 				</table>
 				</form>
-
-				<br />
-
-				</TD>
-				<TD WIDTH=15
-					background="<?php echo $http_path['themes']; ?>/<?php echo $themes_module; ?>/images/login_04.png"><IMG
-					SRC="<?php echo $http_path['themes']; ?>/<?php echo $themes_module; ?>/images/login_04.png"
-					WIDTH=15 HEIGHT=16></TD>
-			</TR>
-			<TR>
-				<TD COLSPAN=3><a href="http://playsms.org"><IMG
-					SRC="<?php echo $http_path['themes']; ?>/<?php echo $themes_module; ?>/images/powered_by_playsms.gif"
-					WIDTH=370 border=0></a></TD>
-			</TR>
-		</tbody>
-		</TABLE>
+				</td>
+			</tr>
+			</tbody>
+		</table>
 		</td>
-	</TR>
+	</tr>
 	</tbody>
-</TABLE>
+</table>
 
-					<?php include $apps_path['themes']."/".$themes_module."/footer.php"; ?>
+<?php include $apps_path['themes']."/".$themes_module."/footer.php"; ?>
