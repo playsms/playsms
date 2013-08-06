20e92d9415998c70bab9a508f2de2a93be507e2c
Anton Raharja <antonrd@gmail.com>
2013-08-06 09:38:51 +0700

    update playsmslog for themes

--- web/plugin/tools/playsmslog/playsmslog.php
+++ web/plugin/tools/playsmslog/playsmslog.php
@@ -11,7 +11,8 @@ switch ($op)
 			<h2>"._('View log')."</h2>
 			<p>"._('Log file').": ".$core_config['apps_path']['logs']."/playsms.log</p>
 			<p>
-			<textarea id=\"playsmslogView\" rows=\"36\" cols=\"98\">".$log."</textarea>
+			<!-- <textarea id=\"playsmslogView\" rows=\"27\" cols=\"70\" readonly=\"yes\" wrap=\"off\">".$log."</textarea> -->
+			<textarea id=\"playsmslogView\" style=\"width: 725px; height: 520px; border: 1px solid #ECF0F1; padding: 5px; font-size: 10pt;\" wrap=off>".$log."</textarea>
 			</p>
 			<script language='javascript' type='text/javascript'>
 				<!--//
