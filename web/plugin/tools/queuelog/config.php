dbddcac9f83780576159418fec9dd6a108ba3967
Anton Raharja <antonrd@gmail.com>
2013-08-07 04:59:08 +0700

    move queuelog from tab tools to my_account

--- web/plugin/tools/queuelog/config.php
+++ web/plugin/tools/queuelog/config.php
@@ -4,7 +4,7 @@ defined('_SECURE_') or die('Forbidden');
 if (isadmin()) {
 	$menutab = $core_config['menutab']['administration'];
 } else {
-	$menutab = $core_config['menutab']['tools'];
+	$menutab = $core_config['menutab']['my_account'];
 }
 $menu_config[$menutab][] = array("index.php?app=menu&inc=tools_queuelog&op=queuelog_list", _('View SMS queue'));
 ?>
