32ebfb7384ec1e1f80bb39c98c5ca604eaf5ffa2
Anton Raharja <antonrd@gmail.com>
2013-08-07 12:13:46 +0700

    use background-image css instead of img src to load profile pic, remove Preferences menu from My Account

--- web/plugin/themes/default/header.php
+++ web/plugin/themes/default/header.php
@@ -25,10 +25,22 @@
 				<tbody>
 				<tr>
 					<td style="background-color: #2C3E50; vertical-align: top; padding: 10px; height: 100px;">
-						<a href="<?php echo $http_path['base']?>"><img style="float: left; padding-right: 10px;" src="<?php echo $core_config['user']['opt']['gravatar']; ?>"></a>
-						<div><a href="<?php echo $http_path['base']?>"><?php echo $name; ?></a></div>
-						<div><a href="<?php echo $http_path['base']?>"><?php echo $username; ?></a></div>
-						<div style='padding-top: 1em;'><a href="index.php?app=page&op=auth_logout"><?php echo _('Logout'); ?></a></div>
+						<style type='text/css'>
+							#profile_pic {
+								background-image:url('<?php echo $core_config['user']['opt']['gravatar']; ?>');
+								background-color: #2980B9;
+								padding-right: 10px;
+								width: 80px;
+								height: 80px;
+								border-bottom: 1px solid rgba(0,0,0,0.3);
+								border-top: 1px solid rgba(255,255,255,0.2);
+								cursor: pointer;
+							}
+						</style>
+						<div id=profile_pic onClick="window.location.href='<?php echo $http_path['base']?>'"/>
+						<div style='padding: 0 0 0 100px; width: 170px'><a href="index.php?app=menu&inc=user_pref&op=user_pref"><?php echo $name; ?></a></div>
+						<div style='padding: 0 0 0 100px; width: 170px'><a href="index.php?app=menu&inc=user_pref&op=user_pref"><?php echo $username; ?></a></div>
+						<div style='padding: 22px 0 0 100px; width: 170px'><a href="index.php?app=page&op=auth_logout"><?php echo _('Logout'); ?></a></div>
 					</td>
 				</tr>
 				<tr>
