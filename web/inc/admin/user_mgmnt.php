9eaa408fe78c9b3fd2f72c7020f8fb809cc42f7b
Anton Raharja <antonrd@gmail.com>
2013-08-19 13:23:02 +0700

    add edit config icon

--- web/inc/admin/user_mgmnt.php
+++ web/inc/admin/user_mgmnt.php
@@ -47,6 +47,7 @@ switch ($op) {
 			$j--;
 			$td_class = ($j % 2) ? "row_odd" : "row_even";
 			$action = "<a href=\"index.php?app=menu&inc=user_pref&op=user_pref&uname=" . $list[$i]['username'] . "\">$icon_edit</a>";
+			$action .= "<a href=\"index.php?app=menu&inc=user_config&op=user_config&uname=" . $list[$i]['username'] . "\">$icon_edit</a>";
 			$action .= "<a href=\"javascript: ConfirmURL('" . _('Are you sure you want to delete user ?') . " (" . _('username') . ": " . $list[$i]['username'] . ")','index.php?app=menu&inc=user_mgmnt&op=user_del&uname=" . $list[$i]['username'] . "')\">$icon_delete</a>";
 			$content .= "
 				<tr>
@@ -104,6 +105,7 @@ switch ($op) {
 			$j--;
 			$td_class = ($j % 2) ? "row_odd" : "row_even";
 			$action = "<a href=\"index.php?app=menu&inc=user_pref&op=user_pref&uname=" . $list[$i]['username'] . "\">$icon_edit</a>";
+			$action .= "<a href=\"index.php?app=menu&inc=user_config&op=user_config&uname=" . $list[$i]['username'] . "\">$icon_edit</a>";
 			$action .= "<a href=\"javascript: ConfirmURL('" . addslashes(_("Are you sure you want to delete user")) . " " . $list[$i]['username'] . " ?','index.php?app=menu&inc=user_mgmnt&op=user_del&uname=" . $list[$i]['username'] . "')\">$icon_delete</a>";
 			$content .= "
 				<tr>
