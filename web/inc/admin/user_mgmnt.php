d8c443901b9f775d8fd372438d595690a25ef50a
Anton Raharja <antonrd@gmail.com>
2013-08-21 11:24:28 +0700

    update user_mgmnt icons, add new icon user_delete

--- web/inc/admin/user_mgmnt.php
+++ web/inc/admin/user_mgmnt.php
@@ -106,7 +106,7 @@ switch ($op) {
 			$td_class = ($j % 2) ? "row_odd" : "row_even";
 			$action = "<a href=\"index.php?app=menu&inc=user_pref&op=user_pref&uname=" . $list[$i]['username'] . "\">".$core_config['icon']['user_pref']."</a>";
 			$action .= "<a href=\"index.php?app=menu&inc=user_config&op=user_config&uname=" . $list[$i]['username'] . "\">".$core_config['icon']['user_config']."</a>";
-			$action .= "<a href=\"javascript: ConfirmURL('" . addslashes(_("Are you sure you want to delete user")) . " " . $list[$i]['username'] . " ?','index.php?app=menu&inc=user_mgmnt&op=user_del&uname=" . $list[$i]['username'] . "')\">".$core_config['icon']['delete']."</a>";
+			$action .= "<a href=\"javascript: ConfirmURL('" . addslashes(_("Are you sure you want to delete user")) . " " . $list[$i]['username'] . " ?','index.php?app=menu&inc=user_mgmnt&op=user_del&uname=" . $list[$i]['username'] . "')\">".$core_config['icon']['user_delete']."</a>";
 			$content .= "
 				<tr>
 					<td class='$td_class' valign=top align=center>" . core_display_datetime($list[$i]['register_datetime']) . "</td>
