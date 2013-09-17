66fcf221bf5d608777bbf234d1c51909ab4f559e
Anton Raharja <antonrd@gmail.com>
2013-09-17 18:16:02 +0700

    move setsmsdeliverystatus() to new fn file fn_dlr.php

--- web/lib/fn_recvsms.php
+++ web/lib/fn_recvsms.php
@@ -310,30 +310,4 @@ function insertsmstoinbox($sms_datetime,$sms_sender,$target_user,$message,$sms_r
 	return $ok;
 }
 
-function setsmsdeliverystatus($smslog_id,$uid,$p_status) {
-	global $core_config;
-	// $p_status = 0 --> pending
-	// $p_status = 1 --> sent
-	// $p_status = 2 --> failed
-	// $p_status = 3 --> delivered
-	//logger_print("smslog_id:".$smslog_id." uid:".$uid." p_status:".$p_status, 2, "setsmsdeliverystatus");
-	$ok = false;
-	$db_query = "UPDATE "._DB_PREF_."_tblSMSOutgoing SET c_timestamp='".mktime()."',p_update='".core_get_datetime()."',p_status='$p_status' WHERE smslog_id='$smslog_id' AND uid='$uid'";
-	if ($aff_id = @dba_affected_rows($db_query)) {
-		//logger_print("saved smslog_id:".$smslog_id, 2, "setsmsdeliverystatus");
-		$ok = true;
-		if ($p_status > 0) {
-			for ($c=0;$c<count($core_config['toolslist']);$c++) {
-				x_hook($core_config['toolslist'][$c],'setsmsdeliverystatus',array($smslog_id,$uid,$p_status));
-			}
-			for ($c=0;$c<count($core_config['featurelist']);$c++) {
-				x_hook($core_config['featurelist'][$c],'setsmsdeliverystatus',array($smslog_id,$uid,$p_status));
-			}
-			$gw = gateway_get();
-			x_hook($gw,'setsmsdeliverystatus',array($smslog_id,$uid,$p_status));
-		}
-	}
-	return $ok;
-}
-
 ?>
--- web/lib/function.php
+++ web/lib/function.php
@@ -5,6 +5,7 @@ defined('_SECURE_') or die('Forbidden');
 include $apps_path['libs']."/fn_rate.php";
 include $apps_path['libs']."/fn_billing.php";
 include $apps_path['libs']."/fn_gateway.php";
+include $apps_path['libs']."/fn_dlr.php";
 include $apps_path['libs']."/fn_recvsms.php";
 include $apps_path['libs']."/fn_sendsms.php";
 include $apps_path['libs']."/fn_phonebook.php";
