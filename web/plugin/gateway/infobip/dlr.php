7ecf6a524bfa30dc0b8c6fe224534f525ea5aaaf
enumanc <enumanc@gmail.com>
2013-09-21 09:30:45 +1100

    Add new plugin gateway infobip (infobip.com)

---
+++ web/plugin/gateway/infobip/dlr.php
@@ -0,0 +1,80 @@
+<?php
+if (! $called_from_hook_call) {
+	chdir ("../../../");
+	include "init.php";
+	include $apps_path['libs']."/function.php";
+	chdir ("plugin/gateway/infobip");
+	$requests = $_REQUEST;
+}
+
+$remote_addr = $_SERVER['REMOTE_ADDR'];
+// srosa 20100531: added var below
+$remote_host = $_SERVER['HTTP_HOST'];
+// srosa 20100531: changed test below to allow hostname in bearerbox_host instead of ip
+// if ($remote_addr != $kannel_param['bearerbox_host'])
+//if ($remote_addr != $kannel_param['bearerbox_host'] && $remote_host != $kannel_param['bearerbox_host']) {
+//	logger_print("exit remote_addr:".$remote_addr." remote_host:".$remote_host." bearerbox_host:".$kannel_param['bearerbox_host'], 2, "kannel dlr");
+//	exit();
+//}
+
+$xml = file_get_contents('php://input');
+//file_put_contents('toto.txt', $xml, true);
+logger_print("dlr request: ".$xml, 3, "infobip debug");
+
+preg_match_all('/id=\"([0-9]+)\"/', $xml, $result);
+$apimsgid = $result[1][0];
+logger_print("apimsgid: ".$apimsgid, 3, "infobip debug");
+
+if ( preg_match_all('/status=\"([A-Z]+)\"/', $xml, $result) ) {
+    $status = $result[1][0];
+}
+
+$db_query = "SELECT smslog_id FROM "._DB_PREF_."_gatewayInfobip_apidata WHERE apimsgid='$apimsgid'";
+$db_result = dba_query($db_query);
+$db_row = dba_fetch_array($db_result);
+$slid = $db_row['smslog_id'];
+
+$db_query = "SELECT uid FROM "._DB_PREF_."_tblSMSOutgoing WHERE smslog_id='$slid'";
+$db_result = dba_query($db_query);
+$db_row = dba_fetch_array($db_result);
+$uid = $db_row['uid'];
+
+logger_print("addr:".$remote_addr." host:".$remote_host." type:".$status." slid:".$slid." uid:".$uid, 2, "infobip dlr");
+
+if ($status && $slid && $uid) {
+	switch ($status) {
+       		case "DELIVERED":               $p_status = 3; break; // delivered
+       		case "NOT_DELIVERED":           $p_status = 2; break; // failed
+       		case "NOT_ENOUGH_CREDITS":      $p_status = 2; break; // failed
+    	}
+
+	setsmsdeliverystatus($slid,$uid,$p_status);
+
+	// log dlr
+	//$db_query = "SELECT apimsgid FROM "._DB_PREF_."_gatewayInfobip_apidata WHERE smslog_id='$slid'";
+	//$db_result = dba_num_rows($db_query);
+	//if ($db_result > 0) {
+		$db_query = "UPDATE "._DB_PREF_."_gatewayInfobip_apidata SET c_timestamp='".mktime()."', status='$status' WHERE smslog_id='$slid'";
+		$db_result = dba_query($db_query);
+	//} else {
+	//	$db_query = "INSERT INTO "._DB_PREF_."_gatewayKannel_dlr (smslog_id,kannel_dlr_type) VALUES ('$slid','$type')";
+	//	$db_result = dba_query($db_query);
+	//}
+}
+
+function xml2array2($xml) {
+  $arr = array();
+  foreach ($xml as $element) {
+    $tag = $element->getName();
+    $e = get_object_vars($element); 
+    if (!empty($e)) {   
+      $arr[$tag] = $element instanceof SimpleXMLElement ? xml2array($element) : $e;
+    }                           
+    else {                      
+      $arr[$tag] = trim($element); 
+    }                           
+  }                             
+  return $arr;                         
+}
+
+?>
