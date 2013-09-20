61d73554b374c07dbeb8e9e79b616d4d19307c6d
Anton Raharja <antonrd@gmail.com>
2013-09-20 09:49:37 +0700

    update PLAYSMS_BIN path to daemon locations

--- daemon/windows/bin/_playsms/playsmsd.bat
+++ daemon/windows/bin/_playsms/playsmsd.bat
@@ -3,6 +3,6 @@
 call C:\playsms\etc\playsms.bat
 
 :while1
-	%PHP_CLI% %PLAYSMS_BIN%\playsmsd.php %PLAYSMS_PATH%
+	%PHP_CLI% %PLAYSMS_BIN%\_playsms\playsmsd.php %PLAYSMS_PATH%
 	timeout %REFRESH_RATE%
 goto :while1
--- daemon/windows/bin/_playsms/sendsmsd.bat
+++ daemon/windows/bin/_playsms/sendsmsd.bat
@@ -3,6 +3,6 @@
 call C:\playsms\etc\playsms.bat
 
 :while1
-	%PHP_CLI% %PLAYSMS_BIN%\sendsmsd.php %PLAYSMS_PATH%
+	%PHP_CLI% %PLAYSMS_BIN%\_playsms\sendsmsd.php %PLAYSMS_PATH%
 	timeout %REFRESH_RATE%
 goto :while1
