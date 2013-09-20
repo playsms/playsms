c6227b47ca3077c7cf6f69ea192add609c03a41d
Anton Raharja <antonrd@gmail.com>
2013-09-20 09:30:31 +0700

    move daemon files locations

--- daemon/windows/bin/playsmsd_start.bat
+++ daemon/windows/bin/playsmsd_start.bat
@@ -2,8 +2,8 @@
 
 call C:\playsms\etc\playsms.bat
 
-start "playsmsd" %PLAYSMS_BIN%\playsmsd
-start "recvsmsd" %PLAYSMS_BIN%\sendsmsd
-start "sendsmsd" %PLAYSMS_BIN%\sendsmsd
-start "dlrssmsd" %PLAYSMS_BIN%\sendsmsd
+start "playsmsd" %PLAYSMS_BIN%\_playsms\playsmsd
+start "recvsmsd" %PLAYSMS_BIN%\_playsms\sendsmsd
+start "sendsmsd" %PLAYSMS_BIN%\_playsms\sendsmsd
+start "dlrssmsd" %PLAYSMS_BIN%\_playsms\sendsmsd
 
