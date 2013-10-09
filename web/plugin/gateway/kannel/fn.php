c2acba37168663e7e2b5f8c2bca1b4c715545c96
Anton Raharja <antonrd@gmail.com>
2013-10-09 23:37:59 +0700

    add more logger_print()

--- web/plugin/gateway/kannel/fn.php
+++ web/plugin/gateway/kannel/fn.php
@@ -4,6 +4,7 @@ defined('_SECURE_') or die('Forbidden');
 function kannel_hook_sendsms($sms_sender,$sms_footer,$sms_to,$sms_msg,$uid='',$gpid=0,$smslog_id=0,$sms_type='text',$unicode=0) {
 	global $kannel_param;
 	global $http_path;
+	logger_print("start smslog_id:".$smslog_id." uid:".$uid." to:".$sms_to, 2, "kannel outgoing");
 	$sms_sender = stripslashes($sms_sender);
 	$sms_footer = stripslashes($sms_footer);
 	$sms_msg = stripslashes($sms_msg);
@@ -109,6 +110,7 @@ function kannel_hook_sendsms($sms_sender,$sms_footer,$sms_to,$sms_msg,$uid='',$g
 		$p_status = 2;
 	}
 	dlr($smslog_id,$uid,$p_status);
+	logger_print("end smslog_id:".$smslog_id." p_status:".$p_status, 2, "kannel outgoing");
 	// good or bad, print it on the log
 	return $ok;
 }
