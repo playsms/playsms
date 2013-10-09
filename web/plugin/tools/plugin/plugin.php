776259c35b00b1ce463cb7c725edcd80cd8bf254
Anton Raharja <antonrd@gmail.com>
2013-10-09 01:25:52 +0000

    cleanup new plugin tools, plugin

--- web/plugin/tools/plugin/plugin.php
+++ web/plugin/tools/plugin/plugin.php
@@ -1,51 +1,47 @@
 <?php
-$content = "<link href='".$http_path['plug']."/tools/plugin/css/style.css' rel='stylesheet'>";
-$content .= "<script src='".$http_path['plug']."/tools/plugin/js/jquery.js' type='text/javascript'></script>
-	<script src='".$http_path['plug']."/tools/plugin/js/jquery.hashchange.js' type='text/javascript'></script>
-	<script src='".$http_path['plug']."/tools/plugin/js/jquery.easytabs.js' type='text/javascript'></script>
+
+defined('_SECURE_') or die('Forbidden');
+
+if (!isadmin()) {
+	forcenoaccess();
+};
+
+$content = "
+	<link href='" . $http_path['plug'] . "/tools/plugin/css/style.css' rel='stylesheet' />
+	<script src='" . $http_path['plug'] . "/tools/plugin/js/jquery.js' type='text/javascript'></script>
+	<script src='" . $http_path['plug'] . "/tools/plugin/js/jquery.hashchange.js' type='text/javascript'></script>
+	<script src='" . $http_path['plug'] . "/tools/plugin/js/jquery.easytabs.js' type='text/javascript'></script>
 	<script type='text/javascript'>
-	    $(document).ready( function() {
-	      $('#tab-container').easytabs();
-	    });
-	  </script>
-";
+		$(document).ready( function() {
+		$('#tab-container').easytabs();
+	});
+	</script>";
 
 $content .= "
-<div id='tab-container' class='tab-container'>
-  <ul class='etabs'>
-    <li class='tab'><a href='#tabs-feature'>"._('Features')."</a></li>
-    <li class='tab'><a href='#tabs-gateway'>"._('Gateways')."</a></li>
-    <li class='tab'><a href='#tabs-theme'>"._('Themes')."</a></li>
-    <li class='tab'><a href='#tabs-tool'>"._('Tools')."</a></li>
-    <li class='tab'><a href='#tabs-lang'>"._('Languages')."</a></li>
-  </ul>
-
-  <div id='tabs-feature'>";
-    $content .= plugin_table('feature');
-    $content .= "<br />";
-  $content .= "</div>
-
-  <div id='tabs-gateway'>";
-    $content .= plugin_table('gateway');
-    $content .= "<br />";
-  $content .= "</div>
-
-  <div id='tabs-theme'>";
-    $content .= plugin_table('themes');
-    $content .= "<br />";
-  $content .= "</div>
-
-  <div id='tabs-tool'>";
-    $content .= plugin_table('tools');
-    $content .= "<br />";
-  $content .= "</div>
-
-  <div id='tabs-lang'>";
-    $content .= plugin_table('language');
-    $content .= "<br />";
-  $content .= "</div>
-
-</div>";
+	<div id='tab-container' class='tab-container'>
+		<ul class='etabs'>
+			<li class='tab'><a href='#tabs-feature'>" . _('Features') . "</a></li>
+			<li class='tab'><a href='#tabs-gateway'>" . _('Gateways') . "</a></li>
+			<li class='tab'><a href='#tabs-theme'>" . _('Themes') . "</a></li>
+			<li class='tab'><a href='#tabs-tool'>" . _('Tools') . "</a></li>
+			<li class='tab'><a href='#tabs-lang'>" . _('Languages') . "</a></li>
+		</ul>
+	<div id='tabs-feature'>
+		".plugin_table('feature')."<br />
+	</div>
+	<div id='tabs-gateway'>
+		".plugin_table('gateway')."<br />
+	</div>
+	<div id='tabs-theme'>
+		".plugin_table('themes')."<br />
+	</div>
+	<div id='tabs-tool'>
+		".plugin_table('tools')."<br />
+	</div>
+	<div id='tabs-lang'>
+		".plugin_table('language')."<br />
+	</div>";
 
 echo $content;
+
 ?>
