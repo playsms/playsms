80d331ee9a007d6a2361c3fa81485192ee062a8e
Anton Raharja <antonrd@gmail.com>
2013-10-10 16:24:07 +0700

    cleanup coding styles (using Eclipse format)

--- web/plugin/tools/report/config.php
+++ web/plugin/tools/report/config.php
@@ -1,10 +1,16 @@
 <?php
-if (isadmin()) {
-	$menutab = $core_config['menutab']['administration'];
-	$menu_config[$menutab][] = array("index.php?app=menu&inc=tools_report&op=report_admin", _('All reports'));
+if (isadmin ()) {
+	$menutab = $core_config ['menutab'] ['administration'];
+	$menu_config [$menutab] [] = array (
+			"index.php?app=menu&inc=tools_report&op=report_admin",
+			_ ( 'All reports' ) 
+	);
 }
-	$menutab = $core_config['menutab']['my_account'];
-	$menu_config[$menutab][] = array("index.php?app=menu&inc=tools_report&op=report_user", _('My report'));
+$menutab = $core_config ['menutab'] ['my_account'];
+$menu_config [$menutab] [] = array (
+		"index.php?app=menu&inc=tools_report&op=report_user",
+		_ ( 'My report' ) 
+);
 
-$simplestat_icon_resent = "<img src=\"".$http_path['themes']."/".$themes_module."/images/unpublicphonebook.gif\" alt=\""._('Reicycle')."\" title=\""._('Recycle')."\" border=0>";
+$simplestat_icon_resent = "<img src=\"" . $http_path ['themes'] . "/" . $themes_module . "/images/unpublicphonebook.gif\" alt=\"" . _ ( 'Reicycle' ) . "\" title=\"" . _ ( 'Recycle' ) . "\" border=0>";
 ?>
