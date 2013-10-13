01b5061bc18685d7a87ae2ca1e6f12da1ca86c6f
Anton Raharja <antonrd@gmail.com>
2013-10-13 09:19:50 +0700

    expand tpl_apply() to check first from plugin, then from themes and finally from common place in themes

--- web/lib/fn_tpl.php
+++ web/lib/fn_tpl.php
@@ -11,16 +11,32 @@ function tpl_apply_core($fn, $tpl) {
 }
 
 function tpl_apply($tpl_name, $tpl) {
-	$themes = themes_get();
-	$fn = _APPS_PATH_THEMES_.'/'.$themes.'/templates/'.q_sanitize($tpl_name).'.tpl';
+	$tpl_name = q_sanitize($tpl_name);
+
+	// check from active plugin
+	$inc = explode('_', _INC_);
+	$plugin_category = $inc[0];
+	$plugin_name = str_replace($plugin_category.'_', '', _INC_);
+	$fn = _APPS_PATH_PLUG_.'/'.$plugin_category.'/'.$plugin_name.'/templates/'.$tpl_name.'.tpl';
 	if (file_exists($fn)) {
 		$content = tpl_apply_core($fn, $tpl);
 		return $content;
 	}
+
+	// check from active template
+	$themes = themes_get();
+	$fn = _APPS_PATH_THEMES_.'/'.$themes.'/templates/'.$tpl_name.'.tpl';
+	if (file_exists($fn)) {
+		$content = tpl_apply_core($fn, $tpl);
+		return $content;
+	}
+
+	// check from common place on themes
 	$fn = _APPS_PATH_TPL_.'/'.$tpl_name .'.tpl';
 	if (file_exists($fn)) {
 		$content = tpl_apply_core($fn, $tpl);
 	}
+
 	return $content;
 }
 
