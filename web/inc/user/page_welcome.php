29b5e0792d276ae666e6469b4cdf9fe7260b7904
Anton Raharja <antonrd@gmail.com>
2013-11-11 17:08:11 +0700

    allow override page_welcome.php by placing a replacement in plugin/themes/<active themes>/

--- web/inc/user/page_welcome.php
+++ web/inc/user/page_welcome.php
@@ -2,24 +2,29 @@
 defined('_SECURE_') or die('Forbidden');
 if(!valid()){forcenoaccess();};
 
-unset($tpl);
-$tpl = array(
-    'name' => 'page_welcome',
-    'var' => array(
-	'HTTP_PATH_THEMES' => _HTTP_PATH_THEMES_,
-	'Welcome to playSMS' => _('Welcome to playSMS'),
-	'About playSMS' => _('About playSMS'),
-	'Changelog' => _('Changelog'),
-	'F.A.Q' => _('F.A.Q'),
-	'License' => _('License'),
-	'Webservices' => _('Webservices'),
-	'READ_README' => core_read_docs($apps_path['base'], 'README'),
-	'READ_CHANGELOG' => core_read_docs($apps_path['base'], 'CHANGELOG'),
-	'READ_FAQ' => core_read_docs($apps_path['base'], 'FAQ'),
-	'READ_LICENSE' => core_read_docs($apps_path['base'], 'LICENSE'),
-	'READ_WEBSERVICES' => core_read_docs($apps_path['base'], 'WEBSERVICES'),	
-    )    
-);
-echo tpl_apply($tpl);
+$fn = _APPS_PATH_THEMES_.'/'.themes_get().'/page_welcome.php';
 
+if (file_exists($fn)) {
+	include $fn;
+} else {
+	unset($tpl);
+	$tpl = array(
+		'name' => 'page_welcome',
+		'var' => array(
+			'HTTP_PATH_THEMES' => _HTTP_PATH_THEMES_,
+			'Welcome to playSMS' => _('Welcome to playSMS'),
+			'About playSMS' => _('About playSMS'),
+			'Changelog' => _('Changelog'),
+			'F.A.Q' => _('F.A.Q'),
+			'License' => _('License'),
+			'Webservices' => _('Webservices'),
+			'READ_README' => core_read_docs($apps_path['base'], 'README'),
+			'READ_CHANGELOG' => core_read_docs($apps_path['base'], 'CHANGELOG'),
+			'READ_FAQ' => core_read_docs($apps_path['base'], 'FAQ'),
+			'READ_LICENSE' => core_read_docs($apps_path['base'], 'LICENSE'),
+			'READ_WEBSERVICES' => core_read_docs($apps_path['base'], 'WEBSERVICES'),	
+		)
+	);
+	echo tpl_apply($tpl);
+}
 ?>
