f8b82e774a2de47e955858b79be22902832b048b
Anton Raharja <antonrd@gmail.com>
2013-11-14 08:08:00 +0700

    changes on sms to email

--- web/lib/fn_recvsms.php
+++ web/lib/fn_recvsms.php
@@ -263,7 +263,7 @@ function insertsmstoinbox($sms_datetime,$sms_sender,$target_user,$message,$sms_r
 		if ($uid = $user['uid']) {
 			// get name from target_user's phonebook
 			$c_name = phonebook_number2name($sms_sender, $target_user);
-			$sender = $c_name ? $c_name.' <'.$sms_sender.'>' : $sms_sender;
+			$sender = $c_name ? $c_name.' ('.$sms_sender.')' : $sms_sender;
 
 			// forward to Inbox
 			if ($fwd_to_inbox = $user['fwd_to_inbox']) {
@@ -282,11 +282,12 @@ function insertsmstoinbox($sms_datetime,$sms_sender,$target_user,$message,$sms_r
 				if ($email = $user['email']) {
 					// make sure sms_datetime is in supported format and in user's timezone
 					$sms_datetime = core_display_datetime($sms_datetime);
-					$subject = _('Forward from')." ".$sms_sender;
-					$body = _('Incoming SMS to email')." ".$web_title."\n\n";
-					$body .= _('Date time').": ".$sms_datetime."\n";
-					$body .= _('Sender').": ".$sender."\n";
-					$body .= _('Receiver').": ".$sms_receiver."\n\n";
+					$subject = _('SMS from')." ".$sms_sender;
+					$body = $web_title."\n\n";
+					$body .= _('SMS to email')."\n\n";
+					$body .= _('Received').": ".$sms_datetime."\n";
+					$body .= _('Receiver').": ".$sms_receiver."\n";
+					$body .= _('Sender').": ".$sender."\n\n";
 					$body .= _('Message').":\n".$message."\n\n";
 					$body .= $email_footer."\n\n";
 					$body = stripslashes($body);
