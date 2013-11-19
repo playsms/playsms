196566ecacb0ffd1590e5bc632e071da210e6072
Anton Raharja <antonrd@gmail.com>
2013-11-19 18:25:46 +0700

    add logger_print to smssync sync

--- web/plugin/tools/smssync/sync.php
+++ web/plugin/tools/smssync/sync.php
@@ -25,11 +25,13 @@ if ($smssync_enable && ($r['secret'] == $smssync_secret) && $message_id) {
 	$db_table = _DB_PREF_.'_toolsSmssysnc';
 	$conditions = array('message_id' => $message_id);
 	if (dba_isavail($db_table, $conditions)) {
-		logger_print("dt:".$sms_datetime." ts:".$r['sent_timestamp']." message_id:".$message_id." s:".$sms_sender." m:".$message." r:".$sms_receiver, 3, "smssync sync");
+		logger_print("saving dt:".$sms_datetime." ts:".$r['sent_timestamp']." message_id:".$message_id." s:".$sms_sender." m:".$message." r:".$sms_receiver, 3, "smssync sync");
 		if ($recvsms_id = recvsms($sms_datetime, $sms_sender, $message, $sms_receiver)) {
 			$items = array('message_id' => $message_id, 'recvsms_id' => $recvsms_id);
 			dba_add($db_table, $items);
 			logger_print("saved message_id:".$message_id." recvsms_id:".$recvsms_id, 3, "smssync sync");
+		} else {
+			logger_print("fail to save message_id:".$message_id, 3, "smssync sync");
 		}
 	}
 }
