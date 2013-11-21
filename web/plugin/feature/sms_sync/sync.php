c7ed7e773560c6938bf55e43c6f4f8454551adf7
Anton Raharja <antonrd@gmail.com>
2013-11-21 10:33:13 +0700

    respond only when sms sync enabled and secret key matched

--- web/plugin/feature/sms_sync/sync.php
+++ web/plugin/feature/sms_sync/sync.php
@@ -28,11 +28,11 @@ if ($sms_sync_enable && $c_uid && ($r['secret'] == $sms_sync_secret) && $message
 	$db_table = _DB_PREF_.'_featureSmssysnc';
 	$conditions = array('uid' => $c_uid, 'message_id' => $message_id);
 	if (dba_isavail($db_table, $conditions)) {
-		logger_print("saving dt:".$sms_datetime." uid:".$c_uid." ts:".$r['sent_timestamp']." message_id:".$message_id." s:".$sms_sender." m:".$message." r:".$sms_receiver, 3, "sms_sync sync");
+		logger_print("saving uid:".$c_uid." dt:".$sms_datetime." ts:".$r['sent_timestamp']." message_id:".$message_id." s:".$sms_sender." m:".$message." r:".$sms_receiver, 3, "sms_sync sync");
 		if ($recvsms_id = recvsms($sms_datetime, $sms_sender, $message, $sms_receiver)) {
 			$items = array('message_id' => $message_id, 'recvsms_id' => $recvsms_id);
 			dba_add($db_table, $items);
-			logger_print("saved message_id:".$message_id." recvsms_id:".$recvsms_id, 3, "sms_sync sync");
+			logger_print("saved uid:".$c_uid." message_id:".$message_id." recvsms_id:".$recvsms_id, 3, "sms_sync sync");
 			$ret = array(
 				'payload' => array(
 					'success' => true,
@@ -41,20 +41,22 @@ if ($sms_sync_enable && $c_uid && ($r['secret'] == $sms_sync_secret) && $message
 			);
 			$ok = TRUE;
 		} else {
-			logger_print("fail to save message_id:".$message_id, 3, "sms_sync sync");
+			$error_string = "fail to save uid:".$c_uid." message_id:".$message_id;
+			logger_print($error_string, 3, "sms_sync sync");
 		}
+	} else {
+		$error_string = "duplicate message uid:".$c_uid." message_id:".$message_id;
+		logger_print($error_string, 3, "sms_sync sync");
 	}
+	if (! $ok) {
+		$ret = array(
+			'payload' => array(
+				'success' => "false",
+				'error' => $error_string
+			)
+		);
+	}
+	echo json_encode($ret);
 }
-
-if (! $ok) {
-	$ret = array(
-		'payload' => array(
-			'success' => "false",
-			'error' => "Unable to process"
-		)
-	);
-}
-
-echo json_encode($ret);
 
 ?>
