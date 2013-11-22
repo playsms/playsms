71f896511a18a060de7fd6ce2f57088ffd15cc49
Anton Raharja <antonrd@gmail.com>
2013-11-22 13:27:35 +0700

    old default renamed to 'azel'

---
+++ web/plugin/themes/azel/init.php
@@ -0,0 +1,6 @@
+<?php
+defined('_SECURE_') or die('Forbidden');
+
+include $apps_path['themes']."/".$themes_module."/config.php";
+include $apps_path['themes']."/".$themes_module."/fn.php";
+?>
---
+++ web/plugin/themes/azel/page_login.php
@@ -0,0 +1,27 @@
+<?php
+defined('_SECURE_') or die('Forbidden');
+
+unset($tpl);
+$tpl = array(
+	'name' => 'page_login',
+	'var' => array(
+		'HTTP_PATH_BASE' => $http_path['base'],
+		'WEB_TITLE' => $web_title,
+		'ERROR' => $error_content,
+		'Username' => _('Username'),
+		'Password' => _('Password'),
+		'Login' => _('Login'),
+		'Register an account' => _('Register an account'),
+		'Forgot password' => _('Forgot password')
+	),
+	'if' => array(
+		'enable_register' => $core_config['main']['cfg_enable_register'],
+		'enable_forgot' => $core_config['main']['cfg_enable_forgot']
+	)
+);
+
+$content = tpl_apply($tpl);
+
+echo themes_apply($content);
+
+?>
