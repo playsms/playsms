e1e57c8ea105ee7ccf85eb18b1e5b6eed356f570
Anton Raharja <antonrd@gmail.com>
2013-11-24 21:32:38 +0700

    update pluginmanager, remove js easytab

--- web/plugin/tools/pluginmanager/pluginmanager.php
+++ web/plugin/tools/pluginmanager/pluginmanager.php
@@ -3,41 +3,31 @@ defined ( '_SECURE_' ) or die ( 'Forbidden' );
 
 if (!isadmin()) { forcenoaccess (); };
 
-$content = "
-	<script src='" . _HTTP_PATH_THEMES_ . "/common/jscss/jquery.easytabs.min.js' type='text/javascript'></script>
-	<script type='text/javascript'>
-		$(document).ready( function() {
-		$('#tab-container').easytabs();
-	});
-	</script>";
-
 $content .= "
 	<h2>" . _ ( 'Manage plugin' ) . "</h2>
-	<div id='tab-container' class='tab-container'>
-		<ul class='nav nav-pills nav-justified'>
-			<li><a href='#tabs-feature'>" . _ ( 'Features' ) . "</a></li>
-			<li><a href='#tabs-gateway'>" . _ ( 'Gateways' ) . "</a></li>
-			<li><a href='#tabs-theme'>" . _ ( 'Themes' ) . "</a></li>
-			<li><a href='#tabs-tools'>" . _ ( 'Tools' ) . "</a></li>
-			<li><a href='#tabs-language'>" . _ ( 'Languages' ) . "</a></li>
-		</ul>
-		<div id='tabs-feature'>
-			" . pluginmanager_display ( 'feature' ) . "<br />
-		</div>
-		<div id='tabs-gateway'>
-			" . pluginmanager_display ( 'gateway' ) . "<br />
-		</div>
-		<div id='tabs-theme'>
-			" . pluginmanager_display ( 'themes' ) . "<br />
-		</div>
-		<div id='tabs-tools'>
-			" . pluginmanager_display ( 'tools' ) . "<br />
-		</div>
-		<div id='tabs-language'>
-			" . pluginmanager_display ( 'language' ) . "<br />
-		</div>
-	</div>";
-
+	<ul class='nav nav-pills nav-justified'>
+		<li><a href='#tabs-feature'>" . _ ( 'Features' ) . "</a></li>
+		<li><a href='#tabs-gateway'>" . _ ( 'Gateways' ) . "</a></li>
+		<li><a href='#tabs-theme'>" . _ ( 'Themes' ) . "</a></li>
+		<li><a href='#tabs-tools'>" . _ ( 'Tools' ) . "</a></li>
+		<li><a href='#tabs-language'>" . _ ( 'Languages' ) . "</a></li>
+	</ul>
+	<div id='tabs-feature'>
+		" . pluginmanager_display ( 'feature' ) . "
+	</div>
+	<div id='tabs-gateway'>
+		" . pluginmanager_display ( 'gateway' ) . "
+	</div>
+	<div id='tabs-theme'>
+		" . pluginmanager_display ( 'themes' ) . "
+	</div>
+	<div id='tabs-tools'>
+		" . pluginmanager_display ( 'tools' ) . "
+	</div>
+	<div id='tabs-language'>
+		" . pluginmanager_display ( 'language' ) . "
+	</div>
+	";
 echo $content;
 
 ?>
