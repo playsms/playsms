cb63cdd10a0284a8faaaf620305caa6a0782e7bf
Anton Raharja <antonrd@gmail.com>
2013-12-03 17:29:51 +0700

    sadly I need to temporarily remove themes play, themes play moved to repo playSMS-extra-plugin

--- web/plugin/themes/play/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('_SECURE_') or die('Forbidden');
-
-include $apps_path['themes']."/".$themes_module."/config.php";
-include $apps_path['themes']."/".$themes_module."/fn.php";
-?>
-
--- web/plugin/themes/play/jscss/play.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-
-  $('.button')
-    .addClass('btn');
-
-});
