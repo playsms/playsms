daf3c7a74ba3f15a850b45c04fb72e2ecd96ba31
Anton Raharja <antonrd@gmail.com>
2013-12-23 13:48:56 +0700

    add logger_print to show error due to missing PHP curl functions in twilio

--- web/plugin/gateway/twilio/fn.php
+++ web/plugin/gateway/twilio/fn.php
@@ -34,37 +34,41 @@ function twilio_hook_sendsms($sms_sender,$sms_footer,$sms_to,$sms_msg,$uid='',$g
 		if (trim($core_config['plugin']['twilio']['callback_url'])) {
 			$data['StatusCallback'] = trim($core_config['plugin']['twilio']['callback_url']);
 		}
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_USERPWD, $core_config['plugin']['twilio']['account_sid'] . ':' . $core_config['plugin']['twilio']['auth_token']);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-		$returns = curl_exec($ch);
-		curl_close($ch);
-		logger_print("url:".$url." callback:".$core_config['plugin']['twilio']['callback_url'], 3, "twilio outgoing");
-		$resp = json_decode($returns);
-		if ($resp->status) {
-			$c_status = $resp->status;
-			$c_message_id = $resp->sid;
-			$c_error_text = $c_status.'|'.$resp->code.'|'.$resp->message;
-			logger_print("sent smslog_id:".$smslog_id." message_id:".$c_message_id." status:".$c_status." error:".$c_error_text, 2, "twilio outgoing");
-			$db_query = "
-				INSERT INTO "._DB_PREF_."_gatewayTwilio (local_smslog_id,remote_smslog_id,status,error_text)
-				VALUES ('$smslog_id','$c_message_id','$c_status','$c_error_text')";
-			$id = @dba_insert_id($db_query);
-			if ($id && ($c_status == 'queued')) {
-				$ok = true;
-				$p_status = 0;
+		if (function_exists('curl_init')) {
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_USERPWD, $core_config['plugin']['twilio']['account_sid'] . ':' . $core_config['plugin']['twilio']['auth_token']);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+			$returns = curl_exec($ch);
+			curl_close($ch);
+			logger_print("url:".$url." callback:".$core_config['plugin']['twilio']['callback_url'], 3, "twilio outgoing");
+			$resp = json_decode($returns);
+			if ($resp->status) {
+				$c_status = $resp->status;
+				$c_message_id = $resp->sid;
+				$c_error_text = $c_status.'|'.$resp->code.'|'.$resp->message;
+				logger_print("sent smslog_id:".$smslog_id." message_id:".$c_message_id." status:".$c_status." error:".$c_error_text, 2, "twilio outgoing");
+				$db_query = "
+					INSERT INTO "._DB_PREF_."_gatewayTwilio (local_smslog_id,remote_smslog_id,status,error_text)
+					VALUES ('$smslog_id','$c_message_id','$c_status','$c_error_text')";
+				$id = @dba_insert_id($db_query);
+				if ($id && ($c_status == 'queued')) {
+					$ok = true;
+					$p_status = 0;
+				} else {
+					$p_status = 2;
+				}
+				dlr($smslog_id,$uid,$p_status);
 			} else {
-				$p_status = 2;
+				// even when the response is not what we expected we still print it out for debug purposes
+				$resp = str_replace("\n", " ", $resp);
+				$resp = str_replace("\r", " ", $resp);
+				logger_print("failed smslog_id:".$smslog_id." resp:".$resp, 2, "twilio outgoing");
 			}
-			dlr($smslog_id,$uid,$p_status);
 		} else {
-			// even when the response is not what we expected we still print it out for debug purposes
-			$resp = str_replace("\n", " ", $resp);
-			$resp = str_replace("\r", " ", $resp);
-			logger_print("failed smslog_id:".$smslog_id." resp:".$resp, 2, "twilio outgoing");
+			logger_print("fail to sendsms due to missing PHP curl functions", 3, "twilio_hook_sendsms");
 		}
 	}
 	if (!$ok) {
