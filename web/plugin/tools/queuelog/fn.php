78d82f89596efb17abc00a555af02bd34542f68a
Anton Raharja <antonrd@gmail.com>
2014-02-11 10:02:23 +0700

    queuelog shows only queued

--- web/plugin/tools/queuelog/fn.php
+++ web/plugin/tools/queuelog/fn.php
@@ -17,7 +17,7 @@ function queuelog_get($line_per_page='', $limit='') {
 	$db_result = dba_query($db_query);
 	while ($db_row = dba_fetch_array($db_result)) {
 		$c_id = $db_row['id'];
-		$db_row['count'] = dba_count(_DB_PREF_.'_tblSMSOutgoing_queue_dst', array('queue_id' => $c_id));
+		$db_row['count'] = dba_count(_DB_PREF_.'_tblSMSOutgoing_queue_dst', array('flag' => 0, 'queue_id' => $c_id));
 		$ret[] = $db_row;
 	}
 	return $ret;
