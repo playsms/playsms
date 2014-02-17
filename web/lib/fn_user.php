1c0b5c7f7394cf3df36941525156929b52c6d0ba
Anton Raharja <antonrd@gmail.com>
2014-02-17 22:28:38 +0700

    add new function user_add()

--- web/lib/fn_user.php
+++ web/lib/fn_user.php
@@ -144,3 +144,105 @@ function user_mobile2uid($mobile) {
 	}
 	return $uid;
 }
+
+/**
+ * Add new user
+ * @param array $data User data
+ * @return array $ret('error_string', 'status')
+ */
+function user_add($data=array()) {
+	global $core_config;
+	$ret['status'] = FALSE;
+	$ret['error_string'] = _('Fail to register an account');
+	$data = ( trim($data['username']) ? $data : $_REQUEST );
+	if (!auth_isadmin() && $core_config['main']['cfg_enable_register']) {
+		$data['status'] = ( trim($data['status']) ? trim($data['status']) : 3 );
+		$data['status'] = ( auth_isadmin() ? $data['status'] : 3 );
+		$username = core_sanitize_username(trim($data['username']));
+		$email = trim($data['email']);
+		$name = trim($data['name']);
+		$mobile = trim($data['mobile']);
+		$password = ( trim($data['password']) ? trim($data['password']) : core_get_random_string(10) );
+		$credit = ( trim($data['credit']) ? trim($data['credit']) : $core_config['main']['cfg_default_credit'] );
+		$footer = '@'.$username;
+		$v = user_add_validate($data);
+		if ($v['status']) {
+			if ($username && $email && $name && $mobile) {
+				$continue = TRUE;
+
+				// check username
+				$db_query = "SELECT username FROM "._DB_PREF_."_tblUser WHERE username='$username'";
+				$db_result = dba_query($db_query);
+				if ($db_row = dba_fetch_array($db_result)) {
+					$ret['error_string'] = _('User is already exists')." ("._('username').": ".$username.")";
+					$continue = FALSE;
+				}
+
+				// check email
+				if ($continue) {
+					$db_query = "SELECT username FROM "._DB_PREF_."_tblUser WHERE email='$email'";
+					$db_result = dba_query($db_query);
+					if ($db_row = dba_fetch_array($db_result)) {
+						$ret['error_string'] = _('User with this email is already exists')." ("._('email').": ".$email.")";
+						$continue = FALSE;
+					}
+				}
+
+				// check mobile
+				if ($continue) {
+					$db_query = "SELECT username FROM "._DB_PREF_."_tblUser WHERE mobile='$mobile'";
+					$db_result = dba_query($db_query);
+					if ($db_row = dba_fetch_array($db_result)) {
+						$ret['error_string'] = _('User with this mobile is already exists')." ("._('mobile').": ".$mobile.")";
+						$continue = FALSE;
+					}
+				}
+
+				if ($continue) {
+					$password_coded = md5($password);
+					$dt = core_get_datetime();
+					$db_query = "
+						INSERT INTO "._DB_PREF_."_tblUser
+						(status, username, password, name, mobile, email, footer, credit, register_datetime, lastupdate_datetime)
+						VALUES
+						('$status','$username','$password_coded','$name','$mobile','$email','$footer','$credit','$dt','$dt')";
+					if ($new_uid = @dba_insert_id($db_query)) {
+						$ret['status'] = TRUE;
+					}
+				}
+				if ($ret['status']) {
+					logger_print("u:".$username." email:".$email." ip:".$_SERVER['REMOTE_ADDR']." mobile:".$mobile." credit:".$credit, 2, "register");
+					$subject = _('New account registration');
+					$body = $core_config['main']['cfg_web_title']."\n";
+					$body .= $core_config['http_path']['base']."\n\n";
+					$body .= _('Username')."\t: $username\n";
+					$body .= _('Password')."\t: $password\n";
+					$body .= _('Mobile')."\t: $mobile\n";
+					if ($credit) {
+						$body .= _('Credit')."\t: $credit\n";
+					}
+					$body .= "\n";
+					$body .= $core_config['main']['cfg_email_footer']."\n\n";
+					$ret['error_string'] = _('User has been added')." ("._('username').": ".$username.")";
+					$data = array(
+						'mail_from_name' => $core_config['main']['cfg_web_title'],
+						'mail_from' => $core_config['main']['cfg_email_service'],
+						'mail_to' => $email,
+						'mail_subject' => $subject,
+						'mail_body' => $body
+					);
+					if (sendmail($data)) {
+						$ret['error_string'] .= _('Password has been sent to your email');
+					} else {
+						$ret['error_string'] .= _('User has been addedd but failed to send email')." ("._('username').": ".$username.")";
+					}
+				}
+			}
+		} else {
+			$ret['error_string'] = $v['error_string'];
+		}
+	} else {
+		$ret['error_string'] = _('Public registration disabled');
+	}
+	return $ret;
+}
