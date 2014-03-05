fed2967f23c2aa19678c4d1842d081d6f8ff0cf9
Anton Raharja <antonrd@gmail.com>
2014-03-05 11:36:19 +0700

    change whats saved on notif_add() at recvsms_inbox_add()

--- web/lib/fn_recvsms.php
+++ web/lib/fn_recvsms.php
@@ -337,8 +337,8 @@ function recvsms_inbox_add($sms_datetime,$sms_sender,$target_user,$message,$sms_
 			}
 		}
 	}
-	if ($ok && $uid && $sms_sender) {
-		notif_add($uid, 'recvsms_inbox_add', 'New inbox from '.$sms_sender, 'You have received a new message from '.$sms_sender);
+	if ($ok && $uid && $sms_sender && $message) {
+		notif_add($uid, 'recvsms_inbox_add', $sms_sender, $message);
 	}
 	return $ok;
 }
