c8cf0eaafaf8ce769f4d115cc53f4926897d477f
Anton Raharja <antonrd@gmail.com>
2014-04-07 10:53:50 +0700

    update menu priority for all report

--- web/plugin/tools/report/config.php
+++ web/plugin/tools/report/config.php
@@ -5,13 +5,13 @@ if (auth_isadmin()) {
 	$menutab = $core_config['menutab']['administration'];
 	$menu_config[$menutab][] = array(
 		"index.php?app=main&inc=tools_report&op=report_admin",
-		_('All reports'),
-		1
+		_('All reports') ,
+		2
 	);
 }
 
 $menutab = $core_config['menutab']['my_account'];
 $menu_config[$menutab][] = array(
-    "index.php?app=main&inc=tools_report&op=report_user",
-    _('My report')
+	"index.php?app=main&inc=tools_report&op=report_user",
+	_('My report')
 );
