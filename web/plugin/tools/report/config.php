b63d5520f4312fce1a799a265b6cae1393858c0a
Anton Raharja <antonrd@gmail.com>
2014-04-08 01:27:42 +0700

    early version of whose online, still need some cleanups

--- web/plugin/tools/report/config.php
+++ web/plugin/tools/report/config.php
@@ -8,6 +8,10 @@ if (auth_isadmin()) {
 		_('All reports') ,
 		2
 	);
+	$menu_config[$menutab][] = array(
+		"index.php?app=main&inc=tools_report&route=online",
+		_('Whose online')
+	);
 }
 
 $menutab = $core_config['menutab']['my_account'];
