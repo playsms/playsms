76c3f7f9af11189e6cbd66c790da99c412905eb1
Anton Raharja <antonrd@gmail.com>
2014-04-08 10:50:14 +0700

    add option to filte whose online and whose idle only

--- web/plugin/tools/report/fn.php
+++ web/plugin/tools/report/fn.php
@@ -73,9 +73,11 @@ function report_count_deleted($uid=0) {
 /**
  * Get whose online
  * @param integer $status User status
+ * @param boolean $online_only Report whose online only
+ * @param boolean $idle_only Report whose online with login status idle only
  * @return array Whose online data
  */
-function report_whoseonline($status=0) {
+function report_whoseonline($status=0, $online_only=FALSE, $idle_only=FALSE) {
 	global $icon_config;
 
 	$ret = array();
@@ -95,11 +97,23 @@ function report_whoseonline($status=0) {
 			$c_is_admin = $icon_config['admin'];
 		}
 
+		$is_idle = FALSE;
+		$is_online = FALSE;
 		$c_idle = (int)(strtotime(core_get_datetime()) - strtotime($val['last_update']));
 		if ($c_idle > 15*60) {
+			$is_idle = TRUE;
 			$c_login_status = $icon_config['idle'];
 		} else {
+			$is_online = TRUE;
 			$c_login_status = $icon_config['online'];
+		}
+
+		if ($online_only && ! $is_online) {
+			continue;
+		}
+
+		if ($idle_only && ! $is_idle) {
+			continue;
 		}
 	
 		$ret[$c_username][] = array(
@@ -124,16 +138,20 @@ function report_whoseonline($status=0) {
 
 /**
  * Get admin whose online
+ * @param boolean $online_only Report whose online only
+ * @param boolean $idle_only Report whose online with login status idle only
  * @return array Whose online data
  */
-function report_whoseonline_admin() {
-	return report_whoseonline(2);
+function report_whoseonline_admin($online_only=FALSE, $idle_only=FALSE) {
+	return report_whoseonline(2, $online_only, $idle_only);
 }
 
 /**
  * Get user whose online
+ * @param boolean $online_only Report whose online only
+ * @param boolean $idle_only Report whose online with login status idle only
  * @return array Whose online data
  */
-function report_whoseonline_user() {
-	return report_whoseonline(3);
+function report_whoseonline_user($online_only=FALSE, $idle_only=FALSE) {
+	return report_whoseonline(3, $online_only, $idle_only);
 }
