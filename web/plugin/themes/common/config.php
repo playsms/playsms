342cff7ea56e637acc17602708a80aad79f36ba4
Anton Raharja <antonrd@gmail.com>
2014-04-14 20:19:24 +0700

    update menus

--- web/plugin/themes/common/config.php
+++ web/plugin/themes/common/config.php
@@ -2,68 +2,109 @@
 defined('_SECURE_') or die('Forbidden');
 
 if (function_exists('bindtextdomain')) {
-	bindtextdomain('messages', $core_config['apps_path']['plug'].'/language/');
+	bindtextdomain('messages', $core_config['apps_path']['plug'] . '/language/');
 	bind_textdomain_codeset('messages', 'UTF-8');
 	textdomain('messages');
 }
 
-
 // common action icons
 
 $icon_config = array(
-	'add' => "<span class='playsms-icon glyphicon glyphicon-plus' alt='"._('Add')."' title='"._('Add')."'></span>",
-	'edit' => "<span class='playsms-icon glyphicon glyphicon-cog' alt='"._('Edit')."' title='"._('Edit')."'></span>",
-	'delete' => "<span class='playsms-icon glyphicon glyphicon-trash' alt='"._('Delete')."' title='"._('Delete')."'></span>",
-	'view' => "<span class='playsms-icon glyphicon glyphicon-eye-open' alt='"._('View')."' title='"._('View')."'></span>",
-	'manage' => "<span class='playsms-icon glyphicon glyphicon-folder-open' alt='"._('Manage')."' title='"._('Manage')."'></span>",
-	'forward' => "<span class='playsms-icon glyphicon glyphicon-new-window' alt='"._('Forward')."' title='"._('Forward')."'></span>",
-	'reply' => "<span class='playsms-icon glyphicon glyphicon-log-out' alt='"._('Reply')."' title='"._('Reply')."'></span>",
-	'resend' => "<span class='playsms-icon glyphicon glyphicon-log-in' alt='"._('Resend')."' title='"._('Resend')."'></span>",
-	'user_pref' => "<span class='playsms-icon glyphicon glyphicon-user' alt='"._('User preference')."' title='"._('User preference')."'></span>",
-	'user_config' => "<span class='playsms-icon glyphicon glyphicon-wrench' alt='"._('User configuration')."' title='"._('User configuration')."'></span>",
-	'user_delete' => "<span class='playsms-icon glyphicon glyphicon-trash' alt='"._('Delete user')."' title='"._('Delete user')."'></span>",
-	'admin' => "<span class='playsms-icon glyphicon glyphicon-certificate' alt='"._('Administrator')."' title='"._('Administrator')."'></span>",
-	'export' => "<span class='playsms-icon glyphicon glyphicon-export' alt='"._('Export')."' title='"._('Export')."'></span>",
-	'import' => "<span class='playsms-icon glyphicon glyphicon-import' alt='"._('Import')."' title='"._('Import')."'></span>",
-	'group' => "<span class='playsms-icon glyphicon glyphicon-briefcase' alt='"._('Group')."' title='"._('Group')."'></span>",
-	'move' => "<span class='playsms-icon glyphicon glyphicon glyphicon-move' alt='"._('Move')."' title='"._('Move')."'></span>",
-	'go' => "<span class='playsms-icon glyphicon glyphicon-cog' alt='"._('Go')."' title='"._('Go')."'></span>",
-	'online' => "<span class='playsms-icon glyphicon glyphicon-ok-circle' alt='"._('Online')."' title='"._('Online')."'></span>",
-	'offline' => "<span class='playsms-icon glyphicon glyphicon-remove-circle' alt='"._('Offline')."' title='"._('Offline')."'></span>",
-	'idle' => "<span class='playsms-icon glyphicon glyphicon-time' alt='"._('Idle')."' title='"._('Idle')."'></span>",
-	'ban' => "<span class='playsms-icon glyphicon glyphicon-thumbs-down' alt='"._('Ban')."' title='"._('Ban')."'></span>",
-	'unban' => "<span class='playsms-icon glyphicon glyphicon-thumbs-up' alt='"._('Unban')."' title='"._('Unban')."'></span>",
+	'add' => "<span class='playsms-icon glyphicon glyphicon-plus' alt='" . _('Add') . "' title='" . _('Add') . "'></span>",
+	'edit' => "<span class='playsms-icon glyphicon glyphicon-cog' alt='" . _('Edit') . "' title='" . _('Edit') . "'></span>",
+	'delete' => "<span class='playsms-icon glyphicon glyphicon-trash' alt='" . _('Delete') . "' title='" . _('Delete') . "'></span>",
+	'view' => "<span class='playsms-icon glyphicon glyphicon-eye-open' alt='" . _('View') . "' title='" . _('View') . "'></span>",
+	'manage' => "<span class='playsms-icon glyphicon glyphicon-folder-open' alt='" . _('Manage') . "' title='" . _('Manage') . "'></span>",
+	'forward' => "<span class='playsms-icon glyphicon glyphicon-new-window' alt='" . _('Forward') . "' title='" . _('Forward') . "'></span>",
+	'reply' => "<span class='playsms-icon glyphicon glyphicon-log-out' alt='" . _('Reply') . "' title='" . _('Reply') . "'></span>",
+	'resend' => "<span class='playsms-icon glyphicon glyphicon-log-in' alt='" . _('Resend') . "' title='" . _('Resend') . "'></span>",
+	'user_pref' => "<span class='playsms-icon glyphicon glyphicon-user' alt='" . _('User preference') . "' title='" . _('User preference') . "'></span>",
+	'user_config' => "<span class='playsms-icon glyphicon glyphicon-wrench' alt='" . _('User configuration') . "' title='" . _('User configuration') . "'></span>",
+	'user_delete' => "<span class='playsms-icon glyphicon glyphicon-trash' alt='" . _('Delete user') . "' title='" . _('Delete user') . "'></span>",
+	'admin' => "<span class='playsms-icon glyphicon glyphicon-certificate' alt='" . _('Administrator') . "' title='" . _('Administrator') . "'></span>",
+	'export' => "<span class='playsms-icon glyphicon glyphicon-export' alt='" . _('Export') . "' title='" . _('Export') . "'></span>",
+	'import' => "<span class='playsms-icon glyphicon glyphicon-import' alt='" . _('Import') . "' title='" . _('Import') . "'></span>",
+	'group' => "<span class='playsms-icon glyphicon glyphicon-briefcase' alt='" . _('Group') . "' title='" . _('Group') . "'></span>",
+	'move' => "<span class='playsms-icon glyphicon glyphicon glyphicon-move' alt='" . _('Move') . "' title='" . _('Move') . "'></span>",
+	'go' => "<span class='playsms-icon glyphicon glyphicon-cog' alt='" . _('Go') . "' title='" . _('Go') . "'></span>",
+	'online' => "<span class='playsms-icon glyphicon glyphicon-ok-circle' alt='" . _('Online') . "' title='" . _('Online') . "'></span>",
+	'offline' => "<span class='playsms-icon glyphicon glyphicon-remove-circle' alt='" . _('Offline') . "' title='" . _('Offline') . "'></span>",
+	'idle' => "<span class='playsms-icon glyphicon glyphicon-time' alt='" . _('Idle') . "' title='" . _('Idle') . "'></span>",
+	'ban' => "<span class='playsms-icon glyphicon glyphicon-thumbs-down' alt='" . _('Ban') . "' title='" . _('Ban') . "'></span>",
+	'unban' => "<span class='playsms-icon glyphicon glyphicon-thumbs-up' alt='" . _('Unban') . "' title='" . _('Unban') . "'></span>",
+	'logout' => "<span class='playsms-icon glyphicon glyphicon-off' alt='" . _('Logout') . "' title='" . _('Logout') . "'></span>",
 );
 
 // menu structure
 
 // menu tabs
 $core_config['menutab'] = array(
-	'home' => _('Home'),
-	'my_account' => _('My Account'),
-	'feature' => _('Feature'),
-	'administration' => _('Administration'),
+	'home' => _('Home') ,
+	'my_account' => _('My Account') ,
+	'feature' => _('Feature') ,
+	'administration' => _('Administration') ,
 );
 
 // my account tab
 $menutab = $core_config['menutab']['my_account'];
 $menu_config[$menutab] = array(
-	array('index.php?app=main&inc=send_sms&op=send_sms', _('Send message'), 1),
-	array('index.php?app=main&inc=user_inbox&op=user_inbox', _('Inbox'), 1),
-	array('index.php?app=main&inc=user_incoming&op=user_incoming', _('Incoming messages'), 1),
-	array('index.php?app=main&inc=user_outgoing&op=user_outgoing', _('Outgoing messages'), 1),
+	array(
+		'index.php?app=main&inc=send_sms&op=send_sms',
+		_('Send message') ,
+		1
+	) ,
+	array(
+		'index.php?app=main&inc=user_inbox&op=user_inbox',
+		_('Inbox') ,
+		1
+	) ,
+	array(
+		'index.php?app=main&inc=user_incoming&op=user_incoming',
+		_('Incoming messages') ,
+		1
+	) ,
+	array(
+		'index.php?app=main&inc=user_outgoing&op=user_outgoing',
+		_('Outgoing messages') ,
+		1
+	) ,
 );
 
 // only if logged in user is an admin then load administration tab
 if (auth_isadmin()) {
+	
 	// administrator menus
 	$menutab = $core_config['menutab']['administration'];
 	$menu_config[$menutab] = array(
-		array('index.php?app=main&inc=all_inbox&op=all_inbox', _('All inbox'), 1),
-		array('index.php?app=main&inc=all_incoming&op=all_incoming', _('All incoming messages'), 1),
-		array('index.php?app=main&inc=all_outgoing&op=all_outgoing', _('All outgoing messages'), 1),
-		array('index.php?app=main&inc=sandbox&op=sandbox', _('Sandbox'), 1),
-		array('index.php?app=main&inc=user_mgmnt&op=user_list', _('Manage user'), 2),
-		array('index.php?app=main&inc=main_config&op=main_config', _('Main configuration'), 2),
+		array(
+			'index.php?app=main&inc=all_inbox&op=all_inbox',
+			_('All inbox') ,
+			1
+		) ,
+		array(
+			'index.php?app=main&inc=all_incoming&op=all_incoming',
+			_('All incoming messages') ,
+			1
+		) ,
+		array(
+			'index.php?app=main&inc=all_outgoing&op=all_outgoing',
+			_('All outgoing messages') ,
+			1
+		) ,
+		array(
+			'index.php?app=main&inc=sandbox&op=sandbox',
+			_('Sandbox') ,
+			1
+		) ,
+		array(
+			'index.php?app=main&inc=user_mgmnt&op=user_list',
+			_('Manage user') ,
+			2
+		) ,
+		array(
+			'index.php?app=main&inc=main_config&op=main_config',
+			_('Main configuration') ,
+			2
+		) ,
 	);
 }
--- web/plugin/themes/ubuntu/fn.php
+++ web/plugin/themes/ubuntu/fn.php
@@ -11,13 +11,13 @@ function ubuntu_hook_themes_apply($content) {
 			'WEB_TITLE' => $web_title,
 			'HTTP_PATH_BASE' => $core_config['http_path']['base'],
 			'HTTP_PATH_THEMES' => $core_config['http_path']['themes'],
-			'THEMES_MODULE' => core_themes_get(),
-			'THEMES_MENU_TREE' => themes_get_menu_tree(),
+			'THEMES_MODULE' => core_themes_get() ,
+			'THEMES_MENU_TREE' => themes_get_menu_tree() ,
 			'NAME' => $user_config['name'],
 			'USERNAME' => $user_config['username'],
 			'GRAVATAR' => $user_config['opt']['gravatar'],
 			'Logout' => _('Logout')
-		),
+		) ,
 		'if' => array(
 			'valid' => auth_isvalid()
 		)
@@ -27,34 +27,25 @@ function ubuntu_hook_themes_apply($content) {
 }
 
 function ubuntu_hook_themes_buildmenu($menu_config) {
-	global $core_config, $user_config;
+	global $core_config, $user_config, $icon_config;
+	
 	$main_menu = "";
 	foreach ($menu_config as $menu_title => $array_menu) {
-		if ($menu_title == $core_config['menutab']['my_account']) {
-			$main_menu .= "<li class='dropdown'><a href='#' data-toggle='dropdown' class='dropdown-toggle'>" . $user_config['name'] . " (".$user_config['username'].") <b class='caret'></b></a>";
-			$main_menu .= "<ul class='dropdown-menu'>";
-		} else {
-			$main_menu .= "<li class='dropdown'><a href='#' data-toggle='dropdown' class='dropdown-toggle'>" . $menu_title . " <b class='caret'></b></a>";
-			$main_menu .= "<ul class='dropdown-menu'>";
-		}
+		$main_menu.= "<li class='dropdown'><a href='#' data-toggle='dropdown' class='dropdown-toggle'>" . $menu_title . " <b class='caret'></b></a>";
+		$main_menu.= "<ul class='dropdown-menu'>";
 		foreach ($array_menu as $sub_menu) {
 			$sub_menu_url = $sub_menu[0];
 			$sub_menu_title = $sub_menu[1];
-			$sub_menu_index = ( $sub_menu[2] ? $sub_menu[2] : 3 );
-			$m[$sub_menu_index.'.'.$sub_menu_title] = "<li><a href='" . _u($sub_menu_url) . "'>" . $sub_menu_title . "</a></li>";
+			$sub_menu_index = ($sub_menu[2] ? $sub_menu[2] : 3);
+			$m[$sub_menu_index . '.' . $sub_menu_title] = "<li><a href='" . _u($sub_menu_url) . "'>" . $sub_menu_title . "</a></li>";
 		}
 		ksort($m);
 		foreach ($m as $mm) {
-			$main_menu .= $mm;
+			$main_menu.= $mm;
 		}
 		unset($m);
-		if ($menu_title == $core_config['menutab']['my_account']) {
-			$main_menu .= "<li><hr /></li>";
-			$main_menu .= "<li><a href='"._u('index.php?app=main&inc=user_config&op=user_config')."'>" . _('User configuration') . "</a></li>";
-			$main_menu .= "<li><a href='"._u('index.php?app=main&inc=user_pref&op=user_pref')."'>" . _('Preferences') . "</a></li>";
-		}
-		$main_menu .= "</ul>";
-		$main_menu .= "</li>";
+		$main_menu.= "</ul>";
+		$main_menu.= "</li>";
 	}
 	
 	$content = "
@@ -72,16 +63,23 @@ function ubuntu_hook_themes_buildmenu($menu_config) {
 					<div class='navbar-collapse collapse'>
 						<ul class='nav navbar-nav'>
 							<li class='active'><a href='" . _u(_HTTP_PATH_BASE_) . "'>" . _('Home') . "</a></li>
-							".$main_menu."
+							" . $main_menu . "
 						</ul>
-						<ul id=nav-login class='nav navbar-nav navbar-right'>
-							<li><a href='"._u('index.php?app=main&inc=core_auth&route=logout')."'>" . _('Logout') . "</a></li>
+						<ul class='nav navbar-nav navbar-right'>
+							<li class='dropdown'><a href='#' data-toggle='dropdown' class='dropdown-toggle'>" . $user_config['name'] . " (" . $user_config['username'] . ") <b class='caret'></b></a>
+								<ul class='dropdown-menu'>
+									<li><a href='" . _u('index.php?app=main&inc=user_config&op=user_config') . "'>" . _('User configuration') . "</a></li>
+									<li><a href='" . _u('index.php?app=main&inc=user_pref&op=user_pref') . "'>" . _('Preferences') . "</a></li>									
+								</ul>
+							</li>
+							<li><a href='" . _u('index.php?app=main&inc=core_auth&route=logout') . "'>" . $icon_config['logout'] . "</a></li>
 						</ul>
 					</div>
 				</div>
 			</div>
 		</nav>
 	";
+	
 	return $content;
 }
 
@@ -89,26 +87,28 @@ function ubuntu_hook_themes_navbar($num, $nav, $max_nav, $url, $page) {
 	global $core_config;
 	$nav_pages = "";
 	if ($num) {
-		$nav_start = ((($nav-1) * $max_nav)+1);
+		$nav_start = ((($nav - 1) * $max_nav) + 1);
 		$nav_end = (($nav) * $max_nav);
 		$start = 1;
-		$end = ceil($num/$max_nav);
+		$end = ceil($num / $max_nav);
 		$nav_pages = "<div class=playsms-nav-bar>";
-		$nav_pages .= "<a href='"._u($url.'&page=1&nav=1')."'> << </a>";
-		$nav_pages .= ($start==$nav) ? " < " : "<a href='"._u($url.'&page='.((($nav-2)*$max_nav)+1).'&nav='.($nav-1))."'> < </a>";
-		$nav_pages .= ($start==$nav) ? "" : " ... ";
-		for($i=$nav_start;$i<=$nav_end;$i++) {
-			if($i>$num){ break; };
+		$nav_pages.= "<a href='" . _u($url . '&page=1&nav=1') . "'> << </a>";
+		$nav_pages.= ($start == $nav) ? " < " : "<a href='" . _u($url . '&page=' . ((($nav - 2) * $max_nav) + 1) . '&nav=' . ($nav - 1)) . "'> < </a>";
+		$nav_pages.= ($start == $nav) ? "" : " ... ";
+		for ($i = $nav_start; $i <= $nav_end; $i++) {
+			if ($i > $num) {
+				break;
+			};
 			if ($i == $page) {
-				$nav_pages .= "<u>$i</u> ";
+				$nav_pages.= "<u>$i</u> ";
 			} else {
-				$nav_pages .= "<a href='"._u($url.'&page='.$i.'&nav='.$nav)."'>".$i."</a> ";
+				$nav_pages.= "<a href='" . _u($url . '&page=' . $i . '&nav=' . $nav) . "'>" . $i . "</a> ";
 			}
 		}
-		$nav_pages .= ($end==$nav) ? "" : "..";
-		$nav_pages .= ($end==$nav) ? " > " : "<a href='"._u($url.'&page='.(($nav*$max_nav)+1).'&nav='.($nav+1))."'> > </a>";
-		$nav_pages .= "<a href='"._u($url.'&page='.$num.'&nav='.$end)."'> >> </a>";
-		$nav_pages .= "</div>";
+		$nav_pages.= ($end == $nav) ? "" : "..";
+		$nav_pages.= ($end == $nav) ? " > " : "<a href='" . _u($url . '&page=' . (($nav * $max_nav) + 1) . '&nav=' . ($nav + 1)) . "'> > </a>";
+		$nav_pages.= "<a href='" . _u($url . '&page=' . $num . '&nav=' . $end) . "'> >> </a>";
+		$nav_pages.= "</div>";
 	}
 	return $nav_pages;
 }
