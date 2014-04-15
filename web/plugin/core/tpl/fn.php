d4fde0df66366f0395c73111ddfd18daffcdb95c
Anton Raharja <antonrd@gmail.com>
2014-04-15 22:34:11 +0700

    cleanup template, allows sub folder for template file name

--- web/plugin/core/tpl/fn.php
+++ web/plugin/core/tpl/fn.php
@@ -19,13 +19,28 @@
 
 defined('_SECURE_') or die('Forbidden');
 
+/**
+ * Template string manipulation
+ * @param  string $content Content
+ * @param  string $key     Template key
+ * @param  string $val     Template value
+ * @return string          Manipulated content
+ */
 function _tpl_set_string($content, $key, $val) {
 	$content = str_replace('{' . $key . '}', $val, $content);
 	return $content;
 }
 
+/**
+ * Template loop manipulation
+ * @param  string $content Content
+ * @param  string $key     Template key
+ * @param  string $val     Template value
+ * @return string          Manipulated content
+ */
 function _tpl_set_array($content, $key, $val) {
 	preg_match("/<loop\." . $key . ">(.*?)<\/loop\." . $key . ">/s", $content, $l);
+	
 	$loop = $l[1];
 	foreach ($val as $v) {
 		$loop_replaced = $loop;
@@ -34,54 +49,103 @@ function _tpl_set_array($content, $key, $val) {
 		}
 		$loop_content.= $loop_replaced;
 	}
+	
 	$content = preg_replace("/<loop\." . $key . ">(.*?)<\/loop\." . $key . ">/s", $loop_content, $content);
 	$content = str_replace("<loop." . $key . ">", '', $content);
 	$content = str_replace("</loop." . $key . ">", '', $content);
+	
 	return $content;
 }
 
+/**
+ * Template boolean manipulation
+ * @param  string $content Content
+ * @param  string $key     Template key
+ * @param  string $val     Template value
+ * @return string          Manipulated content
+ */
 function _tpl_set_bool($content, $key, $val) {
 	if ($key && !$val) {
 		$content = preg_replace("/<if\." . $key . ">(.*?)<\/if\." . $key . ">/s", '', $content);
 	}
 	$content = str_replace("<if." . $key . ">", '', $content);
 	$content = str_replace("</if." . $key . ">", '', $content);
+	
 	return $content;
 }
 
+/**
+ * Actual template apply
+ * @param  string $fn  Template filename
+ * @param  array  $tpl Template data
+ * @return string      Manipulated content
+ */
 function _tpl_apply($fn, $tpl) {
 	$content = trim(file_get_contents($fn));
+	
 	if ($content && is_array($tpl)) {
+		
 		if (isset($tpl['if'])) {
 			foreach ($tpl['if'] as $key => $val) {
 				$content = _tpl_set_bool($content, $key, $val);
 			}
 			unset($tpl['if']);
 		}
+		
 		if (isset($tpl['loop'])) {
 			foreach ($tpl['loop'] as $key => $val) {
 				$content = _tpl_set_array($content, $key, $val);
 			}
 			unset($tpl['loop']);
 		}
+		
 		if (isset($tpl['var'])) {
 			foreach ($tpl['var'] as $key => $val) {
 				$content = _tpl_set_string($content, $key, $val);
 			}
 		}
 	}
+	
 	$content = preg_replace("/<if\..*?>(.*?)<\/if\..*?>/s", '', $content);
 	$content = preg_replace("/<loop\..*?>(.*?)<\/loop\..*?>/s", '', $content);
+	
 	return $content;
 }
 
+/**
+ * Sanitize template name
+ * @param  string $name Template name
+ * @return string       Sanitized template name
+ */
+function _tpl_name_sanitize($name) {
+	$name = str_replace('..', '', $name);
+	$name = str_replace('|', '', $name);
+	$name = str_replace('"', '', $name);
+	$name = str_replace("'", '', $name);
+	$name = str_replace("\\", '/', $name);
+
+	return $name;
+}
+
+/**
+ * Apply template
+ * @param  array  $tpl Template array
+ * @return string      Manipulated content
+ */
 function tpl_apply($tpl) {
-	if (is_array($tpl) && $tpl['name']) {
+	$content = '';
+	$continue = FALSE;
+	
+	if (is_array($tpl)) {
+		if ($tpl_name = _tpl_name_sanitize($tpl['name'])) {
+			$continue = TRUE;
+		}
+	}
+	
+	if ($continue) {
 		
 		// inject anti-CSRF hidden field
 		$tpl['var']['CSRF_FORM'] = _CSRF_FORM_;
-		
-		$tpl_name = core_query_sanitize($tpl['name']);
 		
 		// check from active plugin
 		$c_inc = explode('_', _INC_);
