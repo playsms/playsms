42bb5ed6f8d2b0e8827546d77197079320f2f4d1
Anton Raharja <antonrd@gmail.com>
2014-06-25 09:53:21 +0700

    update sms_sync, code formatter

--- web/plugin/feature/sms_sync/fn.php
+++ web/plugin/feature/sms_sync/fn.php
@@ -1,4 +1,20 @@
 <?php
-defined('_SECURE_') or die('Forbidden');
 
-?>
+/**
+ * This file is part of playSMS.
+ *
+ * playSMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * playSMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with playSMS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+defined('_SECURE_') or die('Forbidden');
--- web/plugin/feature/sms_sync/sms_sync.php
+++ web/plugin/feature/sms_sync/sms_sync.php
@@ -1,15 +1,27 @@
 <?php
+
+/**
+ * This file is part of playSMS.
+ *
+ * playSMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * playSMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with playSMS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 defined('_SECURE_') or die('Forbidden');
 
 if (!auth_isvalid()) {
 	auth_block();
 };
-
-// error messages
-$error_content = '';
-if ($err = $_SESSION['error_string']) {
-	$error_content = "<div class=error_string>$err</div>";
-}
 
 switch (_OP_) {
 	case "sms_sync_list":
@@ -18,37 +30,38 @@ switch (_OP_) {
 		if ($list['feature']['sms_sync']['enable']) {
 			$option_enable = 'checked';
 		}
-		$sync_url = $core_config['http_path']['base'].'/plugin/feature/sms_sync/sync.php?uid='.$user_config['uid'];
+		$sync_url = $core_config['http_path']['base'] . '/plugin/feature/sms_sync/sync.php?uid=' . $user_config['uid'];
 		unset($tpl);
 		$tpl = array(
 			'name' => 'sms_sync',
 			'vars' => array(
-				'ERROR' => $error_content,
-				'HINT_SECRET' => _hint(_('Secret key is used in SMSSync app')),
-				'HINT_ENABLE' => _hint(_('Check to enable receiving push messages from SMSSync app')),
+				'ERROR' => _err_display(),
+				'HINT_SECRET' => _hint(_('Secret key is used in SMSSync app')) ,
+				'HINT_ENABLE' => _hint(_('Check to enable receiving push messages from SMSSync app')) ,
 				'SECRET' => $sms_sync_secret,
 				'CHECKED' => $option_enable,
 				'SYNC_URL' => $sync_url,
-				'Manage sync' => _('Manage sync'),
-				'Secret key' => _('Secret key'),
-				'Enable SMS Sync' => _('Enable SMS Sync'),
-				'Sync URL' => _('Sync URL'),
-			'Notes' => _('Notes'),
-			'Download SMSSync app for Android from' => _('Download SMSSync app for Android from'),
-			'Save' => _('Save')
+				'Manage sync' => _('Manage sync') ,
+				'Secret key' => _('Secret key') ,
+				'Enable SMS Sync' => _('Enable SMS Sync') ,
+				'Sync URL' => _('Sync URL') ,
+				'Notes' => _('Notes') ,
+				'Download SMSSync app for Android from' => _('Download SMSSync app for Android from') ,
+				'Save' => _('Save')
 			)
 		);
 		_p(tpl_apply($tpl));
 		break;
+
 	case "sms_sync_save":
 		$items['secret'] = $_POST['sms_sync_secret'];
-		$items['enable'] = ( trim($_POST['sms_sync_enable']) ? 1 : 0 );
+		$items['enable'] = (trim($_POST['sms_sync_enable']) ? 1 : 0);
 		if (registry_update($user_config['uid'], 'feature', 'sms_sync', $items)) {
 			$_SESSION['error_string'] = _('SMS Sync configuration has been saved');
 		} else {
 			$_SESSION['error_string'] = _('Fail to save SMS Sync configuration');
 		}
-		header("Location: "._u('index.php?app=main&inc=feature_sms_sync&op=sms_sync_list'));
+		header("Location: " . _u('index.php?app=main&inc=feature_sms_sync&op=sms_sync_list'));
 		exit();
 		break;
 }
