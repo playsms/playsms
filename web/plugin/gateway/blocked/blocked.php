3e8663c91d6754cc0cd83475352506297dad470b
Anton Raharja <antonrd@gmail.com>
2014-09-18 21:39:57 +0700

    enhance dev and blocked, add simulate incoming SMS to Settings menu

--- web/plugin/gateway/blocked/blocked.php
+++ web/plugin/gateway/blocked/blocked.php
@@ -1,2 +1,48 @@
 <?php
+
+/**
+ * This file is part of playSMS.
+ *
+ * playSMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * playSMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with playSMS. If not, see <http://www.gnu.org/licenses/>.
+ */
 defined('_SECURE_') or die('Forbidden');
+
+if (!auth_isadmin()) {
+	auth_block();
+}
+
+include $core_config['apps_path']['plug'] . "/gateway/blocked/config.php";
+
+$gw = core_gateway_get();
+
+if ($gw == $plugin_config['blocked']['name']) {
+	$status_active = "<span class=status_active />";
+} else {
+	$status_active = "<span class=status_inactive />";
+}
+
+switch (_OP_) {
+	case "manage" :
+		if ($err = $_SESSION['error_string']) {
+			$content = "<div class=error_string>$err</div>";
+		}
+		$content .= "
+			<h2>" . _('Manage blocked') . "</h2>
+			<table class=playsms-table>
+				<tbody><tr><td class=label-sizer>" . _('Gateway name') . "</td><td>blocked $status_active</td></tr></tbody>
+			</table>
+		";
+		_p($content);
+		break;
+}
--- web/plugin/gateway/dev/config.php
+++ web/plugin/gateway/dev/config.php
@@ -1,12 +1,14 @@
 <?php
 defined('_SECURE_') or die('Forbidden');
 
-$plugin_config['dev']['name']			= 'dev';
-$plugin_config['dev']['enable_incoming']	= true;
-$plugin_config['dev']['enable_outgoing']	= true;
+$plugin_config['dev']['name'] = 'dev';
+$plugin_config['dev']['enable_incoming'] = true;
+$plugin_config['dev']['enable_outgoing'] = true;
 
-// insert to left menu array
-//if (isadmin()) {
-//	$menutab_gateway = $core_config['menutab']['gateway'];
-//	$menu_config[$menutab_gateway][] = array("index.php?app=main&inc=gateway_dev&op=manage", _('Manage dev'));
-//}
+if (auth_isadmin()) {
+	$menutab = $core_config['menutab']['settings'];
+	$menu_config[$menutab][] = array(
+		"index.php?app=main&inc=gateway_dev&route=simulate&op=simulate",
+		_('Simulate incoming SMS') 
+	);
+}
