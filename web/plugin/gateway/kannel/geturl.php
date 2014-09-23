3a35ceb1d1ef3e82c18e1f3e9459cff84a720221
Anton Raharja <antonrd@gmail.com>
2014-09-23 09:01:48 +0700

    put smsc on log incoming

--- web/plugin/gateway/kannel/geturl.php
+++ web/plugin/gateway/kannel/geturl.php
@@ -50,7 +50,7 @@ $a = trim($_REQUEST['a']); // message
 $Q = trim($_REQUEST['Q']); // sms_receiver
 $smsc = trim($_REQUEST['smsc']); // SMSC
 
-logger_print("addr:" . $remote_addr . " host:" . $remote_host . " t:" . $t . " q:" . $q . " a:" . $a . " Q:" . $Q, 3, "kannel incoming");
+logger_print("addr:" . $remote_addr . " host:" . $remote_host . " t:" . $t . " q:" . $q . " a:" . $a . " Q:" . $Q . " smsc:[" . $smsc . "]", 3, "kannel incoming");
 
 if ($t && $q && $a) {
 	// collected:
