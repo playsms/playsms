63133bbbda8428a7f7514eb5230fdaf08ac14fa1
t-ony <t-ony@users.noreply.github.com>
2014-10-24 23:21:09 +0300

    user.php follow up on issue #296
    HI Anton,
    
    I modified the user report to avoid using queue numbers while generating the report since each group has 1 queue number and that was leading to incorrect values in reports.
    
    One thing I changed in the design, I executed one MySQL query grouping the p_status and saved the result in an arrayand then updated the tpl values from the array instead of querying MySQL for every value (pending,sent,failed,delivered) thus executing several MySQL queries using the report_count function.
    
    please let me know if this works or if it affects other areas in your code.
    
    Thanks,
    Tony

--- web/plugin/feature/report/user.php
+++ web/plugin/feature/report/user.php
@@ -36,6 +36,30 @@ foreach ($data AS $a) {
 // CREDIT
 $credit = $user_config['credit'];
 
+//p_status values mapped to tpl array elements
+$map_values = array(
+    '0' => 'num_rows_pending',
+    '1' => 'num_rows_sent',
+    '2' => 'num_rows_failed',
+    '3' => 'num_rows_delivered'
+);
+
+//mysqli connection parameters
+$mysqli = new mysqli(_DB_HOST_, _DB_USER_, _DB_PASS_, _DB_NAME_);
+
+//check connection
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+//populate array result with the values from the mysql query        
+$result = mysqli_query($mysqli, 'SELECT flag_deleted, p_status, COUNT(*) AS count from ' . _DB_PREF_ . '_tblSMSOutgoing where uid =' . $c_uid . ' group by flag_deleted, p_status');
+
+for ($set = array(); $row = $result->fetch_assoc(); $set[] = $row);
+
+
+//define tpl before updating it with array result values
 $tpl = array(
 	'name' => 'report_user',
 	'vars' => array(
@@ -48,13 +72,25 @@ $tpl = array(
 		'Deleted' => _('Deleted') ,
 		'Billing' => _('Billing') ,
 		'Credit' => _('Credit') ,
-		'num_rows_pending' => report_count_pending($c_uid) ,
-		'num_rows_sent' => report_count_sent($c_uid) ,
-		'num_rows_delivered' => report_count_delivered($c_uid) ,
-		'num_rows_failed' => report_count_failed($c_uid) ,
-		'num_rows_deleted' => report_count_deleted($c_uid) ,
+                'num_rows_pending' => 0,
+                'num_rows_sent' => 0,
+                'num_rows_delivered' => 0,
+                'num_rows_failed' => 0,
+                'num_rows_deleted' => 0,
 		'billing' => $billing,
 		'credit' => $credit
 	)
 );
+
+//update tpl array with values from the result array
+
+for ($i = 0; $i < count($set); $i++) {
+    $c = 0;
+    if ($set[$i]['flag_deleted'] == 0) {
+        $tpl['vars'][$map_values[$set[$i]['p_status']]] += $set[$i]['count'];
+    } else {
+        $tpl['vars']['num_rows_deleted'] += $set[$i]['count'];
+    }
+    
+}
 _p(tpl_apply($tpl));
