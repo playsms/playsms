2cbf56fb071c36ada0414133ba7f7c12e5baa203
Anton Raharja <antonrd@gmail.com>
2014-11-18 09:11:50 +0700

    update lang

--- contrib/tools/language/1-update-pot-files.sh
+++ contrib/tools/language/1-update-pot-files.sh
@@ -12,6 +12,7 @@ CWD=$(pwd)
 ##Common strings
 cd $PLAYSMS
 touch plugin/language/messages.pot
+touch plugin/language/index.html
 xgettext -L PHP --omit-header --no-location --sort-output --from-code=utf-8 -j -o plugin/language/messages.pot init.php
 xgettext -L PHP --omit-header --no-location --sort-output --from-code=utf-8 -j -o plugin/language/messages.pot index.php
 find lib/ -iname "*.php" -exec xgettext -L PHP --omit-header --no-location --sort-output --from-code=utf-8 -j -o plugin/language/messages.pot {} \;
@@ -25,7 +26,10 @@ find . -type d -name "language" | grep -v "plugin/language" | sed -e "s/\/[^\/]*
 for i in `cat /tmp/.lang_folders` ; do mkdir -p "$i/language" ; done
 for i in `cat /tmp/.lang_folders` ; do rm -f "$i/language/messages.pot" ; done
 for i in `cat /tmp/.lang_folders` ; do touch "$i/language/messages.pot" ; done
-for i in `cat /tmp/.lang_folders` ; do find $i -iname '*.php' -exec xgettext -L PHP --omit-header --no-location --sort-output --from-code=utf-8 -j -o "$i/language/messages.pot" {} \; ; done
+for i in `cat /tmp/.lang_folders` ; do 
+	find $i -iname '*.php' -exec xgettext -L PHP --omit-header --no-location --sort-output --from-code=utf-8 -j -o "$i/language/messages.pot" {} \; ; 
+	touch "$i/language/index.html"
+done
 rm /tmp/.lang_folders
 cd $CWD
 
