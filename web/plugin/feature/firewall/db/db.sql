8228a67cf86a2b1d7d573bf0ba91635534583fb3
Anton Raharja <antonrd@gmail.com>
2014-12-16 10:19:44 +0700

    update db sql

--- web/plugin/feature/firewall/db/db.sql
+++ web/plugin/feature/firewall/db/db.sql
@@ -1,27 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.1.9
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Dec 11, 2014 at 08:26 AM
--- Server version: 5.5.40-0ubuntu1
--- PHP Version: 5.5.12-2ubuntu4.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `playsms`
---
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `playsms_featureFirewall`
 --
@@ -30,11 +6,7 @@ DROP TABLE IF EXISTS `playsms_featureFirewall`;
 CREATE TABLE `playsms_featureFirewall` (
   `c_timestamp` bigint(20) NOT NULL DEFAULT '0',
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11) NOT NULL,
-  `ip_address` varchar(20) NOT NULL,
+  `uid` int(11) NOT NULL DEFAULT '0',
+  `ip_address` varchar(20) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
