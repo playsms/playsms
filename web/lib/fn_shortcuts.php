3acf7df2041a3db9912b7996ce9f7f29be1275e5
Anton Raharja <antonrd@gmail.com>
2015-02-23 22:36:24 +0700

    cleanup shortcuts

--- web/lib/fn_shortcuts.php
+++ web/lib/fn_shortcuts.php
@@ -10,21 +10,20 @@
  *
  * playSMS is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with playSMS.  If not, see <http://www.gnu.org/licenses/>.
+ * along with playSMS. If not, see <http://www.gnu.org/licenses/>.
  */
-
 defined('_SECURE_') or die('Forbidden');
 
 // lib/fn_core.php
 
 
-
 /**
  * Shortcut to core_print() for printing output to display
+ * 
  * @return string
  */
 function _p($content) {
@@ -32,7 +31,6 @@ function _p($content) {
 }
 
 // lib/fn_themes.php
-
 function _a($url, $title = '', $css_class = '', $css_id = '') {
 	return themes_link($url, $title, $css_class, $css_id);
 }
@@ -60,49 +58,69 @@ function _mandatory($text) {
 /**
  * Generate options for select HTML tag
  * Shortcut to themes_select_options()
- * @param  array  $options  Select options
- * @param  string $selected Selected option
- * @return string           Options for select HTML tag
+ * 
+ * @param array $options
+ *        Select options
+ * @param string $selected
+ *        Selected option
+ * @return string Options for select HTML tag
  */
-function _options($options = array() , $selected = '') {
+function _options($options = array(), $selected = '') {
 	return themes_select_options($options, $selected);
 }
 
 /**
  * Generate select HTML tag
  * Shortcut to themes_select()
- * @param  string $name     Tag name
- * @param  array  $options  Select options
- * @param  string $selected Selected option
- * @param  array  $tag_params  Additional input tag parameters
- * @param  string $css_id      CSS ID
- * @param  string $css_class   CSS class name
- * @return string           Select HTML tag
+ * 
+ * @param string $name
+ *        Tag name
+ * @param array $options
+ *        Select options
+ * @param string $selected
+ *        Selected option
+ * @param array $tag_params
+ *        Additional input tag parameters
+ * @param string $css_id
+ *        CSS ID
+ * @param string $css_class
+ *        CSS class name
+ * @return string Select HTML tag
  */
-function _select($name, $options = array() , $selected = '', $tag_options = array() , $css_id = '', $css_class = '') {
-	return themes_select($name, $options, $selected, $tag_options, $css_id, $css_class);
+function _select($name, $options = array(), $selected = '', $tag_params = array(), $css_id = '', $css_class = '') {
+	return themes_select($name, $options, $selected, $tag_params, $css_id, $css_class);
 }
 
 /**
  * Generate select HTML tag for yes-no or enabled-disabled type of options
  * Shortcut to themes_select_yesno()
- * @param  string  $name     Tag name
- * @param  boolean $selected TRUE if yes/enabled
- * @param  string  $yes      'Yes' or 'Enabled' option
- * @param  string  $no       'No' or 'Disabled' option
- * @param  array  $tag_params  Additional input tag parameters
- * @param  string $css_id      CSS ID
- * @param  string $css_class   CSS class name
- * @return string            Select HTML tag
+ * 
+ * @param string $name
+ *        Tag name
+ * @param boolean $selected
+ *        TRUE if yes/enabled
+ * @param string $yes
+ *        'Yes' or 'Enabled' option
+ * @param string $no
+ *        'No' or 'Disabled' option
+ * @param array $tag_params
+ *        Additional input tag parameters
+ * @param string $css_id
+ *        CSS ID
+ * @param string $css_class
+ *        CSS class name
+ * @return string Select HTML tag
  */
-function _yesno($name, $selected = '', $yes = '', $no = '', $tag_options = array() , $css_id = '', $css_class = '') {
-	return themes_select_yesno($name, $selected, $yes, $no, $tag_options, $css_id, $css_class);
+function _yesno($name, $selected = '', $yes = '', $no = '', $tag_params = array(), $css_id = '', $css_class = '') {
+	return themes_select_yesno($name, $selected, $yes, $no, $tag_params, $css_id, $css_class);
 }
 
 /**
  * Display error string from function parameter or session
  * Shortcut to themes_display_error_string()
- * @param  string $error_string Array of error strings (optional)
+ * 
+ * @param string $error_string
+ *        Array of error strings (optional)
  * @return string HTML string of error strings
  */
 function _err_display($error_string = array()) {
@@ -112,20 +130,26 @@ function _err_display($error_string = array()) {
 /**
  * Generate HTML input tag
  * Shortcut to themes_input()
- * @param  string $type        Input type
- * @param  string $name        Input name
- * @param  string $value       Input default value
- * @param  array  $tag_options Additional input tag parameters
- * @param  string $css_id      CSS ID
- * @param  string $css_class   CSS class name
- * @return string              HTML input tag
+ * 
+ * @param string $type
+ *        Input type
+ * @param string $name
+ *        Input name
+ * @param string $value
+ *        Input default value
+ * @param array $tag_params
+ *        Additional input tag parameters
+ * @param string $css_id
+ *        CSS ID
+ * @param string $css_class
+ *        CSS class name
+ * @return string HTML input tag
  */
-function _input($type = 'text', $name = '', $value = '', $tag_options = array() , $css_id = '', $css_class = '') {
-	return themes_input($type, $name, $value, $tag_options, $css_id, $css_class);
+function _input($type = 'text', $name = '', $value = '', $tag_params = array(), $css_id = '', $css_class = '') {
+	return themes_input($type, $name, $value, $tag_params, $css_id, $css_class);
 }
 
 // lib/fn_logger.php
-
 function _log($log, $level, $label) {
 	return logger_print($log, $level, $label);
 }
