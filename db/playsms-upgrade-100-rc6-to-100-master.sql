6ce65bd74bda84fbffc7cda822d4b2c255163875
Anton Raharja <antonrd@gmail.com>
2015-03-11 10:34:34 +0700

    remove long dash in sql db files, I have no idea that it could cause fail inserts

--- db/playsms-upgrade-100-rc6-to-100-master.sql
+++ db/playsms-upgrade-100-rc6-to-100-master.sql
@@ -1,5 +1,5 @@
 -- 1.0-master
----------------------------------------------------------------------------------------
+
 
 -- version
 UPDATE `playsms_tblRegistry` SET `registry_value` = '1.0-master' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
