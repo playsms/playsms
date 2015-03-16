7475d1e9b1a98d8ef8425f082d58e0f0a58172be
Anton Raharja <antonrd@gmail.com>
2015-03-16 23:35:47 +0700

    sms_subscribe send replies without footer

--- web/plugin/feature/sms_subscribe/sms_subscribe.php
+++ web/plugin/feature/sms_subscribe/sms_subscribe.php
@@ -753,7 +753,7 @@ switch (_OP_) {
 			if ($sms_to[0]) {
 				$unicode = core_detect_unicode($message);
 				$message = addslashes($message);
-				list($ok, $to, $smslog_id, $queue) = sendsms_helper($username, $sms_to, $message, 'text', $unicode, $smsc);
+				list($ok, $to, $smslog_id, $queue) = sendsms_helper($username, $sms_to, $message, 'text', $unicode, $smsc, TRUE);
 				if ($ok[0]) {
 					$counter++;
 					dba_update(_DB_PREF_ . '_featureSubscribe_msg', array(
