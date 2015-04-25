5b974705981531cf19d1356e04872644eeb39d87
Anton Raharja <antonrd@gmail.com>
2015-04-25 09:39:05 +0700

    fix #435 sendfromfile not working

--- web/plugin/feature/sendfromfile/sendfromfile.php
+++ web/plugin/feature/sendfromfile/sendfromfile.php
@@ -18,13 +18,13 @@
  */
 defined('_SECURE_') or die('Forbidden');
 
-if (! auth_isvalid()) {
+if (!auth_isvalid()) {
 	auth_block();
 }
 
 switch (_OP_) {
 	case 'list':
-		$content = _dialog().'<h2>' . _('Send from file') . '</h2><p />';
+		$content = _dialog() . '<h2>' . _('Send from file') . '</h2><p />';
 		if (auth_isadmin()) {
 			$info_format = _('destination number, message, username');
 		} else {
@@ -40,7 +40,7 @@ switch (_OP_) {
 							<p>" . _('Please select CSV file') . "</p>
 							<p><input type=\"file\" name=\"fncsv\"></p>
 							<p class=help-block>" . _('CSV file format') . " : " . $info_format . "</p>
-							<p><input type=checkbox name=fncsv_dup value=1 checked> "._('Prevent duplicates')."</p>
+							<p><input type=checkbox name=fncsv_dup value=1 checked> " . _('Prevent duplicates') . "</p>
 							<p><input type=\"submit\" value=\"" . _('Upload file') . "\" class=\"button\"></p>
 							</form>
 						</td>
@@ -52,46 +52,58 @@ switch (_OP_) {
 	case 'upload_confirm':
 		$filename = $_FILES['fncsv']['name'];
 		$fn = $_FILES['fncsv']['tmp_name'];
-		$fs = $_FILES['fncsv']['size'];
-		$nodups = $_POST['fncsv_dup'];
+		$fs = (int) $_FILES['fncsv']['size'];
+		$nodups = ($_REQUEST['fncsv_dup'] ? TRUE : FALSE);
 		$all_numbers = array();
-		$row = 0;
 		$valid = 0;
 		$invalid = 0;
+		$item_valid = array();
+		$item_invalid = array();
+		
 		if (($fs == filesize($fn)) && file_exists($fn)) {
 			if (($fd = fopen($fn, 'r')) !== FALSE) {
-				$sid = uniqid('SID', true);
+				$sid = md5(uniqid('SID', true));
 				$continue = true;
 				while ((($data = fgetcsv($fd, $fs, ',')) !== FALSE) && $continue) {
-					$row++;
 					$dup = false;
 					$sms_to = trim($data[0]);
 					$sms_msg = trim($data[1]);
 					if (auth_isadmin()) {
-						$sms_username = trim($data[2]);
-						$uid = user_username2uid($sms_username);
+						if ($sms_username = trim($data[2])) {
+							if ($uid = user_username2uid($sms_username)) {
+								$data[2] = $sms_username;
+							} else {
+								$sms_username = $user_config['username'];
+								$uid = $user_config['uid'];
+								$data[2] = $sms_username;
+							}
+						} else {
+							$sms_username = $user_config['username'];
+							$uid = $user_config['uid'];
+							$data[2] = $sms_username;
+						}
 					} else {
 						$sms_username = $user_config['username'];
 						$uid = $user_config['uid'];
 						$data[2] = $sms_username;
 					}
 					if ($nodups) {
-                                              if (in_array($sms_to,$all_numbers)) $dup = true;
+						if (in_array($sms_to, $all_numbers)) $dup = true;
 					}
 					if ($sms_to && $sms_msg && $uid && !$dup) {
-                                                $all_numbers[] = $sms_to;
+						$all_numbers[] = $sms_to;
 						$db_query = "INSERT INTO " . _DB_PREF_ . "_featureSendfromfile (uid,sid,sms_datetime,sms_to,sms_msg,sms_username) ";
 						$db_query .= "VALUES ('$uid','$sid','" . core_get_datetime() . "','$sms_to','" . addslashes($sms_msg) . "','$sms_username')";
 						if ($db_result = dba_insert_id($db_query)) {
+							$item_valid[$valid] = $data;
 							$valid++;
-							$item_valid[$valid - 1] = $data;
 						} else {
+							$item_invalid[$invalid] = $data;
 							$invalid++;
-							$item_invalid[$invalid - 1] = $data;
 						}
 					} else if ($sms_to || $sms_msg) {
+						$item_invalid[$invalid] = $data;
 						$invalid++;
-						$item_invalid[$invalid - 1] = $data;
 					}
 					$num_of_rows = $valid + $invalid;
 					if ($num_of_rows >= $sendfromfile_row_limit) {
@@ -100,71 +112,50 @@ switch (_OP_) {
 				}
 			}
 		} else {
-			$_SESSION['dialog']['info'][] = _('Invalid CSV file');
+			$_SESSION['dialog']['danger'][] = _('Invalid CSV file');
 			header("Location: " . _u('index.php?app=main&inc=feature_sendfromfile&op=list'));
 			exit();
 			break;
 		}
+		
 		$content = '<h2>' . _('Send from file') . '</h2><p />';
 		$content .= '<h3>' . _('Confirmation') . '</h3><p />';
 		$content .= _('Uploaded file') . ': ' . $filename . '<p />';
+		
 		if ($valid) {
 			$content .= _('Found valid entries in uploaded file') . ' (' . _('valid entries') . ': ' . $valid . ' ' . _('of') . ' ' . $num_of_rows . ')<p />';
-			$content .= '<h3>' . _('Valid entries') . '</h3>';
-			$content .= "
-				<div class=table-responsive>
-				<table class=playsms-table-list>
-				<thead><tr>
-					<th width=5%>*</th>
-					<th width=20%>" . _('Destination number') . "</th>
-					<th width=55%>" . _('Message') . "</th>
-					<th width=20%>" . _('Username') . "</th>
-				</tr></thead>
-				<tbody>";
-			$j = 0;
-			for ($i = 0; $i < count($item_valid); $i++) {
-				if ($item_valid[$i][0] && $item_valid[$i][1] && $item_valid[$i][2]) {
-					$j++;
-					$content .= "
-						<tr>
-							<td>&nbsp;" . $j . ".</td>
-							<td>" . $item_valid[$i][0] . "</td>
-							<td>" . $item_valid[$i][1] . "</td>
-							<td>" . $item_valid[$i][2] . "</td>
-						</tr>";
-				}
-			}
-			$content .= "</tbody></table></div>";
+			/*
+			 * $content .= '<h4>' . _('Valid entries') . '</h4>'; $content .= " <div class=table-responsive> <table class=playsms-table-list> <thead><tr> <th width=20%>" . _('Destination number') . "</th> <th width=60%>" . _('Message') . "</th> <th width=20%>" . _('Username') . "</th> </tr></thead> <tbody>"; $j = 0; foreach ($item_valid as $item) { if ($item[0] && $item[1] && $item[2]) { $content .= " <tr> <td>" . $item[0] . "</td> <td>" . $item[1] . "</td> <td>" . $item[2] . "</td> </tr>"; } } $content .= "</tbody></table></div>";
+			 */
 		}
+		
 		if ($invalid) {
 			$content .= '<p /><br />';
 			$content .= _('Found invalid entries in uploaded file') . ' (' . _('invalid entries') . ': ' . $invalid . ' ' . _('of') . ' ' . $num_of_rows . ')<p />';
-			$content .= '<h3>' . _('Invalid entries') . '</h3>';
+			$content .= '<h4>' . _('Invalid entries') . '</h4>';
 			$content .= "
 				<div class=table-responsive>
 				<table class=playsms-table-list>
 				<thead><tr>
-					<th width=4>*</th>
 					<th width='20%'>" . _('Destination number') . "</th>
 					<th width='60%'>" . _('Message') . "</th>
 					<th width='20%'>" . _('Username') . "</th>
 				</tr></thead>";
 			$j = 0;
-			for ($i = 0; $i < count($item_invalid); $i++) {
-				if ($item_invalid[$i][0] || $item_invalid[$i][1] || $item_invalid[$i][2]) {
-					$j++;
+			foreach ($item_invalid as $item) {
+				if ($item[0] && $item[1] && $item[2]) {
 					$content .= "
 						<tr>
-							<td>" . $j . ".</td>
-							<td>" . $item_invalid[$i][0] . "</td>
-							<td>" . $item_invalid[$i][1] . "</td>
-							<td>" . $item_invalid[$i][2] . "</td>
+							<td>" . $item[0] . "</td>
+							<td>" . $item[1] . "</td>
+							<td>" . $item[2] . "</td>
 						</tr>";
 				}
 			}
 			$content .= "</tbody></table></div>";
 		}
-		$content .= '<h3>' . _('Your choice') . ': </h3><p />';
+		
+		$content .= '<h4>' . _('Your choice') . '</h4><p />';
 		$content .= "<form action=\"index.php?app=main&inc=feature_sendfromfile&op=upload_cancel\" method=\"post\">";
 		$content .= _CSRF_FORM_ . "<input type=hidden name=sid value='" . $sid . "'>";
 		$content .= "<input type=\"submit\" value=\"" . _('Cancel send from file') . "\" class=\"button\"></p>";
@@ -173,43 +164,59 @@ switch (_OP_) {
 		$content .= _CSRF_FORM_ . "<input type=hidden name=sid value='" . $sid . "'>";
 		$content .= "<input type=\"submit\" value=\"" . _('Send SMS to valid entries') . "\" class=\"button\"></p>";
 		$content .= "</form>";
+		
 		_p($content);
 		break;
+	
 	case 'upload_cancel':
 		if ($sid = $_REQUEST['sid']) {
 			$db_query = "DELETE FROM " . _DB_PREF_ . "_featureSendfromfile WHERE sid='$sid'";
-			if ($db_result = dba_affected_rows($db_query)) {
-				$_SESSION['dialog']['info'][] = _('Send from file has been cancelled');
-			} else {
-				$_SESSION['dialog']['info'][] = _('Fail to remove cancelled entries from database');
-			}
+			dba_query($db_query);
+			$_SESSION['dialog']['danger'][] = _('Send from file has been cancelled');
 		} else {
-			$_SESSION['dialog']['info'][] = _('Invalid session ID');
+			$_SESSION['dialog']['danger'][] = _('Invalid session ID');
 		}
 		header("Location: " . _u('index.php?app=main&inc=feature_sendfromfile&op=list'));
 		exit();
 		break;
+	
 	case 'upload_process':
-		set_time_limit(600);
+		set_time_limit(0);
 		if ($sid = $_REQUEST['sid']) {
+			$data = array();
 			$db_query = "SELECT * FROM " . _DB_PREF_ . "_featureSendfromfile WHERE sid='$sid'";
 			$db_result = dba_query($db_query);
 			while ($db_row = dba_fetch_array($db_result)) {
 				$c_sms_to = $db_row['sms_to'];
-				$c_sms_msg = addslashes($db_row['sms_msg']);
 				$c_username = $db_row['sms_username'];
-				if ($c_sms_to && $c_sms_msg && $c_username) {
+				$c_sms_msg = addslashes($db_row['sms_msg']);
+				$c_hash = md5($c_username . $c_sms_msg);
+				if ($c_sms_to && $c_username && $c_sms_msg) {
+					$data[$c_hash]['username'] = $c_username;
+					$data[$c_hash]['message'] = $c_sms_msg;
+					$data[$c_hash]['sms_to'][] = $c_sms_to;
+				}
+			}
+			foreach ($data as $hash => $item) {
+				$username = $item['username'];
+				$message = $item['message'];
+				$sms_to = $item['sms_to'];
+				_log('hash:' . $hash . ' u:' . $username . ' m:[' . $message . '] to_count:' . count($sms_to), 3, 'sendfromfile upload_process');
+				if ($username && $message && count($sms_to)) {
 					$type = 'text';
-					$unicode = '0';
-					$c_sms_msg = addslashes($c_sms_msg);
-					list($ok, $to, $smslog_id, $queue) = sendsms_helper($c_username, $c_sms_to, $c_sms_msg, $type, $unicode);
+					if ($unicode = core_detect_unicode($message)) {
+						if (function_exists('mb_convert_encoding')) {
+							$message = mb_convert_encoding($message, "UCS-2BE", "auto");
+						}
+					}
+					list($ok, $to, $smslog_id, $queue) = sendsms_helper($username, $sms_to, $message, $type, $unicode);
 				}
 			}
 			$db_query = "DELETE FROM " . _DB_PREF_ . "_featureSendfromfile WHERE sid='$sid'";
-			$db_result = dba_affected_rows($db_query);
+			dba_query($db_query);
 			$_SESSION['dialog']['info'][] = _('SMS has been sent to valid numbers in uploaded file');
 		} else {
-			$_SESSION['dialog']['info'][] = _('Invalid session ID');
+			$_SESSION['dialog']['danger'][] = _('Invalid session ID');
 		}
 		header("Location: " . _u('index.php?app=main&inc=feature_sendfromfile&op=list'));
 		exit();
