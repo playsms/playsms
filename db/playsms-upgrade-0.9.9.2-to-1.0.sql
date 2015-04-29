31b8c0861bc23e252ad95a9e32b09e98507c8891
Anton Raharja <antonrd@gmail.com>
2015-04-29 23:48:31 +0700

    update db files

--- db/playsms-upgrade-0.9.9.2-to-1.0.sql
+++ db/playsms-upgrade-0.9.9.2-to-1.0.sql
@@ -743,3 +743,10 @@ UPDATE `playsms_tblRegistry` SET `registry_value` = '1.0-rc8' WHERE `registry_gr
 
 -- version
 UPDATE `playsms_tblRegistry` SET `registry_value` = '1.0-rc9' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
+
+
+-- 1.0
+
+
+-- version
+UPDATE `playsms_tblRegistry` SET `registry_value` = '1.0' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
