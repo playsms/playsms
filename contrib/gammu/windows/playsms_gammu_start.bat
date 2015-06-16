b35d80b529cc5ee6af19e453edb088260d9b5072
Anton Raharja <antonrd@gmail.com>
2015-06-16 10:34:34 +0700

    remove gammu for windows contrib, nobody maintains it

--- contrib/gammu/windows/playsms_gammu_start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call C:\playsms\etc\playsms.bat
-
-start "gammu" %GAMMU_BIN%\gammu-smsd -c C:\playsms\etc\gammu-smsdrc
-start "playsmsd_start" %PLAYSMS_BIN%\playsmsd_start.bat
-TASKKILL /F /FI "WINDOWTITLE eq playsmsd_start*"
--- contrib/gammu/windows/playsms_gammu_stop.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-TASKKILL /F /FI "WINDOWTITLE eq gammu*"
-TASKKILL /F /FI "WINDOWTITLE eq playsmsd*"
-TASKKILL /F /FI "WINDOWTITLE eq sendsmsd*"
