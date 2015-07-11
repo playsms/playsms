fe2f811301a8b92973595453e536d3bd7b0abc3c
Anton Raharja <antonrd@gmail.com>
2015-07-12 05:21:51 +0700

    playSMS version 1.1

--- db/playsms-upgrade_1.0_to_1.1.sql
+++ db/playsms-upgrade_1.0_to_1.1.sql
@@ -2,7 +2,7 @@
 
 
 -- version
-UPDATE `playsms_tblRegistry` SET `registry_value` = '1.1-master' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
+UPDATE `playsms_tblRegistry` SET `registry_value` = '1.1' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
 
 -- sms custom
 ALTER TABLE `playsms_featureCustom` CHANGE `custom_keyword` `custom_keyword` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
