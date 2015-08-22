432c5573175ecc571cb631e20f933272d5ab9fea
Anton Raharja <antonrd@gmail.com>
2015-08-22 17:26:02 +0700

    fix #479 the status shown that playSMS Daemon is not running although IT IS running

--- web/plugin/feature/playsmslog/config.php
+++ web/plugin/feature/playsmslog/config.php
@@ -3,7 +3,11 @@ defined('_SECURE_') or die('Forbidden');
 
 if (auth_isadmin()) {
 	$menutab = $core_config['menutab']['reports'];
-	$menu_config[$menutab][] = array("index.php?app=main&inc=feature_playsmslog&op=playsmslog_list", _('View log'));
+	$menu_config[$menutab][] = array(
+		"index.php?app=main&inc=feature_playsmslog&op=playsmslog_list",
+		_('View log') 
+	);
 }
 
-$plugin_config['playsmsd']['bin'] = '/usr/local/bin/playsmsd';
+$plugin_config['playsmslog']['playsmsd']['bin'] = '/usr/local/bin/playsmsd';
+$plugin_config['playsmslog']['playsmsd']['conf'] = '/etc/playsmsd.conf';
