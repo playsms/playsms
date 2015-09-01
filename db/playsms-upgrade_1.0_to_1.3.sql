66740dd5f64b08c1db236f36609cbe3c71c259c6
Anton Raharja <antonrd@gmail.com>
2015-09-01 21:39:07 +0700

    add update db to 1.3

---
+++ db/playsms-upgrade_1.0_to_1.3.sql
@@ -0,0 +1,86 @@
+-- 1.1-master
+
+
+-- version
+UPDATE `playsms_tblRegistry` SET `registry_value` = '1.1' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
+
+-- sms custom
+ALTER TABLE `playsms_featureCustom` CHANGE `custom_keyword` `custom_keyword` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
+ALTER TABLE `playsms_featureCustom` ADD `service_name` varchar(255) NOT NULL DEFAULT '' AFTER `uid` ;
+ALTER TABLE `playsms_featureCustom` ADD `sms_receiver` varchar(20) NOT NULL DEFAULT '' AFTER `custom_keyword` ;
+
+
+-- 1.2.1-master
+
+
+-- version
+UPDATE `playsms_tblRegistry` SET `registry_value` = '1.2.1' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
+
+-- feature outgoing
+ALTER TABLE `playsms_featureOutgoing` CHANGE `prefix` `prefix` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ;
+
+--
+-- Table structure for table `playsms_gatewayPlaynet_outgoing`
+--
+
+DROP TABLE IF EXISTS `playsms_gatewayPlaynet_outgoing`;
+CREATE TABLE `playsms_gatewayPlaynet_outgoing` (
+  `c_timestamp` bigint(20) NOT NULL DEFAULT '0',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` varchar(20) NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `last_update` varchar(20) NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `flag` int(11) NOT NULL DEFAULT '0',
+  `uid` int(11) NOT NULL DEFAULT '0',
+  `smsc` varchar(100) NOT NULL DEFAULT '',
+  `smslog_id` int(11) NOT NULL DEFAULT '0',
+  `sender_id` varchar(100) NOT NULL DEFAULT '',
+  `sms_to` varchar(100) NOT NULL DEFAULT '',
+  `message` text NOT NULL,
+  `sms_type` int(11) NOT NULL DEFAULT '0',
+  `unicode` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- core ACL
+ALTER TABLE `playsms_tblACL` ADD `flag_disallowed` INT(11) NOT NULL DEFAULT '0' AFTER `url`;
+
+--
+-- Table structure for table `playsms_gatewayJasmin`
+--
+
+DROP TABLE IF EXISTS `playsms_gatewayJasmin_log`;
+CREATE TABLE `playsms_gatewayJasmin_log` (
+  `c_timestamp` bigint(20) NOT NULL DEFAULT '0',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `local_smslog_id` int(11) NOT NULL DEFAULT '0',
+  `remote_smslog_id` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+-- 1.3-master
+
+
+-- version
+UPDATE `playsms_tblRegistry` SET `registry_value` = '1.3' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
+
+
+-- gateway plugin Generic
+
+--
+-- Table structure for table `playsms_gatewayGeneric`
+--
+
+DROP TABLE IF EXISTS `playsms_gatewayGeneric_log`;
+CREATE TABLE `playsms_gatewayGeneric_log` (
+  `c_timestamp` bigint(20) NOT NULL DEFAULT '0',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `local_smslog_id` int(11) NOT NULL DEFAULT '0',
+  `remote_smslog_id` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- playsmsd multi submission
+ALTER TABLE `playsms_tblSMSOutgoing_queue_dst` ADD `chunk` INT(11) NOT NULL DEFAULT '0' AFTER `queue_id`;
