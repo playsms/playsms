4302ddcda52e210069177293741481aed5c7b828
Anton Raharja <antonrd@gmail.com>
2015-09-01 15:12:43 +0700

    update db for 1.3

--- db/playsms-upgrade_1.2_or_1.2.1_to_1.3.sql
+++ db/playsms-upgrade_1.2_or_1.2.1_to_1.3.sql
@@ -2,7 +2,7 @@
 
 
 -- version
-UPDATE `playsms_tblRegistry` SET `registry_value` = '1.3-master' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
+UPDATE `playsms_tblRegistry` SET `registry_value` = '1.3' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
 
 
 -- gateway plugin Generic
@@ -20,3 +20,6 @@ CREATE TABLE `playsms_gatewayGeneric_log` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+-- playsmsd multi submission
+ALTER TABLE `playsms_tblSMSOutgoing_queue_dst` ADD `chunk` INT(11) NOT NULL DEFAULT '0' AFTER `queue_id`;
