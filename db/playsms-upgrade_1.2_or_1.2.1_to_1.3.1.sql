ef85596d95e7d716e0acaa9ebcd4b0081519d8d4
Anton Raharja <antonrd@gmail.com>
2015-09-28 22:17:25 +0700

    add and update version to 1.3.1

---
+++ db/playsms-upgrade_1.2_or_1.2.1_to_1.3.1.sql
@@ -0,0 +1,32 @@
+-- 1.3-master
+
+
+-- version
+UPDATE `playsms_tblRegistry` SET `registry_value` = '1.3' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
+
+
+-- gateway plugin Generic
+
+--
+-- Table structure for table `playsms_gatewayGeneric`
+--
+
+DROP TABLE IF EXISTS `playsms_gatewayGeneric_log`;
+CREATE TABLE `playsms_gatewayGeneric_log` (
+  `c_timestamp` bigint(20) NOT NULL DEFAULT '0',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `local_smslog_id` int(11) NOT NULL DEFAULT '0',
+  `remote_smslog_id` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- playsmsd multi submission
+ALTER TABLE `playsms_tblSMSOutgoing_queue_dst` ADD `chunk` INT(11) NOT NULL DEFAULT '0' AFTER `queue_id`;
+
+
+-- 1.3.1
+
+
+-- version
+UPDATE `playsms_tblRegistry` SET `registry_value` = '1.3.1' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
---
+++ db/playsms-upgrade_1.3_to_1.3.1.sql
@@ -0,0 +1,5 @@
+-- 1.3.1
+
+
+-- version
+UPDATE `playsms_tblRegistry` SET `registry_value` = '1.3.1' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
