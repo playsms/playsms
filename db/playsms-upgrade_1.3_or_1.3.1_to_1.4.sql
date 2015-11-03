54d6ac0751974da03ca0a49bf24222e346b8e160
Anton Raharja <antonrd@gmail.com>
2015-11-03 09:23:11 +0700

    change version to 1.4-master

---
+++ db/playsms-upgrade_1.3_or_1.3.1_to_1.4.sql
@@ -0,0 +1,5 @@
+-- 1.4-master
+
+
+-- version
+UPDATE `playsms_tblRegistry` SET `registry_value` = '1.4-master' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
