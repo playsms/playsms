d3fec720b8f4a04ee56b99d48fe30943295f9acc
Anton Raharja <antonrd@gmail.com>
2016-06-10 03:10:07 +0700

    MAJOR: add SQL query to update tblSMSOutgoing.parent_uid with tblUser.parent_uid

--- db/playsms-upgrade_1.3.1_to_1.4.sql
+++ db/playsms-upgrade_1.3.1_to_1.4.sql
@@ -6,3 +6,6 @@ UPDATE `playsms_tblRegistry` SET `registry_value` = '1.4-master' WHERE `registry
 
 -- tblSMSOutgoing
 ALTER TABLE `playsms_tblSMSOutgoing` ADD `parent_uid` INT(11) NOT NULL DEFAULT '0' AFTER `uid`;
+
+-- update tblSMSOutgoing.parent_uid with tblUser.parent_uid
+UPDATE `playsms_tblSMSOutgoing` A INNER JOIN `playsms_tblUser` B ON A.uid=B.uid SET A.parent_uid=B.parent_uid;
