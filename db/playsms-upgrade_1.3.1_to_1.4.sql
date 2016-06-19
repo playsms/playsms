f5c1eb7aed392c795cff1d53f8ab200de5afde07
Anton Raharja <antonrd@gmail.com>
2016-06-20 00:19:17 +0700

    update version to 1.4

--- db/playsms-upgrade_1.3.1_to_1.4.sql
+++ db/playsms-upgrade_1.3.1_to_1.4.sql
@@ -9,3 +9,6 @@ ALTER TABLE `playsms_tblSMSOutgoing` ADD `parent_uid` INT(11) NOT NULL DEFAULT '
 
 -- update tblSMSOutgoing.parent_uid with tblUser.parent_uid
 UPDATE `playsms_tblSMSOutgoing` A INNER JOIN `playsms_tblUser` B ON A.uid=B.uid SET A.parent_uid=B.parent_uid;
+
+-- version
+UPDATE `playsms_tblRegistry` SET `registry_value` = '1.4' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
