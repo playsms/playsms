74a2e4151e89f4eac109754baf4b325f6c51e03c
Anton Raharja <antonrd@gmail.com>
2016-06-26 00:54:33 +0700

    add adhoc_credit field

--- db/playsms-upgrade_1.3.1_to_1.4.sql
+++ db/playsms-upgrade_1.3.1_to_1.4.sql
@@ -12,3 +12,6 @@ UPDATE `playsms_tblSMSOutgoing` A INNER JOIN `playsms_tblUser` B ON A.uid=B.uid 
 
 -- version
 UPDATE `playsms_tblRegistry` SET `registry_value` = '1.4' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
+
+-- tblUser
+ALTER TABLE `playsms_tblUser` ADD `adhoc_credit` DECIMAL(13,3) NOT NULL DEFAULT '0.000' AFTER `credit`;
