47d002587d4bca21bd102283250da49fdc5ba8f9
Anton Raharja <antonrd@gmail.com>
2016-07-07 07:38:10 +0700

    formatting sms board

--- web/plugin/feature/sms_board/sms_board.php
+++ web/plugin/feature/sms_board/sms_board.php
@@ -10,18 +10,18 @@
  *
  * playSMS is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with playSMS.  If not, see <http://www.gnu.org/licenses/>.
+ * along with playSMS. If not, see <http://www.gnu.org/licenses/>.
  */
-
 defined('_SECURE_') or die('Forbidden');
 
 if (!auth_isvalid()) {
 	auth_block();
-};
+}
+;
 
 if ($board_id = $_REQUEST['board_id']) {
 	if (!($board_id = dba_valid(_DB_PREF_ . '_featureBoard', 'board_id', $board_id))) {
@@ -38,18 +38,18 @@ switch (_OP_) {
 			<table class=playsms-table-list>
 			<thead><tr>";
 		if (auth_isadmin()) {
-			$content.= "
+			$content .= "
 				<th width=20%>" . _('Keyword') . "</th>
 				<th width=50%>" . _('Forward') . "</th>
 				<th width=20%>" . _('User') . "</th>
 				<th width=10%>" . _('Action') . "</th>";
 		} else {
-			$content.= "
+			$content .= "
 				<th width=20%>" . _('Keyword') . "</th>
 				<th width=70%>" . _('Forward') . "</th>
 				<th width=10%>" . _('Action') . "</th>";
 		}
-		$content.= "
+		$content .= "
 			</tr></thead>
 			<tbody>";
 		if (!auth_isadmin()) {
@@ -61,13 +61,13 @@ switch (_OP_) {
 		while ($db_row = dba_fetch_array($db_result)) {
 			if ($owner = user_uid2username($db_row['uid'])) {
 				$action = "<a href=\"" . _u('index.php?app=main&inc=feature_sms_board&route=view&op=list&board_id=' . $db_row['board_id']) . "\">" . $icon_config['view'] . "</a>&nbsp;";
-				$action.= "<a href=\"" . _u('index.php?app=main&inc=feature_sms_board&op=sms_board_edit&board_id=' . $db_row['board_id']) . "\">" . $icon_config['edit'] . "</a>&nbsp;";
-				$action.= "<a href=\"javascript: ConfirmURL('" . _('Are you sure you want to delete SMS board with all its messages ?') . " (" . _('keyword') . ": " . $db_row['board_keyword'] . ")','" . _u('index.php?app=main&inc=feature_sms_board&op=sms_board_del&board_id=' . $db_row['board_id']) . "')\">" . $icon_config['delete'] . "</a>";
+				$action .= "<a href=\"" . _u('index.php?app=main&inc=feature_sms_board&op=sms_board_edit&board_id=' . $db_row['board_id']) . "\">" . $icon_config['edit'] . "</a>&nbsp;";
+				$action .= "<a href=\"javascript: ConfirmURL('" . _('Are you sure you want to delete SMS board with all its messages ?') . " (" . _('keyword') . ": " . $db_row['board_keyword'] . ")','" . _u('index.php?app=main&inc=feature_sms_board&op=sms_board_del&board_id=' . $db_row['board_id']) . "')\">" . $icon_config['delete'] . "</a>";
 				if (auth_isadmin()) {
 					$option_owner = "<td>$owner</td>";
 				}
 				$i++;
-				$content.= "
+				$content .= "
 					<tr>
 						<td>" . $db_row['board_keyword'] . "</td>
 						<td>" . $db_row['board_forward_email'] . "</td>
@@ -76,14 +76,14 @@ switch (_OP_) {
 					</tr>";
 			}
 		}
-		$content.= "
+		$content .= "
 			</tbody>
 			</table>
 			</div>
 			" . _button('index.php?app=main&inc=feature_sms_board&op=sms_board_add', _('Add SMS board'));
 		_p($content);
 		break;
-
+	
 	case "sms_board_edit":
 		$db_query = "SELECT * FROM " . _DB_PREF_ . "_featureBoard WHERE board_id='$board_id'";
 		$db_result = dba_query($db_query);
@@ -123,7 +123,7 @@ switch (_OP_) {
 			" . _back('index.php?app=main&inc=feature_sms_board&op=sms_board_list');
 		_p($content);
 		break;
-
+	
 	case "sms_board_edit_yes":
 		$edit_board_keyword = $_POST['edit_board_keyword'];
 		$edit_board_reply = trim($_POST['edit_board_reply']);
@@ -133,10 +133,10 @@ switch (_OP_) {
 		if ($board_id) {
 			if (!$edit_template) {
 				$edit_template = "<div class=sms_board_row>\n";
-				$edit_template.= "\t<div class=sender>{SENDER}</div>\n";
-				$edit_template.= "\t<div class=datetime>{DATETIME}</div>\n";
-				$edit_template.= "\t<div class=message>{MESSAGE}</div>\n";
-				$edit_template.= "</div>\n";
+				$edit_template .= "\t<div class=sender>{SENDER}</div>\n";
+				$edit_template .= "\t<div class=datetime>{DATETIME}</div>\n";
+				$edit_template .= "\t<div class=message>{MESSAGE}</div>\n";
+				$edit_template .= "</div>\n";
 			}
 			$db_query = "
 				UPDATE " . _DB_PREF_ . "_featureBoard
@@ -153,7 +153,7 @@ switch (_OP_) {
 		header("Location: " . _u('index.php?app=main&inc=feature_sms_board&op=sms_board_edit&board_id=' . $board_id));
 		exit();
 		break;
-
+	
 	case "sms_board_del":
 		$db_query = "SELECT board_keyword FROM " . _DB_PREF_ . "_featureBoard WHERE board_id='$board_id'";
 		$db_result = dba_query($db_query);
@@ -168,7 +168,7 @@ switch (_OP_) {
 		header("Location: " . _u('index.php?app=main&inc=feature_sms_board&op=sms_board_list'));
 		exit();
 		break;
-
+	
 	case "sms_board_add":
 		$content = _dialog() . "
 			<h2>" . _('Manage board') . "</h2>
@@ -194,7 +194,7 @@ switch (_OP_) {
 			" . _back('index.php?app=main&inc=feature_sms_board&op=sms_board_list');
 		_p($content);
 		break;
-
+	
 	case "sms_board_add_yes":
 		$add_board_keyword = strtoupper($_POST['add_board_keyword']);
 		$add_board_reply = trim($_POST['add_board_reply']);
@@ -205,10 +205,10 @@ switch (_OP_) {
 			if (keyword_isavail($add_board_keyword)) {
 				if (!$add_template) {
 					$add_template = "<div class=sms_board_row>\n";
-					$add_template.= "\t<div class=sender>{SENDER}</div>\n";
-					$add_template.= "\t<div class=datetime>{DATETIME}</div>\n";
-					$add_template.= "\t<div class=message>{MESSAGE}</div>\n";
-					$add_template.= "</div>\n";
+					$add_template .= "\t<div class=sender>{SENDER}</div>\n";
+					$add_template .= "\t<div class=datetime>{DATETIME}</div>\n";
+					$add_template .= "\t<div class=message>{MESSAGE}</div>\n";
+					$add_template .= "</div>\n";
 				}
 				$db_query = "
 					INSERT INTO " . _DB_PREF_ . "_featureBoard (uid,board_keyword,board_reply,board_forward_email,board_css,board_pref_template)
