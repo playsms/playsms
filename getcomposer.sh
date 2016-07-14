4aaa8f3646e4f093a70a5ba4e07687caa824aecf
Anton Raharja <antonrd@gmail.com>
2016-07-14 21:29:27 +0700

    no need root for getting and setup composer files

--- getcomposer.sh
+++ getcomposer.sh
@@ -2,14 +2,6 @@
 
 PATHSRC=$(pwd)
 
-USERID=$(id -u)
-if [ "$USERID" != "0" ]; then
-	echo
-	echo "ERROR: You need to run this script as root"
-	echo
-	exit 1
-fi
-
 echo
 echo "Getting composer from https://getcomposer.com"
 echo
@@ -21,10 +13,12 @@ cd /tmp/
 php -r "readfile('https://getcomposer.org/installer');" | php >/dev/null 2>&1
 
 if [ -e "composer.phar" ]; then
-	rm -f /usr/local/bin/composer /usr/local/bin/composer.phar >/dev/null 2>&1
-	ln -s composer.phar composer >/dev/null 2>&1
-	mv composer composer.phar /usr/local/bin/ >/dev/null 2>&1
-	chmod +x /usr/local/bin/composer /usr/local/bin/composer.phar >/dev/null 2>&1
+	#rm -f /usr/local/bin/composer /usr/local/bin/composer.phar >/dev/null 2>&1
+	rm -f ./composer >/dev/null 2>&1
+	ln -s ./composer.phar ./composer >/dev/null 2>&1
+	#mv composer composer.phar /usr/local/bin/ >/dev/null 2>&1
+	#chmod +x /usr/local/bin/composer /usr/local/bin/composer.phar >/dev/null 2>&1
+	chmod +x ./composer.phar >/dev/null 2>&1
 fi
 
 echo "Composer has been installed"
@@ -32,9 +26,9 @@ echo
 echo "Pleas wait while composer getting and updating required packages"
 echo
 
-if [ -x "/usr/local/bin/composer.phar" ]; then
+if [ -x "./composer.phar" ]; then
 	cd "$PATHSRC"
-	/usr/local/bin/composer.phar update
+	./composer.phar update
 else
 	echo "ERROR: unable to get composer from https://getcomposer.com"
 	echo
