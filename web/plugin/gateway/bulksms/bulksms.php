22c459cedfd1f257599ed23b5e7822bac33e3217
aristide <mendoo.aristide@gmail.com>
2016-11-07 17:15:59 +0000

    update bulksms gateway

--- web/plugin/gateway/bulksms/bulksms.php
+++ web/plugin/gateway/bulksms/bulksms.php
@@ -26,7 +26,7 @@ switch (_OP_) {
 				<td>" . _ ( 'Username' ) . "</td><td><input type=text maxlength=30 name=up_username value=\"" . $plugin_config['bulksms']['username'] . "\"></td>
 			</tr>
 			<tr>
-				<td>" . _ ( 'Password' ) . "</td><td><input type=password maxlength=30 name=up_password value=\"\"> " . _hint ( _ ( 'Fill to change the password' ) ) . "</td>
+				<td>" . _ ( 'Password' ) . "</td><td><input type=password maxlength=30 name=up_password value=\"" . $plugin_config['bulksms']['password'] . "\"> " . _hint ( _ ( 'Fill to change the password' ) ) . "</td>
 			</tr>
 			<tr>
 				<td>" . _ ( 'Module sender ID' ) . "</td><td><input type=text maxlength=16 name=up_module_sender value=\"" . $plugin_config['bulksms']['module_sender'] . "\"> " . _hint ( _ ( 'Max. 16 numeric or 11 alphanumeric char. empty to disable' ) ) . "</td>
@@ -61,25 +61,27 @@ switch (_OP_) {
 		$up_send_url = $_POST['up_send_url'];
 		$up_incoming_path = $_POST['up_incoming_path'];
 		$up_additional_param = ($_POST['up_additional_param'] ? $_POST['up_additional_param'] : "deliv_ack=1&callback=3");
-		$_SESSION['dialog']['info'][] = _ ( 'No changes have been made' );
-		if ($up_username && $up_send_url) {
-			if ($up_password) {
-				$password_change = "cfg_password='$up_password',";
-			}
-			$db_query = "
-				UPDATE " . _DB_PREF_ . "_gatewayBulksms_config
-				SET c_timestamp='" . mktime () . "',				
-				cfg_username='$up_username',
-				" . $password_change . "
-				cfg_module_sender='$up_module_sender',
-				cfg_datetime_timezone='$up_global_timezone',
-				cfg_send_url='$up_send_url',
-				cfg_additional_param='$up_additional_param',
-				cfg_incoming_path='$up_incoming_path'";
-			if (@dba_affected_rows ( $db_query )) {
-				$_SESSION['dialog']['info'][] = _ ( 'Gateway module configurations has been saved' );
-			}
+		
+		$password_change = '';
+		if ($up_password) {
+			$password_change = "cfg_password='$up_password',";
 		}
+		$db_query = "
+			UPDATE " . _DB_PREF_ . "_gatewayBulksms_config
+			SET c_timestamp='" . mktime () . "',				
+			cfg_username='$up_username',
+			" . $password_change . "
+			cfg_module_sender='$up_module_sender',
+			cfg_datetime_timezone='$up_global_timezone',
+			cfg_send_url='$up_send_url',
+			cfg_additional_param='$up_additional_param',
+			cfg_incoming_path='$up_incoming_path'";
+		if (@dba_affected_rows ( $db_query )) {
+			$_SESSION['dialog']['info'][] = _ ( 'Gateway module configurations has been saved' );
+		}else{
+			$_SESSION['dialog']['error'][] = _ ( 'Error: configurations not saved' );
+		}
+
 		header ( "Location: index.php?app=main&inc=gateway_bulksms&op=manage" );
 		exit ();
 		break;
