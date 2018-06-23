a39be0273bb8d7b09b051c2eb4c45657778ae4ce
Anton Raharja <antonrd@gmail.com>
2018-06-24 02:10:24 +0700

    change version from 1.5-dev to 1.4.1

---
+++ db/playsms-upgrade_1.4_to_1.4.1.sql
@@ -0,0 +1,5 @@
+-- 1.4.1
+
+
+-- version
+UPDATE `playsms_tblRegistry` SET `registry_value` = '1.4.1' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
