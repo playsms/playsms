6c097e881d4d93c0d53b469716caf700a99172d9
Anton Raharja <antonrd@gmail.com>
2020-02-05 20:43:25 +0700

    prepare to change md5 for password hashing

---
+++ db/playsms-upgrade_1.4.3_to_1.4.4.sql
@@ -0,0 +1,8 @@
+-- 1.4.4
+
+
+-- version
+UPDATE `playsms_tblRegistry` SET `registry_value` = '1.4.4-test' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
+
+-- tblUser
+ALTER TABLE `playsms_tblUser` ADD `salt` VARCHAR(255) NOT NULL DEFAULT '' AFTER `password`;
