3d6dd921d71bf78c17f95f81557c2a1b434ce426
Anton Raharja <antonrd@gmail.com>
2020-02-06 10:12:12 +0700

    update tblUser password column size

--- db/playsms-upgrade_1.4.3_to_1.4.4.sql
+++ db/playsms-upgrade_1.4.3_to_1.4.4.sql
@@ -5,4 +5,5 @@
 UPDATE `playsms_tblRegistry` SET `registry_value` = '1.4.4-test' WHERE `registry_group` = 'core' AND `registry_family` = 'config' AND `registry_key` = 'playsms_version' ;
 
 -- tblUser
-ALTER TABLE `playsms_tblUser` ADD `salt` VARCHAR(255) NOT NULL DEFAULT '' AFTER `password`;
+ALTER TABLE `playsms_tblUser` MODIFY COLUMN `password` VARCHAR(255) NOT NULL DEFAULT '' ;
+ALTER TABLE `playsms_tblUser` ADD `salt` VARCHAR(255) NOT NULL DEFAULT '' AFTER `password` ;
