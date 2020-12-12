ee389893c9a1ccccd6b1ae5bee2c6da8871277ac
Anton Raharja <antonrd@gmail.com>
2020-12-12 19:31:38 +0000

    sms_poll replace old pChart lib with szymach/c-pchart

--- web/plugin/feature/sms_poll/graph_poll.php
+++ web/plugin/feature/sms_poll/graph_poll.php
@@ -1,89 +1,36 @@
 <?php
 
-/**
- * This file is part of playSMS.
- *
- * playSMS is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * playSMS is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with playSMS. If not, see <http://www.gnu.org/licenses/>.
- */
 defined('_SECURE_') or die('Forbidden');
 
-$c_path = $core_config['apps_path']['plug'] . '/feature/sms_poll';
+error_reporting(0);
 
-include ($c_path . "/lib/pChart/pDraw.php");
-include ($c_path . "/lib/pChart/pData.php");
-include ($c_path . "/lib/pChart/pPie.php");
-include ($c_path . "/lib/pChart/pColor.php");
+use CpChart\Chart\Pie;
+use CpChart\Data;
+use CpChart\Image;
 
-use pChart\pDraw;
-use pChart\pPie;
-
-/* Create the pChart object */
-$myPicture = new pDraw(400, 200);
-
-/* Create and populate the pData object */
-//$myPicture = new pData();
-$myPicture->myData->addPoints($results, "ScoreA");
-$myPicture->myData->setSerieDescription("ScoreA", "Application A");
+/* Create and populate the Data object */
+$data = new Data();
+$data->addPoints($results, "ScoreA");
+$data->setSerieDescription("ScoreA", "Application A");
 
 /* Define the absissa serie */
-$myPicture->myData->addPoints($choices, "Labels");
-$myPicture->myData->setAbscissa("Labels");
+$data->addPoints($choices, "Labels");
+$data->setAbscissa("Labels");
+
+/* Create the Image object */
+$sms_poll_image_width = 400;
+$sms_poll_image_height = 200;
+$image = new Image($sms_poll_image_width, $sms_poll_image_height, $data, true);
 
 /* Create the pPie object */
-$PieChart = new pPie($myPicture);
+$pieChart = new Pie($image, $data);
 
-/* Enable shadow computing */
-$myPicture->setShadow(TRUE, array(
-	"X" => 3,
-	"Y" => 3,
-	"R" => 0,
-	"G" => 0,
-	"B" => 0,
-	"Alpha" => 10 
-));
-/* Draw a splitted pie chart */
-$PieChart->draw3DPie(150, 120, array(
-	"WriteValues" => TRUE,
-	"DataGapAngle" => 4,
-	"DataGapRadius" => 5,
-	"Border" => TRUE 
-));
-
-/* Write the legend */
-$myPicture->setShadow(TRUE, array(
-	"X" => 1,
-	"Y" => 1,
-	"R" => 0,
-	"G" => 0,
-	"B" => 0,
-	"Alpha" => 20 
-));
-
-/* Write the legend box */
-$myPicture->setFontProperties(array(
-	"FontName" => $c_path . "/lib/pChart/fonts/MankSans.ttf",
-	"FontSize" => 8,
-	"R" => 100,
-	"G" => 100,
-	"B" => 100 
-));
-$PieChart->drawPieLegend(3, 8, array(
-	"Style" => LEGEND_NOBORDER,
-	"Mode" => LEGEND_HORIZONTAL 
-));
+/* Draw an AA pie chart */
+$sms_poll_image_width_center = round($sms_poll_image_width / 2 * 0.95);
+$sms_poll_image_height_center = round($sms_poll_image_height / 2 * 1.18);
+$pieChart->draw3DPie($sms_poll_image_width_center, $sms_poll_image_height_center, ["DrawLabels" => true, "Border" => true]);
 
 /* Render the picture (choose the best way) */
 ob_end_clean();
 header('Content-disposition: filename=sms_poll_graph.png');
-$myPicture->autoOutput();
+$image->autoOutput();
