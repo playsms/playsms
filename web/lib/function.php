09ad96d907a1b0b449bf98083df123fa4967e0b4
Anton Raharja <antonrd@gmail.com>
2020-12-23 09:49:48 +0700

    move lib/fn_dlr.php to core/

--- web/lib/function.php
+++ web/lib/function.php
@@ -22,7 +22,6 @@ defined('_SECURE_') or die('Forbidden');
 // main functions
 include $core_config['apps_path']['libs']."/fn_phonebook.php";
 include $core_config['apps_path']['libs']."/fn_rate.php";
-include $core_config['apps_path']['libs']."/fn_dlr.php";
 include $core_config['apps_path']['libs']."/fn_webservices.php";
 include $core_config['apps_path']['libs']."/fn_keyword.php";
 
