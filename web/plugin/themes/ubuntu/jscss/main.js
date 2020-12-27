9b2f455f662ddcdaa2093e079d3135e67c93690e
Anton Raharja <antonrd@gmail.com>
2020-12-27 12:16:17 +0700

    default button css is now btn-default

--- web/plugin/themes/ubuntu/jscss/main.js
+++ web/plugin/themes/ubuntu/jscss/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('.button').addClass('btn');
-	$('.button').addClass('btn-primary');
+	$('.button').addClass('btn-default');
 	$('.playsms-table-list').addClass('table');
 	$('.playsms-table-list').addClass('table-striped');
 	$('.playsms-table-list').addClass('table-hover');
