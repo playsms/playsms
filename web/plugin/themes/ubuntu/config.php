21b153895b3c4cf20b20335ee973ed19f900fc25
Anton Raharja <antonrd@gmail.com>
2021-01-01 21:16:17 +0700

    empty sample config.php on themes

--- web/plugin/themes/ubuntu/config.php
+++ web/plugin/themes/ubuntu/config.php
@@ -1,19 +1,2 @@
 <?php
 defined('_SECURE_') or die('Forbidden');
-
-// default loaded page/plugin
-/*
-$themes_config['ubuntu']['main'] = array(
-	'default_inc' => 'feature_report',
-	'default_route' => 'user',
-	'default_op' => '',
-);
-*/
-
-// override common action icons
-/*
-$themes_config['ubuntu']['icon'] = array(
-	'edit' => "<span class='playsms-icon fas fa-edit' alt='"._('Edit')."' title='"._('Edit')."'></span>",
-	'delete' => "<span class='playsms-icon fas fa-trash' alt='"._('Delete')."' title='"._('Delete')."'></span>",
-);
-*/
