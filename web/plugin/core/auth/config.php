bd11fff5b8115bd68241156e7a2d663aa5d58902
Anton Raharja <antonrd@gmail.com>
2021-01-02 21:03:40 +0700

    update captcha size so it wont be cut out

--- web/plugin/core/auth/config.php
+++ web/plugin/core/auth/config.php
@@ -4,8 +4,8 @@ defined('_SECURE_') or die('Forbidden');
 // captcha options
 $auth_captcha_length = 6;
 $auth_captcha_seed = 'efhkmnpqrwxyz98765432';
-$auth_captcha_width = 160;
-$auth_captcha_height = 30;
+$auth_captcha_width = 150;
+$auth_captcha_height = 40;
 
 // enable/disable captcha
 $auth_captcha_form_login = TRUE;
