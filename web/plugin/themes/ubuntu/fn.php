154dc8a881cb7d4e0b26c6abaa0e5b0493aaf9fe
Anton Raharja <antonrd@gmail.com>
2021-01-02 12:00:16 +0700

    final for themes ubuntu bootstrap 3, after this it will be updated using bootstrap 4

--- web/plugin/themes/ubuntu/fn.php
+++ web/plugin/themes/ubuntu/fn.php
@@ -1,48 +1,7 @@
 <?php
 defined('_SECURE_') or die('Forbidden');
 
-function common_hook_themes_apply($content) {
-	global $core_config, $user_config;
-	
-	$themes_lang = strtolower(substr($user_config['language_module'], 0, 2));
-	
-	if ($themes_layout = trim($_SESSION['tmp']['themes']['layout'])) {
-		$themes_layout = 'themes_layout_' . $themes_layout;
-		unset($_SESSION['tmp']['themes']['layout']);
-	} else {
-		$themes_layout = 'themes_layout';
-	}
-	
-	$tpl = array(
-		'name' => $themes_layout,
-		'vars' => array(
-			'CONTENT' => $content,
-			'HTTP_PATH_BASE' => $core_config['http_path']['base'],
-			'HTTP_PATH_THEMES' => $core_config['http_path']['themes'],
-			'THEMES_MODULE' => core_themes_get(),
-			'THEMES_MENU_TREE' => themes_menu_tree(),
-			'THEMES_SUBMENU' => themes_submenu(),
-			'THEMES_LANG' => ($themes_lang ? $themes_lang : 'en'),
-			'CREDIT_SHOW_URL' => _u('index.php?app=ws&op=credit'),
-			'NAME' => $user_config['name'],
-			'USERNAME' => $user_config['username'],
-			'GRAVATAR' => $user_config['opt']['gravatar'],
-			'LAYOUT_FOOTER' => $core_config['main']['layout_footer'],
-			'Logout' => _('Logout') 
-		),
-		'ifs' => array(
-			'valid' => auth_isvalid() 
-		) 
-	);
-	$content = tpl_apply($tpl, array(
-		'core_config',
-		'user_config' 
-	));
-	
-	return $content;
-}
-
-function common_hook_themes_submenu($content = '') {
+function ubuntu_hook_themes_submenu($content = '') {
 	global $user_config;
 	
 	$separator = "&nbsp;&nbsp;&nbsp;";
@@ -67,7 +26,7 @@ function common_hook_themes_submenu($content = '') {
 	return $ret;
 }
 
-function common_hook_themes_menu_tree($menu_config) {
+function ubuntu_hook_themes_menu_tree($menu_config) {
 	global $core_config, $user_config, $icon_config;
 	
 	$main_menu = "";
@@ -128,145 +87,4 @@ function common_hook_themes_menu_tree($menu_config) {
 		</nav>";
 	
 	return $content;
-}
-
-function common_hook_themes_navbar($num, $nav, $max_nav, $url, $page) {
-	global $core_config;
-	
-	$nav_pages = "";
-	if ($num) {
-		$nav_start = ((($nav - 1) * $max_nav) + 1);
-		$nav_end = (($nav) * $max_nav);
-		$start = 1;
-		$end = ceil($num / $max_nav);
-		
-		$nav_pages = "<div class=playsms-nav-bar><ul class=pagination>";
-		$nav_pages .= "<li class=page-item><a class=page-link href='" . _u($url . '&page=1&nav=1') . "'> << </a></li>";
-		
-		if ($nav == $start) {
-			$nav_pages .= "";
-		} else {
-			$nav_pages .= "<li class=page-item><a class=page-link href='" . _u($url . '&page=' . ((($nav - 2) * $max_nav) + 1) . '&nav=' . ($nav - 1)) . "'> < </a></li>";
-		}
-		
-		for ($i = $nav_start; $i <= $nav_end; $i++) {
-			if ($i > $num) {
-				break;
-			}
-			
-			if ($i == $page) {
-				$page_active = 'active';
-			} else {
-				$page_active = '';
-			}
-			
-			$nav_pages .= "<li class='page-item " . $page_active . "'><a class=page-link href='" . _u($url . '&page=' . $i . '&nav=' . $nav) . "'>" . $i . "</a></li>";
-		}
-		
-		if ($nav == $end) {
-			$nav_pages .= "";
-		} else {
-			$nav_pages .= "<li class=page-item><a class=page-link href='" . _u($url . '&page=' . (($nav * $max_nav) + 1) . '&nav=' . ($nav + 1)) . "'> > </a></li>";
-		}
-
-		$nav_pages .= "<li class=page-item><a class=page-link href='" . _u($url . '&page=' . $num . '&nav=' . $end) . "'> >> </a></li>";
-		$nav_pages .= "</ul></div>";
-	}
-	
-	return $nav_pages;
-}
-
-function common_hook_themes_dialog($type, $message) {
-	$modal_id = uniqid();
-
-	$ret .= "<!-- Modal " . $modal_id . " -->";
-	$ret .= "
-		<div class='modal fade' id='dialog_box_" . $modal_id . "' tabindex='-1' role='dialog' aria-labelledby='dialog_box_title' aria-hidden='true'>
-			<div class='modal-dialog' role='document'>
-				<div class='modal-content'>
-					<div class='modal-header bg-" . $type . "'>
-						<h5 class='modal-title' id='dialog_box_title'>" . _('Information') . "</h5>
-					</div>
-					<div class='modal-body'>
-						" . $message . "
-					</div>
-					<div class='modal-footer'>
-						<button type='button' id='dialog_box_close_" . $modal_id . "' class='btn btn-primary' data-dismiss='modal'>" . _('Close') . "</button>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<script type='text/javascript'>
-			$(document).ready(function() {
-				$('#dialog_box_" . $modal_id . "').modal('show');
-				$('#dialog_box_" . $modal_id . "').on('shown.bs.modal', function(event) {
-					$('#dialog_box_close_" . $modal_id . "').focus();
-				});
-			});
-		</script>
-		<!-- /Modal " . $modal_id . "-->";
-
-	return $ret;
-}
-
-function common_hook_themes_dialog_confirmation($content, $url, $icon, $title, $form, $load, $nofooter) {
-	$modal_id = uniqid();
-	
-	if ($form) {
-		$action = "$('#" . $url . "').submit();";
-	} else {
-		$action = "window.location.href = \"" . $url . "\";";
-	}
-	
-	if ($load) {
-		$url_to_load = $content;
-		$content_load_js = "$('#dialog_confirmation_box_content_" . $modal_id . "').load('" . $url_to_load . "');";
-		$content = "";
-	}
-	
-	if ($nofooter) {
-		$modal_footer = "";
-	} else {
-		$modal_footer = ";
-			<div class='modal-footer'>
-				<button type='button' id='confirmation_button_no_" . $modal_id . "' class='btn btn-primary' data-dismiss='modal'>" . _('No') . "</button>
-				<button type='button' id='confirmation_button_yes_" . $modal_id . "' class='btn btn-danger' data-dismiss='modal'>" . _('Yes') . "</button>
-			</div>";
-	}
-	
-	$ret .= "<!-- Modal " . $modal_id . "-->";
-	$ret .= "<a href='#' data-toggle='modal' data-target='#dialog_confirmation_box_" . $modal_id . "' id='confirmation_icon_" . $modal_id . "' class='confirmation-icon'>" . $icon . "</a>";
-	$ret .= "
-		<div class='modal fade' id='dialog_confirmation_box_" . $modal_id . "' tabindex='-1' role='dialog' aria-labelledby='dialog_confirmation_box_title_" . $modal_id . "' aria-hidden='true'>
-			<div class='modal-dialog' role='document'>
-				<div class='modal-content'>
-					<div class='modal-header bg-danger'>
-						<h5 class='modal-title' id='dialog_confirmation_box_title_" . $modal_id . "'>" . $title . "</h5>
-					</div>
-					<div class='modal-body'>
-						<div id='dialog_confirmation_box_content_" . $modal_id . "'>
-							" . $content . "
-						</div>
-					</div>
-					" . $modal_footer . "
-				</div>
-			</div>
-		</div>
-		
-		<script type='text/javascript'>
-			$(document).ready(function() {
-				$('#dialog_confirmation_box_" . $modal_id . "').on('shown.bs.modal', function(event) {
-					$('#confirmation_button_no_" . $modal_id . "').focus();
-					$('#confirmation_button_yes_" . $modal_id . "').click(function() {
-						" . $action . "
-						return false;
-					});
-					" . $content_load_js . "
-				});
-			});
-		</script>
-		<!-- /Modal " . $modal_id . "-->";
-
-	return $ret;
 }
--- web/plugin/themes/ubuntu/jscss/main.js
+++ web/plugin/themes/ubuntu/jscss/main.js
@@ -1,15 +1,28 @@
 $(document).ready(function() {
-	$('.button').addClass('btn');
-	$('.button').addClass('btn-primary');
-	$('.playsms-table-list').addClass('table');
-	$('.playsms-table-list').addClass('table-striped');
-	$('.playsms-table-list').addClass('table-hover');
-	$('.playsms-table-list').addClass('table-condensed');
-	$('.playsms-table-list').tablesorter();
-	$('.playsms-table').addClass('table');
-	$('.playsms-table').addClass('table-hover');
-	$('.playsms-table').addClass('table-condensed');
-	$('.playsms-tooltip').tooltip();
-	$('.playsms-mandatory').tooltip();
-	$('.form-control').addClass('playsms-input-sizer');
+	$(".button").addClass("btn btn-primary");
+	
+	$(".playsms-table-list").addClass("table table-striped table-hover table-condensed");
+	$(".playsms-table-list").tablesorter();
+	
+	$(".playsms-table").addClass("table table-hover table-condensed");
+	
+	$(".playsms-tooltip").tooltip();
+	
+	$(".playsms-mandatory").tooltip();
+	
+	$(".form-control").addClass("playsms-input-sizer");
+	
+	$("input[type='text']").addClass("form-control playsms-input-sizer");
+	$("input[type='password']").addClass("form-control playsms-input-sizer");
+	$("input[type='file']").addClass("form-control playsms-input-sizer");
+	$("input[type='hidden']").addClass("playsms-input-sizer");
+	
+	$("input[type='text']").css("display", "inline");
+	$("input[type='password']").css("display", "inline");
+	$("input[type='file']").css("display", "inline");
+	$("input[type='hidden']").css("display", "inline");
+
+	$("textarea").addClass("form-control playsms-input-sizer");
+	
+	$("select").addClass("form-control playsms-input-sizer");	
 });
