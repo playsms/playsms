442c021456059d00bc8aa14cd73befde8ae2b73e
Anton Raharja <antonrd@gmail.com>
2021-12-08 20:49:20 +0700

    plugin gateway value can be zero, but cannot be empty

--- web/plugin/core/gateway/fn.php
+++ web/plugin/core/gateway/fn.php
@@ -123,7 +123,9 @@ function gateway_apply_smsc_config($smsc, $plugin_config) {
 		if ($smsc_data['name'] && $smsc_data['gateway'] && $smsc_data['data']) {
 			$smsc_config = json_decode($smsc_data['data'], TRUE);
 			foreach ($smsc_config as $key => $val) {
-				$plugin_config[$smsc_data['gateway']][$key] = $val;
+				if ($val !== '') {
+					$plugin_config[$smsc_data['gateway']][$key] = $val;
+				}
 			}
 		}
 	}
