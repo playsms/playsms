ea94938ac64ffaddd92ce3ede31cfa34e9e379f4
Anton Raharja <araharja@pm.me>
2023-11-13 02:24:10 +0700

    Update for better returns and adds phpdoc

--- storage/application/plugin/core/dba/fn.php
+++ storage/application/plugin/core/dba/fn.php
@@ -18,6 +18,16 @@
  */
 defined('_SECURE_') or die('Forbidden');
 
+/**
+ * Connect to database
+ * @param string $db_user Database user
+ * @param string $db_pass Database password
+ * @param string $db_name Database name
+ * @param string $db_host Database host default to 127.0.0.1
+ * @param string $db_port Database port default to 3306
+ * @param bool $persistant TRUE for persistant database connection
+ * @return PDO PHP Data Objects
+ */
 function dba_connect($db_user, $db_pass, $db_name, $db_host = '127.0.0.1', $db_port = 3306, $persistant = true)
 {
 	global $DBA_PDO;
@@ -66,6 +76,11 @@ function dba_connect($db_user, $db_pass, $db_name, $db_host = '127.0.0.1', $db_p
 	return $DBA_PDO;
 }
 
+/**
+ * Prepare statement
+ * @param string $db_query Prepared SQL statement
+ * @return PDOStatement|bool PDO statement or FALSE
+ */
 function _dba_prepare($db_query)
 {
 	global $DBA_PDO;
@@ -75,30 +90,44 @@ function _dba_prepare($db_query)
 			return $pdo_statement;
 		}
 	} catch (PDOException $e) {
-		_log(_('Exception') . ': ' . $e->getMessage() . ' ip:' . _REMOTE_ADDR_, 2, '_dba_prepare');
+		_log(_('Exception') . ': ' . $e->getMessage() . ' q:' . $db_query . ' ip:' . _REMOTE_ADDR_, 2, '_dba_prepare');
 		die(_('FATAL ERROR') . ' : ' . _('Database prepare statement operation has failed'));
 	}
 
 	return false;
 }
 
+/**
+ * Execute prepared statement
+ * @param PDOStatement $pdo_statement PDO statement
+ * @param array $db_argv SQL arguments
+ * @return bool TRUE for sucessful execution and FALSE for failure
+ */
 function _dba_execute($pdo_statement, $db_argv = [])
 {
+	$ret = false;
 	try {
 		if (is_array($db_argv) && $db_argv) {
-			$db_result = $pdo_statement->execute($db_argv);
+			$ret = $pdo_statement->execute($db_argv);
 		} else {
-			$db_result = $pdo_statement->execute();
+			$ret = $pdo_statement->execute();
 		}
 
 	} catch (PDOException $e) {
+		//_log(_('Exception') . ': ' . $e->getMessage() . ' q:' . $pdo_statement->queryString . ' argv:' . print_r($db_argv, true) . ' ip:' . _REMOTE_ADDR_, 2, '_dba_execute');
 		_log(_('Exception') . ': ' . $e->getMessage() . ' ip:' . _REMOTE_ADDR_, 2, '_dba_execute');
 		die(_('FATAL ERROR') . ' : ' . _('Database query execution has failed'));
 	}
 
-	return $db_result;
+	return $ret;
 }
 
+/**
+ * Query database
+ * @param string $db_query Prepared SQL statement
+ * @param array $db_argv SQL arguments
+ * @return PDOStatement|bool PDO statement or FALSE
+ */
 function dba_query($db_query, $db_argv = [])
 {
 	if ($pdo_statement = _dba_prepare($db_query)) {
@@ -110,6 +139,11 @@ function dba_query($db_query, $db_argv = [])
 	return false;
 }
 
+/**
+ * Fetch results as array indexed by column name
+ * @param PDOStatement $db_result PDO statement from dba_query()
+ * @return array|bool Array result or FALSE
+ */
 function dba_fetch_array($db_result)
 {
 	if ($db_result) {
@@ -119,6 +153,11 @@ function dba_fetch_array($db_result)
 	}
 }
 
+/**
+ * Fetch results as array indexed by column number
+ * @param PDOStatement $db_result PDO statement from dba_query()
+ * @return array|bool Array result or FALSE
+ */
 function dba_fetch_row($db_result)
 {
 	if ($db_result) {
@@ -128,6 +167,12 @@ function dba_fetch_row($db_result)
 	}
 }
 
+/**
+ * Query database and count the number of rows
+ * @param string $db_query Prepared SQL statement
+ * @param array $db_argv SQL arguments
+ * @return int Number of rows
+ */
 function dba_num_rows($db_query, $db_argv = [])
 {
 	if ($db_result = dba_query($db_query, $db_argv)) {
@@ -141,6 +186,12 @@ function dba_num_rows($db_query, $db_argv = [])
 	}
 }
 
+/**
+ * Delete or update database and count the number of updated rows
+ * @param string $db_query Prepared SQL statement
+ * @param array $db_argv SQL arguments
+ * @return int Number of updated rows
+ */
 function dba_affected_rows($db_query, $db_argv = [])
 {
 	if ($pdo_statement = _dba_prepare($db_query)) {
@@ -152,6 +203,12 @@ function dba_affected_rows($db_query, $db_argv = [])
 	return 0;
 }
 
+/**
+ * Insert into database and count the number of inserted rows
+ * @param string $db_query Prepared SQL statement
+ * @param array $db_argv SQL arguments
+ * @return int Number of inserted rows
+ */
 function dba_insert_id($db_query, $db_argv = [])
 {
 	global $DBA_PDO;
@@ -165,46 +222,119 @@ function dba_insert_id($db_query, $db_argv = [])
 	return 0;
 }
 
-function dba_search($db_table, $fields = '*', $conditions = '', $keywords = '', $extras = '', $join = '')
+/**
+ * Build SQL statement
+ * @param string $db_table DB table name prefixed with _DB_PREF_ or without
+ * @param array|string $fields Array or string of fields
+ * @param array $conditions List of WHERE conditions
+ * @param array $keywords List of WHERE conditions for LIKE
+ * @param array $extras List of extra SQL
+ * @param array|string $joins Array or string of JOIN SQL
+ * @return array List of a query and its argument array
+ */
+function dba_build($db_table, $fields = [], $conditions = [], $keywords = [], $extras = [], $joins = [])
 {
-	$ret = array();
+	$ret = ['', []];
 	$db_argv = [];
 
-	if ($fields) {
+	// db table
+	// $db_table can be prefixed with _DB_PREF_ or without
+	// Example: _DB_PREF_ . '_tblUser' or just 'tblUser'
+	$db_table = trim($db_table);
+	if (!(_DB_PREF_ && (string) substr($db_table, 0, strlen(_DB_PREF_)) === _DB_PREF_)) {
+		$db_table = _DB_PREF_ . '_' . $db_table;
+	}
+	if (!$db_table) {
+		return $ret;
+	}
+
+	// fields
+	$q_fields = '';
+	if (is_array($fields)) {
+		foreach ( $fields as $field ) {
+			$q_fields .= $field . ',';
+		}
+		$q_fields = substr($q_fields, 0, -1);
+	} else {
 		$q_fields = trim($fields);
 	}
+	$q_fields = empty($q_fields) ? '*' : $q_fields;
+
+	// WHERE conditions
+	$q_conditions = '';
 	if (is_array($conditions)) {
 		foreach ( $conditions as $key => $val ) {
 			$q_conditions .= "AND " . $key . "=? ";
 			$db_argv[] = $val;
 		}
 	}
+
+	// WHERE conditions wiht LIKE
+	$q_keywords = '';
 	if (is_array($keywords)) {
-		$q_keywords = "AND (";
 		foreach ( $keywords as $key => $val ) {
 			$q_keywords .= "OR " . $key . " LIKE ? ";
 			$db_argv[] = $val;
 		}
-		$q_keywords .= ")";
-		$q_keywords = str_replace("(OR", "(", $q_keywords);
+		if ($q_keywords = trim(str_replace("(OR", "(", $q_keywords))) {
+			$q_keywords = "AND (" . $q_keywords . ")";
+		}
 	}
-	$q_sql_where = trim($q_conditions . " " . $q_keywords);
+
+	// combine to get final WHERE conditions
+	$q_sql_where = '';
 	if ($q_conditions || $q_keywords) {
-		$q_where = 'WHERE';
-		$q_sql_where = substr($q_sql_where, 3);
+		$q_sql_where = trim($q_conditions . " " . $q_keywords);
+		if ($q_sql_where = substr($q_sql_where, 3)) {
+			$q_sql_where = "WHERE " . $q_sql_where;
+		}
 	}
+
+	// extra SQL such as GROUP BY, ORDER BY, LIMIT and OFFSET
+	$q_extras = '';
 	if (is_array($extras)) {
 		foreach ( $extras as $key => $val ) {
 			$q_extras .= $key . " " . $val . " ";
 		}
 	}
-	$db_query = trim("SELECT " . $q_fields . " FROM " . $db_table . " " . $join . " " . $q_where . " " . $q_sql_where . " " . $q_extras);
-	//_log("DEBUG q: ".$db_query, 2, "dba_search");
-	if ($db_argv) {
-		$db_result = dba_query($db_query, $db_argv);
+
+	// JOIN statements
+	$q_joins = '';
+	if (is_array($joins)) {
+		foreach ( $joins as $join ) {
+			$q_joins .= $join . ' ';
+		}
+		$q_joins = trim($q_joins);
 	} else {
-		$db_result = dba_query($db_query);
+		$q_joins = trim($joins);
 	}
+
+	$db_query = trim("SELECT " . $q_fields . " FROM " . $db_table . " " . $q_joins . " " . $q_sql_where . " " . $q_extras);
+	//_log("DEBUG q: " . $db_query, 2, "dba_build");
+
+	$ret = [$db_query, $db_argv];
+
+	return $ret;
+}
+
+
+/**
+ * Search database and returns array results
+ * Not recommended for large result unless you know what you are doing
+ * @param string $db_table DB table name prefixed with _DB_PREF_ or without
+ * @param array|string $fields Array or string of fields
+ * @param array $conditions List of WHERE conditions
+ * @param array $keywords List of WHERE conditions for LIKE
+ * @param array $extras List of extra SQL
+ * @param array|string $joins Array or string of JOIN SQL
+ * @return array
+ */
+function dba_search($db_table, $fields = [], $conditions = [], $keywords = [], $extras = [], $joins = [])
+{
+	$ret = [];
+
+	list($db_query, $db_argv) = dba_build($db_table, $fields, $conditions, $keywords, $extras, $joins);
+	$db_result = dba_query($db_query, $db_argv);
 	while ($db_row = dba_fetch_array($db_result)) {
 		$ret[] = $db_row;
 	}
@@ -212,42 +342,22 @@ function dba_search($db_table, $fields = '*', $conditions = '', $keywords = '', 
 	return $ret;
 }
 
-function dba_count($db_table, $conditions = '', $keywords = '', $extras = '', $join = '')
+/**
+ * Execute COUNT query and returns the number of rows
+ * Not recommended for large result unless you know what you are doing
+ * @param string $db_table DB table name prefixed with _DB_PREF_ or without
+ * @param array $conditions List of conditions
+ * @param array $keywords List of keywords
+ * @param array $extras List of extra SQL
+ * @param array|string $joins Array or string of JOIN SQL
+ * @return int
+ */
+function dba_count($db_table, $conditions = [], $keywords = [], $extras = [], $joins = [])
 {
 	$ret = 0;
-	$db_argv = [];
 
-	if (is_array($conditions)) {
-		foreach ( $conditions as $key => $val ) {
-			$q_conditions .= "AND " . $key . "=? ";
-			$db_argv[] = $val;
-		}
-	}
-	if (is_array($keywords)) {
-		$q_keywords = "AND (";
-		foreach ( $keywords as $key => $val ) {
-			$q_keywords .= "OR " . $key . " LIKE ? ";
-			$db_argv[] = $val;
-		}
-		$q_keywords .= ")";
-		$q_keywords = str_replace("(OR", "(", $q_keywords);
-	}
-	$q_sql_where = trim($q_conditions . " " . $q_keywords);
-	if ($q_conditions || $q_keywords) {
-		$q_where = 'WHERE';
-		$q_sql_where = substr($q_sql_where, 3);
-	}
-	if (is_array($extras)) {
-		foreach ( $extras as $key => $val ) {
-			$q_extras .= $key . " " . $val . " ";
-		}
-	}
-	$db_query = "SELECT COUNT(*) AS count FROM " . $db_table . " " . $join . " " . $q_where . " " . $q_sql_where . " " . $q_extras;
-	if ($db_argv) {
-		$db_result = dba_query($db_query, $db_argv);
-	} else {
-		$db_result = dba_query($db_query);
-	}
+	list($db_query, $db_argv) = dba_build($db_table, 'COUNT(*) AS count', $conditions, $keywords, $extras, $joins);
+	$db_result = dba_query($db_query, $db_argv);
 	if ($db_row = dba_fetch_array($db_result)) {
 		$ret = (int) $db_row['count'];
 	}
@@ -255,14 +365,27 @@ function dba_count($db_table, $conditions = '', $keywords = '', $extras = '', $j
 	return $ret;
 }
 
+/**
+ * Insert into database
+ * @param string $db_table DB table name prefixed with _DB_PREF_ or without
+ * @param array $items List of items to be inserted
+ * @return int Number of inserted rows
+ */
 function dba_add($db_table, $items)
 {
-	$ret = false;
 	$db_argv = [];
-	$sets = '';
-	$vals = '';
+
+	$db_table = trim($db_table);
+	if (!(_DB_PREF_ && (string) substr($db_table, 0, strlen(_DB_PREF_)) === _DB_PREF_)) {
+		$db_table = _DB_PREF_ . '_' . $db_table;
+	}
+	if (!$db_table) {
+		return 0;
+	}
 
 	if (is_array($items)) {
+		$sets = '';
+		$vals = '';
 		foreach ( $items as $key => $val ) {
 			$sets .= $key . ",";
 			$vals .= "?,";
@@ -272,114 +395,191 @@ function dba_add($db_table, $items)
 			$sets = substr($sets, 0, -1);
 			$vals = substr($vals, 0, -1);
 			$db_query = "INSERT INTO " . $db_table . " (" . $sets . ") VALUES (" . $vals . ")";
-			if ($c_id = dba_insert_id($db_query, $db_argv)) {
-				$ret = $c_id;
-			}
+			return dba_insert_id($db_query, $db_argv);
 		}
 	}
 
-	return $ret;
+	return 0;
 }
 
-function dba_update($db_table, $items, $condition = '', $operand = 'AND')
+/**
+ * Update database
+ * @param string $db_table DB table name prefixed with _DB_PREF_ or without
+ * @param array $items List of items to be updated
+ * @param array $conditions List of conditions
+ * @param string $operand Operand AND/OR to be used for multiple conditions
+ * @return int Number of updated rows
+ */
+function dba_update($db_table, $items, $conditions = [], $operand = 'AND')
 {
-	$ret = false;
 	$db_argv = [];
-	$q_condition = '';
+
+	$db_table = trim($db_table);
+	if (!(_DB_PREF_ && (string) substr($db_table, 0, strlen(_DB_PREF_)) === _DB_PREF_)) {
+		$db_table = _DB_PREF_ . '_' . $db_table;
+	}
+	if (!$db_table) {
+		return 0;
+	}
+
+	$operand = strtoupper(trim($operand));
+	$operand = $operand === 'AND' || $operand === 'OR' ? $operand : 'AND';
 
 	if (is_array($items)) {
+		$sets = '';
 		foreach ( $items as $key => $val ) {
 			$sets .= $key . "=?,";
 			$db_argv[] = $val;
 		}
 		if ($sets) {
 			$sets = substr($sets, 0, -1);
-			if (is_array($condition)) {
-				foreach ( $condition as $key => $val ) {
-					$q_condition .= $operand . " " . $key . "=? ";
+			if (is_array($conditions)) {
+				$q_conditions = '';
+				foreach ( $conditions as $key => $val ) {
+					$q_conditions .= $operand . " " . $key . "=? ";
 					$db_argv[] = $val;
 				}
-				if ($q_condition = trim($q_condition)) {
-					$q_condition = "WHERE " . substr($q_condition, 3);
+				if ($q_conditions = substr(trim($q_conditions), 3)) {
+					$q_conditions = "WHERE " . $q_conditions;
 				}
 			}
-			$db_query = "UPDATE " . $db_table . " SET " . $sets . " " . $q_condition;
-			if ($c_rows = dba_affected_rows($db_query, $db_argv)) {
-				$ret = $c_rows;
-			}
+			$db_query = "UPDATE " . $db_table . " SET " . $sets . " " . $q_conditions;
+			return dba_affected_rows($db_query, $db_argv);
 		}
 	}
 
-	return $ret;
+	return 0;
 }
 
-function dba_remove($db_table, $condition = '', $operand = 'AND')
+/**
+ * Remove from database
+ * @param string $db_table DB table name prefixed with _DB_PREF_ or without
+ * @param array $conditions List of conditions
+ * @param string $operand Operand AND/OR to be used for multiple conditions
+ * @return int Number of deleted rows
+ */
+function dba_remove($db_table, $conditions = [], $operand = 'AND')
 {
-	$ret = false;
 	$db_argv = [];
-	$q_condition = '';
 
-	if (is_array($condition)) {
-		foreach ( $condition as $key => $val ) {
-			$q_condition .= $operand . " " . $key . "=? ";
-			$db_argv[] = $val;
-		}
-		if ($q_condition = trim($q_condition)) {
-			$q_condition = "WHERE " . substr($q_condition, 3);
-		}
-		$db_query = "DELETE FROM " . $db_table . " " . $q_condition;
-		if ($c_rows = dba_affected_rows($db_query, $db_argv)) {
-			$ret = $c_rows;
-		}
+	$db_table = trim($db_table);
+	if (!(_DB_PREF_ && (string) substr($db_table, 0, strlen(_DB_PREF_)) === _DB_PREF_)) {
+		$db_table = _DB_PREF_ . '_' . $db_table;
+	}
+	if (!$db_table) {
+		return 0;
 	}
 
-	return $ret;
-}
-
-function dba_isavail($db_table, $conditions = '', $operand = 'OR')
-{
-	$ret = false;
-	$db_argv = [];
-	$q_condition = '';
+	$operand = strtoupper(trim($operand));
+	$operand = $operand === 'AND' || $operand === 'OR' ? $operand : 'AND';
 
 	if (is_array($conditions)) {
+		$q_conditions = '';
 		foreach ( $conditions as $key => $val ) {
-			$q_condition .= $operand . " " . $key . "=? ";
+			$q_conditions .= $operand . " " . $key . "=? ";
 			$db_argv[] = $val;
 		}
-		if ($q_condition = trim($q_condition)) {
-			$q_condition = "WHERE " . substr($q_condition, 3);
+		if ($q_conditions = substr(trim($q_conditions), 3)) {
+			$q_conditions = "WHERE " . $q_conditions;
+		}
+		$db_query = "DELETE FROM " . $db_table . " " . $q_conditions;
+		return dba_affected_rows($db_query, $db_argv);
+	}
+
+	return 0;
+}
+
+/**
+ * Check if specific data value does not exists so its available to be added later when needed
+ * @param string $db_table DB table name prefixed with _DB_PREF_ or without
+ * @param array $conditions List of conditions
+ * @param string $operand Operand AND/OR to be used for multiple conditions
+ * @return bool TRUE if data value does not exists thus available to be added later
+ */
+function dba_isavail($db_table, $conditions = [], $operand = 'OR')
+{
+	$db_argv = [];
+
+	$db_table = trim($db_table);
+	if (!(_DB_PREF_ && (string) substr($db_table, 0, strlen(_DB_PREF_)) === _DB_PREF_)) {
+		$db_table = _DB_PREF_ . '_' . $db_table;
+	}
+	if (!$db_table) {
+		return 0;
+	}
+
+	$operand = strtoupper(trim($operand));
+	$operand = $operand === 'AND' || $operand === 'OR' ? $operand : 'OR'; // default is OR
+
+	if (is_array($conditions)) {
+		$q_conditions = '';
+		foreach ( $conditions as $key => $val ) {
+			$q_conditions .= $operand . " " . $key . "=? ";
+			$db_argv[] = $val;
+		}
+		$q_conditions = trim($q_conditions);
+		if ($q_conditions = substr($q_conditions, 3)) {
+			$q_conditions = "WHERE " . $q_conditions;
+		}
+		$db_query = "SELECT * FROM " . $db_table . " " . $q_conditions . " LIMIT 1";
+		$db_result = dba_query($db_query, $db_argv);
+		if (dba_fetch_array($db_result)) {
+			return false; // data exists, so its not available
+		} else {
+			return true;
 		}
 	}
-	$db_query = "SELECT * FROM " . $db_table . " " . $q_condition . " LIMIT 1";
-	$db_result = dba_query($db_query, $db_argv);
-	if (dba_fetch_array($db_result)) {
-		$ret = false;
-	} else {
-		$ret = true;
-	}
 
-	return $ret;
+	return false;
 }
 
+/**
+ * Check if specific data value is exists
+ * This function is the opposite of dba_isavail()
+ * @param string $db_table DB table name prefixed with _DB_PREF_ or without
+ * @param array $conditions List of conditions
+ * @param string $operand Operand AND/OR to be used for multiple conditions
+ * @return bool TRUE if data value is exists
+ */
 function dba_isexists($db_table, $conditions = '', $operand = 'OR')
 {
-	$ret = (dba_isavail($db_table, $conditions, $operand) ? false : true);
-	return $ret;
+	return (dba_isavail($db_table, $conditions, $operand) ? false : true);
 }
 
+/**
+ * Check if field value pair is valid by checking their existance in database
+ * This function currently used by plugins in playSMS
+ * @param string $db_table DB table name prefixed with _DB_PREF_ or without
+ * @param string $field DB table's field name
+ * @param string $value DB table's field value
+ * @return bool TRUE if data is valid
+ */
 function dba_valid($db_table, $field, $value)
 {
 	global $user_config;
-	$ret = false;
+
+	$db_table = trim($db_table);
+	if (!(_DB_PREF_ && (string) substr($db_table, 0, strlen(_DB_PREF_)) === _DB_PREF_)) {
+		$db_table = _DB_PREF_ . '_' . $db_table;
+	}
+	if (!$db_table) {
+		return false;
+	}
+
+	$field = trim($field);
+	$value = trim($value);
+
 	if ($db_table && $field && $value) {
 		$conditions[$field] = $value;
 		if (!auth_isadmin()) {
 			$conditions['uid'] = $user_config['uid'];
 		}
-		if ($list = dba_search($db_table, $field, $conditions)) {
-			$ret = $list[0][$field];
+		if ($list = dba_search($db_table, $field, $conditions, [], ['LIMIT' => 1])) {
+			if (isset($list[0][$field]) && $list[0][$field]) {
+				return true;
+			}
 		}
 	}
-	return $ret;
+
+	return false;
 }
