994de74d02b1f3e52fe2a5fce50e3b29df3fded3
Anton Raharja <araharja@pm.me>
2023-11-17 08:39:21 +0700

    remove one of the double _INC_

--- web/init.php
+++ web/init.php
@@ -189,7 +189,7 @@ if ($_POST['X-CSRF-Token']) {
 		$c_last_post[$key] = $val;
 	}
 
-	$_SESSION['tmp']['last_post'][md5(trim(_APP_ . _INC_ . _ROUTE_ . _INC_))] = $c_last_post;
+	$_SESSION['tmp']['last_post'][md5(trim(_APP_ . _INC_ . _ROUTE_))] = $c_last_post;
 }
 
 // connect to database
