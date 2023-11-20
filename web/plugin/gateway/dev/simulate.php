817b7438306344099285c326f487e8d4bfe4ce5a
Anton Raharja <araharja@pm.me>
2023-11-20 21:39:08 +0700

    fix simulate incoming for multi line themes_dialog()

--- web/plugin/gateway/dev/simulate.php
+++ web/plugin/gateway/dev/simulate.php
@@ -23,18 +23,18 @@ if (!auth_isadmin()) {
 }
 
 switch (_OP_) {
-	case "simulate" :
+	case "simulate":
 		$sender = '629876543210';
 		$receiver = '1234';
 		$datetime = core_get_datetime();
-		
+
 		$content .= _dialog() . "
-			<h2>" . _('Simulate incoming SMS') . "</h2>
+			<h2 class=page-header-title>" . _('Simulate incoming SMS') . "</h2>
 			<form action=\"index.php?app=main&inc=gateway_dev&route=simulate&op=simulate_yes\" method=post>
 			" . _CSRF_FORM_ . "
 			<table class=playsms-table>
 				<tbody>
-				<tr><td class=label-sizer>" . _('Message') . "</td><td><input type=text name=message value=\"$message\" maxlength=250></td></tr>
+				<tr><td class=playsms-label-sizer>" . _('Message') . "</td><td><input type=text name=message value=\"$message\" maxlength=250></td></tr>
 				<tr><td>" . _('Sender') . "</td><td><input type=text name=sender value=\"$sender\" maxlength=20></td></tr>
 				<tr><td>" . _('Receiver') . "</td><td><input type=text name=receiver value=\"$receiver\" maxlength=20></td></tr>
 				<tr><td>" . _('Date/Time') . "</td><td><input type=text name=datetime value=\"" . core_display_datetime($datetime) . "\" maxlength=20></td></tr>
@@ -44,26 +44,26 @@ switch (_OP_) {
 			</form>";
 		_p($content);
 		break;
-	case "simulate_yes" :
+
+	case "simulate_yes":
 		$sms_sender = ($_REQUEST['sender'] ? $_REQUEST['sender'] : '629876543210');
 		$sms_receiver = ($_REQUEST['receiver'] ? $_REQUEST['receiver'] : '1234');
 		$sms_datetime = ($_REQUEST['datetime'] ? $_REQUEST['datetime'] : core_get_datetime());
 		$message = ($_REQUEST['message'] ? $_REQUEST['message'] : _('This is a test incoming SMS message'));
 		$message = htmlspecialchars_decode($message);
-		
+
 		if (trim($sms_sender) && trim($sms_receiver) && trim($sms_datetime) && trim($message)) {
 			recvsms($sms_datetime, $sms_sender, $message, $sms_receiver, 'dev');
-			$err[] = "Sender ID: " . $sms_sender;
-			$err[] = "Receiver number: " . $sms_receiver;
-			$err[] = "Sent: " . $sms_datetime;
-			$err[] = "Message: " . stripslashes($message);
-			_log(print_r($err, TRUE), 3, "dev incoming");
-			$_SESSION['dialog']['info'][] = $err;
+			$debug_msg[] = "Sender ID: " . $sms_sender;
+			$debug_msg[] = "Receiver number: " . $sms_receiver;
+			$debug_msg[] = "Sent: " . $sms_datetime;
+			$debug_msg[] = "Message: " . stripslashes($message);
+			_log(print_r($debug_msg, TRUE), 3, "dev incoming");
+			$_SESSION['dialog']['info'][] = $debug_msg;
 		} else {
 			$_SESSION['dialog']['info'][] = _('Fail to simulate incoming SMS');
 		}
-		
+
 		header("Location: " . _u('index.php?app=main&inc=gateway_dev&route=simulate&op=simulate'));
 		exit();
-		break;
 }
