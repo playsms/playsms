287f2ffcb28f8ebee101373eedea53347585b25c
Anton Raharja <araharja@pm.me>
2023-11-27 10:33:25 +0700

    add sendsmsd_limit, default ishttps to true

--- web/config-dist.php
+++ web/config-dist.php
@@ -1,19 +1,12 @@
 <?php
-// PHP PEAR DB compatible database engine:
-// mysql, mysqli, pgsql, odbc and others supported by PHP PEAR DB
-$core_config['db']['type'] = 'mysqli';		// database engine
+
+// Database connection configuration
+$core_config['db']['type'] = 'mysql';		// database engine
 $core_config['db']['host'] = '#DBHOST#';	// database host/server
 $core_config['db']['port'] = '#DBPORT#';	// database port
 $core_config['db']['user'] = '#DBUSER#';	// database username
 $core_config['db']['pass'] = '#DBPASS#';	// database password
 $core_config['db']['name'] = '#DBNAME#';	// database name
-
-// alternatively you can pass DSN and connect options
-// ref:
-// - http://pear.php.net/manual/en/package.database.db.intro-dsn.php
-// - http://pear.php.net/manual/en/package.database.db.intro-connect.php
-//$core_config['db']['dsn'] = 'mysql://root:password@localhost/playsms';
-//$core_config['db']['options'] = $options = array('debug' => 2, 'portability' => DB_PORTABILITY_ALL);
 
 // SMTP configuration
 $core_config['smtp']['relm'] = ''; // yes, not realm, it's relm
@@ -39,43 +32,46 @@ $core_config['apps_path']['logs'] = '#PATHLOG#';
 
 // log level: 0=disabled, 1=info, 2=warning, 3=debug, 4=verbose
 // WARNING: log level 3 and 4 will also save sensitive information such as password for used gateway
-$core_config['logstate']	= 2;
+$core_config['logstate'] = 2;
 
 // log file
-$core_config['logfile']		= 'playsms.log';
+$core_config['logfile'] = 'playsms.log';
 
 // WARNING: will log almost anything but passwords
-$core_config['logaudit']	= true;
+$core_config['logaudit'] = true;
 
 // log audit file
-$core_config['logauditfile']	= 'audit.log';
+$core_config['logauditfile'] = 'audit.log';
 
 // are we using http or https ? the default is using https instead http
-$core_config['ishttps']		= true;
+$core_config['ishttps'] = true;
 
 // are we using dlrd or not. the default is using dlrd
-$core_config['isdlrd']		= true;
+$core_config['isdlrd'] = true;
 
 // limit the number of DLR processed by dlrd in one time
-$core_config['dlrd_limit']	= 1000;
+$core_config['dlrd_limit'] = 1000;
 
 // are we using recvsmsd or not. the default is using recvsmsd
-$core_config['isrecvsmsd']	= true;
+$core_config['isrecvsmsd'] = true;
 
 // limit the number of incoming SMS processed by recvsmsd in one time
-$core_config['recvsmsd_limit']	= 1000;
+$core_config['recvsmsd_limit'] = 1000;
 
 // are we using sendsmsd or not. the default is using sendsmsd
-$core_config['issendsmsd']	= true;
+$core_config['issendsmsd'] = true;
 
 // limit the number of queue processed by sendsmsd in one time
-$core_config['sendsmsd_queue']	= 10;
+$core_config['sendsmsd_queue'] = 10;
 
 // limit the number of chunk per queue
-$core_config['sendsmsd_chunk']	= 20;
+$core_config['sendsmsd_chunk'] = 20;
 
 // chunk size
 $core_config['sendsmsd_chunk_size'] = 100;
 
+// limit the number of outgoing SMS processed by sendsmsd in one time
+$core_config['sendsmsd_limit'] = 1000;
+
 // webservices require username
-$core_config['webservices_username']	= true;
+$core_config['webservices_username'] = true;
