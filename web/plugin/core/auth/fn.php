45dc223c37973113c5ad25b2fd306eadef31c647
Anton Raharja <araharja@pm.me>
2023-11-27 10:47:49 +0700

    auth, formatting

--- web/plugin/core/auth/fn.php
+++ web/plugin/core/auth/fn.php
@@ -27,7 +27,8 @@ defined('_SECURE_') or die('Forbidden');
  *        Password
  * @return boolean TRUE when validated or boolean FALSE when validation failed
  */
-function auth_validate_login($username, $password) {
+function auth_validate_login($username, $password)
+{
 
 	// fixme anton - sanitize username
 	if (!($username && $username == core_sanitize_username($username))) {
@@ -38,13 +39,13 @@ function auth_validate_login($username, $password) {
 
 	$uid = user_username2uid($username);
 	_log('login attempt u:' . $username . ' uid:' . $uid . ' p:' . md5($password) . ' ip:' . $_SERVER['REMOTE_ADDR'], 3, 'auth_validate_login');
-	
+
 	// check blacklist
 	if (blacklist_ifipexists($username, $_SERVER['REMOTE_ADDR'])) {
 		_log('IP blacklisted u:' . $username . ' uid:' . $uid . ' ip:' . $_SERVER['REMOTE_ADDR'], 2, 'auth_validate_login');
 		return FALSE;
 	}
-	
+
 	if (user_banned_get($uid)) {
 		_log('user banned u:' . $username . ' uid:' . $uid . ' ip:' . $_SERVER['REMOTE_ADDR'], 2, 'auth_validate_login');
 		return FALSE;
@@ -56,10 +57,10 @@ function auth_validate_login($username, $password) {
 	$password = md5($password);
 	if ($password && $res_password && ($password === $res_password)) {
 		_log('valid login u:' . $username . ' uid:' . $uid . ' ip:' . $_SERVER['REMOTE_ADDR'], 2, 'auth_validate_login');
-		
+
 		// remove IP on successful login
 		blacklist_clearip($username, $_SERVER['REMOTE_ADDR']);
-		
+
 		return true;
 	} else {
 		$ret = registry_search(1, 'auth', 'tmp_password', $username);
@@ -69,17 +70,17 @@ function auth_validate_login($username, $password) {
 			if (!registry_remove(1, 'auth', 'tmp_password', $username)) {
 				_log('WARNING: unable to remove temporary password after successful login', 3, 'login');
 			}
-			
+
 			// remove IP on successful login
 			blacklist_clearip($username, $_SERVER['REMOTE_ADDR']);
-			
+
 			return true;
 		}
 	}
-	
+
 	// check blacklist
 	blacklist_checkip($username, $_SERVER['REMOTE_ADDR']);
-	
+
 	_log('invalid login u:' . $username . ' uid:' . $uid . ' ip:' . $_SERVER['REMOTE_ADDR'], 2, 'auth_validate_login');
 	return false;
 }
@@ -93,7 +94,8 @@ function auth_validate_login($username, $password) {
  *        Password
  * @return boolean TRUE when validated or boolean FALSE when validation failed
  */
-function auth_validate_email($email, $password) {
+function auth_validate_email($email, $password)
+{
 	$username = user_email2username($email);
 	_log('login attempt email:' . $email . ' u:' . $username . ' p:' . md5($password) . ' ip:' . $_SERVER['REMOTE_ADDR'], 3, 'auth_validate_email');
 	return auth_validate_login($username, $password);
@@ -106,55 +108,56 @@ function auth_validate_email($email, $password) {
  *        Token
  * @return string User ID when validated or boolean FALSE when validation failed
  */
-function auth_validate_token($token) {
+function auth_validate_token($token)
+{
 	$token = trim($token);
 	if (_APP_ == 'main' || _APP_ == 'menu') {
 		_log('login attempt token:' . $token . ' ip:' . $_SERVER['REMOTE_ADDR'], 3, 'auth_validate_token');
 	}
-	
+
 	if ($token) {
 		$db_query = "SELECT uid,username,enable_webservices,webservices_ip FROM " . _DB_PREF_ . "_tblUser WHERE flag_deleted='0' AND token='$token'";
 		$db_result = dba_query($db_query);
 		$db_row = dba_fetch_array($db_result);
 		$username = trim($db_row['username']);
-		
+
 		// check blacklist
 		if (blacklist_ifipexists($username, $_SERVER['REMOTE_ADDR'])) {
 			_log('IP blacklisted u:' . $username . ' uid:' . user_username2uid($username) . ' ip:' . $_SERVER['REMOTE_ADDR'], 2, 'auth_validate_login');
-			
+
 			return FALSE;
 		}
-		
+
 		if (($uid = trim($db_row['uid'])) && $username && ($db_row['enable_webservices'])) {
 			$ip = explode(',', $db_row['webservices_ip']);
 			if (is_array($ip)) {
-				foreach ($ip as $key => $net) {
+				foreach ( $ip as $key => $net ) {
 					if (core_net_match($net, $_SERVER['REMOTE_ADDR'])) {
 						if (user_banned_get($uid)) {
 							_log('user banned u:' . $username . ' uid:' . $uid . ' ip:' . $_SERVER['REMOTE_ADDR'], 2, 'auth_validate_token');
-							
+
 							return FALSE;
 						}
-						
+
 						if (_APP_ == 'main' || _APP_ == 'menu') {
 							_log('valid login u:' . $username . ' uid:' . $uid . ' ip:' . $_SERVER['REMOTE_ADDR'], 2, 'auth_validate_token');
 						}
-						
+
 						// remove IP on successful login
 						blacklist_clearip($username, $_SERVER['REMOTE_ADDR']);
-						
+
 						return $uid;
 					}
 				}
 			}
 		}
 	}
-	
+
 	// check blacklist
 	blacklist_checkip($username, $_SERVER['REMOTE_ADDR']);
-	
+
 	_log('invalid login t:' . $token . ' ip:' . $_SERVER['REMOTE_ADDR'], 2, 'auth_validate_token');
-	
+
 	return FALSE;
 }
 
@@ -163,16 +166,17 @@ function auth_validate_token($token) {
  *
  * @return boolean TRUE if valid
  */
-function auth_isvalid() {
+function auth_isvalid()
+{
 	if ($_SESSION['sid'] && $_SESSION['uid'] && $_SESSION['valid']) {
 		$hash = user_session_get('', $_SESSION['sid']);
 		if (is_array($hash) && isset($hash[key($hash)]['sid']) && isset($hash[key($hash)]['uid'])) {
 			if ($_SESSION['sid'] == $hash[key($hash)]['sid'] && $_SESSION['uid'] == $hash[key($hash)]['uid']) {
 				return acl_checkurl($_SERVER['QUERY_STRING'], $_SESSION['uid']);
-			}	
+			}
 		}
 	}
-	
+
 	return FALSE;
 }
 
@@ -181,7 +185,8 @@ function auth_isvalid() {
  *
  * @return boolean TRUE if valid and visitor has admin access level
  */
-function auth_isadmin() {
+function auth_isadmin()
+{
 	if ((int) $_SESSION['status'] === 2) {
 		if (auth_isvalid()) {
 			return TRUE;
@@ -195,7 +200,8 @@ function auth_isadmin() {
  *
  * @return boolean TRUE if valid and visitor has user access level
  */
-function auth_isuser() {
+function auth_isuser()
+{
 	if ((int) $_SESSION['status'] === 3) {
 		if (auth_isvalid()) {
 			return TRUE;
@@ -209,7 +215,8 @@ function auth_isuser() {
  *
  * @return boolean TRUE if valid and visitor has subuser access level
  */
-function auth_issubuser() {
+function auth_issubuser()
+{
 	if ((int) $_SESSION['status'] === 4) {
 		if (auth_isvalid()) {
 			return TRUE;
@@ -225,7 +232,8 @@ function auth_issubuser() {
  *        Account status
  * @return boolean TRUE if valid and visitor has certain user status
  */
-function auth_isstatus($status) {
+function auth_isstatus($status)
+{
 	if ((int) $_SESSION['status'] && (int) $_SESSION['status'] === (int) $status) {
 		if (auth_isvalid()) {
 			return TRUE;
@@ -241,7 +249,8 @@ function auth_isstatus($status) {
  *        Access Control List
  * @return boolean TRUE if valid and visitor has certain ACL
  */
-function auth_isacl($acl) {
+function auth_isacl($acl)
+{
 	if (auth_isvalid()) {
 		if (auth_isadmin()) {
 			return TRUE;
@@ -259,7 +268,8 @@ function auth_isacl($acl) {
 /**
  * Display page for blocked access
  */
-function auth_block() {
+function auth_block()
+{
 	header("Location: " . _u('index.php?app=main&inc=core_auth&route=block&op=block'));
 	exit();
 }
@@ -270,9 +280,10 @@ function auth_block() {
  * @param string $username
  *        Username
  */
-function auth_session_setup($uid) {
+function auth_session_setup($uid)
+{
 	global $core_config;
-	
+
 	// regenerate new session ID
 	session_regenerate_id(TRUE);
 
@@ -287,7 +298,7 @@ function auth_session_setup($uid) {
 		if (!is_array($_SESSION['tmp']['login_as'])) {
 			$_SESSION['tmp']['login_as'] = array();
 		}
-		
+
 		// save session in registry
 		if (!$core_config['daemon_process']) {
 			user_session_set($c_user['uid']);
@@ -325,26 +336,29 @@ function auth_session_destroy()
 	_log("session destroyed uid:" . $uid . " hash:" . $login_sid, 2, "auth_session_destroy");
 }
 
-function auth_login_as($uid) {
-	
+function auth_login_as($uid)
+{
+
 	// save current login
 	array_unshift($_SESSION['tmp']['login_as'], $_SESSION['uid']);
-	
+
 	// setup new session
 	auth_session_setup($uid);
 }
 
-function auth_login_return() {
-	
+function auth_login_return()
+{
+
 	// get previous login
 	$previous_login = $_SESSION['tmp']['login_as'][0];
 	array_shift($_SESSION['tmp']['login_as']);
-	
+
 	// return to previous session
 	auth_session_setup($previous_login);
 }
 
-function auth_login_as_check() {
+function auth_login_as_check()
+{
 	if (isset($_SESSION['tmp']['login_as'])) {
 		if (count($_SESSION['tmp']['login_as']) > 0) {
 			return TRUE;
