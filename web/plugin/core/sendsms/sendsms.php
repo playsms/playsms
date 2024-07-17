d04a7781ec63f871f3db70cb712531ab8c962929
Anton Raharja <araharja@pm.me>
2024-07-17 04:08:06 +0000

    update sendsms filter inputs

--- web/plugin/core/sendsms/sendsms.php
+++ web/plugin/core/sendsms/sendsms.php
@@ -22,17 +22,27 @@ if (!auth_isvalid()) {
 	auth_block();
 }
 
+$_REQUEST['to'] = _t($_REQUEST['to']);
+$_REQUEST['message'] = _t($_REQUEST['message']);
+$_REQUEST['popup'] = (int) $_REQUEST['popup'];
+$_REQUEST['sms_sender'] = _t($_REQUEST['sms_sender']);
+$_REQUEST['sms_footer'] = _t($_REQUEST['sms_footer']);
+$_REQUEST['sms_schedule'] = _t($_REQUEST['sms_schedule']);
+$_REQUEST['msg_flash'] = _t($_REQUEST['msg_flash']);
+$_REQUEST['msg_unicode'] = _t($_REQUEST['msg_unicode']);
+$_REQUEST['p_num_text'] = _t($_REQUEST['p_num_text']);
+
 switch (_OP_) {
 	case "sendsms":
-		
+
 		// get $to and $message from session or query string
 		$to = stripslashes($_REQUEST['to']);
 		$message = (stripslashes($_REQUEST['message']) ? stripslashes($_REQUEST['message']) : trim(stripslashes($_SESSION['tmp']['message'])));
-		
+
 		// sender ID
 		$sms_from = sendsms_get_sender($user_config['username']);
 		$ismatched = FALSE;
-		foreach (sender_id_getall($user_config['username']) as $sender_id) {
+		foreach ( sender_id_getall($user_config['username']) as $sender_id ) {
 			$selected = '';
 			if (strtoupper($sms_from) == strtoupper($sender_id)) {
 				$selected = 'selected';
@@ -41,29 +51,29 @@ switch (_OP_) {
 			$option_values .= "<option value=\"" . $sender_id . "\" title=\"" . $sender_id . "\" " . $selected . ">" . $sender_id . "</option>";
 		}
 		$sms_sender_id = "<select name=sms_sender style='width: 100%'>" . $option_values . "</select>";
-		
+
 		if (!$ismatched) {
 			$sms_sender_id = "<input type='text' style='width: 100%' name='sms_sender' value='" . $sms_from . "' readonly>";
 		}
-		
+
 		// SMS footer
-		$sms_footer = $user_config['footer'];
-		
+		$sms_footer = _t($user_config['footer']);
+
 		// message template
 		$option_values = "<option value=\"\" default>--" . _('Please select template') . "--</option>";
 		$c_templates = sendsms_get_template();
 		if (is_array($c_templates)) {
 			for ($i = 0; $i < count($c_templates); $i++) {
-				$option_values .= "<option value=\"" . $c_templates[$i]['text'] . "\" title=\"" . $c_templates[$i]['text'] . "\">" . $c_templates[$i]['title'] . "</option>";
-				$input_values .= "<input type=\"hidden\" name=\"content_" . $i . "\" value=\"" . $c_templates[$i]['text'] . "\">";
+				$option_values .= "<option value=\"" . _t($c_templates[$i]['text']) . "\" title=\"" . _t($c_templates[$i]['text']) . "\">" . _t($c_templates[$i]['title']) . "</option>";
+				$input_values .= "<input type=\"hidden\" name=\"content_" . $i . "\" value=\"" . _t($c_templates[$i]['text']) . "\">";
 			}
 			if ($c_templates[0]) {
 				$sms_template = "<div id=msg_template><select name=smstemplate id=msg_template_select style='width: 100%' onClick=\"SetSmsTemplate();\">$option_values</select></div>";
 			}
 		}
-		
+
 		$layout = ($_REQUEST['popup'] == 1 ? 'sendsms_popup' : 'sendsms');
-		
+
 		// build form
 		unset($tpl);
 		$tpl = array(
@@ -91,7 +101,7 @@ switch (_OP_) {
 				'sms_sender_id' => $sms_sender_id,
 				'sms_template' => $sms_template,
 				'return_url' => $_REQUEST['return_url'],
-				
+
 				// 'sms_schedule' => core_display_datetime(core_get_datetime()),
 				'sms_schedule' => '',
 				'message' => $message,
@@ -102,68 +112,68 @@ switch (_OP_) {
 				'max_sms_length_unicode' => $user_config['opt']['max_sms_length_unicode'],
 				'lang' => substr($user_config['language_module'], 0, 2),
 				'chars' => _('chars'),
-				'SMS' => _('SMS') 
+				'SMS' => _('SMS')
 			),
 			'ifs' => array(
-				'calendar' => file_exists($core_config['apps_path']['themes'] . '/common/jscss/bootstrap-datetimepicker/bootstrap-datetimepicker.' . substr($user_config['language_module'], 0, 2) . '.js') 
-			) 
+				'calendar' => file_exists($core_config['apps_path']['themes'] . '/common/jscss/bootstrap-datetimepicker/bootstrap-datetimepicker.' . substr($user_config['language_module'], 0, 2) . '.js')
+			)
 		);
 		_p(tpl_apply($tpl));
 		break;
-	
+
 	case "sendsms_yes":
-		
+
 		// popup related
 		$return_url = trim(htmlspecialchars_decode($_REQUEST['return_url']));
 		if ($_REQUEST['submit'] == _('Cancel')) {
 			header("Location: " . $return_url);
 			exit();
 		}
-		
+
 		// sender ID
 		$sms_sender = trim($_REQUEST['sms_sender']);
-		
+
 		// SMS footer
 		$sms_footer = trim($_REQUEST['sms_footer']);
-		
+
 		// nofooter option
 		$nofooter = true;
 		if ($sms_footer) {
 			$nofooter = false;
 		}
-		
+
 		// schedule option
 		$sms_schedule = trim($_REQUEST['sms_schedule']);
-		
+
 		// type of SMS, text or flash
 		$msg_flash = $_REQUEST['msg_flash'];
 		$sms_type = "text";
 		if ($msg_flash == "on") {
 			$sms_type = "flash";
 		}
-		
+
 		// unicode or not
 		$msg_unicode = $_REQUEST['msg_unicode'];
 		$unicode = "0";
 		if ($msg_unicode == "on") {
 			$unicode = "1";
 		}
-		
+
 		// SMS message
 		$message = $_REQUEST['message'];
-		
+
 		// save it in session for next form
 		$_SESSION['tmp']['message'] = $message;
-		
+
 		// destination numbers
 		if ($sms_to = trim($_REQUEST['p_num_text'])) {
 			$sms_to = explode(',', $sms_to);
 		}
-		
+
 		if ($sms_to[0] && $message) {
-			
+
 			list($ok, $to, $smslog_id, $queue, $counts, $sms_count, $sms_failed, $error_strings) = sendsms_helper($user_config['username'], $sms_to, $message, $sms_type, $unicode, '', $nofooter, $sms_footer, $sms_sender, $sms_schedule, $reference_id);
-			
+
 			if (!$sms_count && $sms_failed) {
 				$_SESSION['dialog']['danger'][] = _('Fail to send message to all destinations') . " (" . _('queued') . ":" . (int) $sms_count . " " . _('failed') . ":" . (int) $sms_failed . ")";
 			} else if ($sms_count && $sms_failed) {
@@ -180,12 +190,11 @@ switch (_OP_) {
 		} else {
 			$_SESSION['dialog']['danger'][] = _('You must select receiver and your message should not be empty');
 		}
-		
+
 		if ($return_url) {
 			header("Location: " . $return_url);
 		} else {
 			header("Location: " . _u('index.php?app=main&inc=core_sendsms&op=sendsms'));
 		}
 		exit();
-		break;
 }
