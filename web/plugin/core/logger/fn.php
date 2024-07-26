d344753d49bbd34f7734f8b6a5aa5d2ab047385d
Anton Raharja <araharja@pm.me>
2024-07-26 04:44:46 +0000

    update core/logger: use _REMOTE_ADDR_, code formatting

--- web/plugin/core/logger/fn.php
+++ web/plugin/core/logger/fn.php
@@ -19,12 +19,13 @@
 
 defined('_SECURE_') or die('Forbidden');
 
-function logger_print($log, $level='', $label='') {
+function logger_print($log, $level = '', $label = '')
+{
 	global $core_config, $user_config;
 
-	$remote = ( trim($_SERVER['REMOTE_ADDR']) ? trim($_SERVER['REMOTE_ADDR']) : '-' );
-	$host = ( trim($_SERVER['HTTP_HOST']) ? trim($_SERVER['HTTP_HOST']) : '-' );
-	$logfile = ( $core_config['logfile'] ? $core_config['logfile'] : 'playsms.log' );
+	$remote = _REMOTE_ADDR_ ? _REMOTE_ADDR_ : '-';
+	$host = trim($_SERVER['HTTP_HOST']) ? trim($_SERVER['HTTP_HOST']) : '-';
+	$logfile = $core_config['logfile'] ? $core_config['logfile'] : 'playsms.log';
 
 	// max log length is 1000
 	if (strlen($log) > 1000) {
@@ -32,21 +33,21 @@ function logger_print($log, $level='', $label='') {
 	}
 
 	// default level is 2
-	$level = ( (int)$level > 0 ? (int)$level : 2 );
+	$level = ((int) $level > 0 ? (int) $level : 2);
 
 	// label should not have spaces, replace single space with double _
 	$label = str_replace(' ', '__', $label);
-	$label = ( $label ? $label : '-' );
+	$label = $label ? $label : '-';
 
-	$username = ( $user_config['username'] ? $user_config['username'] : '-' );
+	$username = $user_config['username'] ? $user_config['username'] : '-';
 	if (logger_get_level() >= $level) {
-		$type = 'L'.$level;
-		$fn = $core_config['apps_path']['logs'].'/'.$logfile;
+		$type = 'L' . $level;
+		$fn = $core_config['apps_path']['logs'] . '/' . $logfile;
 		if ($fd = fopen($fn, 'a+')) {
 			$dt = date($core_config['datetime']['format'], time());
 
 			// REMOTE_ADDR HTTP_HOST DATE TIME PID USERNAME TYPE LABEL # LOG
-			$message = stripslashes($remote." ".$host." ".$dt." "._PID_." ".$username." ".$type." ".$label." # ".$log);
+			$message = stripslashes($remote . " " . $host . " " . $dt . " " . _PID_ . " " . $username . " " . $type . " " . $label . " # " . $log);
 			$message = str_replace("\n", " ", $message);
 			$message = str_replace("\r", " ", $message);
 			$message .= "\n";
@@ -57,43 +58,46 @@ function logger_print($log, $level='', $label='') {
 	}
 }
 
-function logger_get_level() {
+function logger_get_level()
+{
 	global $core_config;
 	return $core_config['logstate'];
 }
 
-function logger_set_level($level=0) {
+function logger_set_level($level = 0)
+{
 	global $core_config;
 	$core_config['logstate'] = $level;
 }
 
-function logger_audit() {
+function logger_audit()
+{
 	global $core_config, $user_config;
 
-	$host = ( trim($_SERVER['HTTP_HOST']) ? trim($_SERVER['HTTP_HOST']) : '-' );
+	$host = trim($_SERVER['HTTP_HOST']) ? trim($_SERVER['HTTP_HOST']) : '-';
 	if ($core_config['logaudit']) {
-		foreach ($_GET as $key => $val) {
-			if(stristr($key, 'password') === FALSE) {
-				$log .= $key.':'.$val.' ';
+		foreach ( $_GET as $key => $val ) {
+			if (stristr($key, 'password') === FALSE) {
+				$log .= $key . ':' . $val . ' ';
 			} else {
-				$log .= $key.':xxxxxx ';
+				$log .= $key . ':xxxxxx ';
 			}
 		}
-		foreach ($_POST as $key => $val) {
-			if(stristr($key, 'password') === FALSE) {
-				$log .= $key.':'.$val.' ';
+		foreach ( $_POST as $key => $val ) {
+			if (stristr($key, 'password') === FALSE) {
+				$log .= $key . ':' . $val . ' ';
 			} else {
-				$log .= $key.':xxxxxx ';
+				$log .= $key . ':xxxxxx ';
 			}
 		}
 		$log = trim($log);
-		$logauditfile = ( $core_config['logauditfile'] ? $core_config['logauditfile'] : 'audit.log' );
-		$username = ( $user_config['username'] ? $user_config['username'] : '-' );
-		$ip = $_SERVER['REMOTE_ADDR'];
-		$fn = $core_config['apps_path']['logs'].'/'.$logauditfile;
+		$logauditfile = $core_config['logauditfile'] ? $core_config['logauditfile'] : 'audit.log';
+		$username = $user_config['username'] ? $user_config['username'] : '-';
+		$ip = _REMOTE_ADDR_ ? _REMOTE_ADDR_ : '-';
+		$fn = $core_config['apps_path']['logs'] . '/' . $logauditfile;
 		if ($fd = fopen($fn, 'a+')) {
 			$dt = date($core_config['datetime']['format'], time());
-			$message = stripslashes($host." ".$dt." "._PID_." ".$username." ip:".$ip." ".$log);
+			$message = stripslashes($host . " " . $dt . " " . _PID_ . " " . $username . " ip:" . $ip . " " . $log);
 			$message = str_replace("\n", " ", $message);
 			$message = str_replace("\r", " ", $message);
 			$message .= "\n";
