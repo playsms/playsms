741d43282d31b767a5bc631ea0a66029f91c3fbe
Anton Raharja <araharja@pm.me>
2024-08-21 04:48:15 +0000

    update feature/queuelog: code formatting

--- web/plugin/feature/queuelog/fn.php
+++ web/plugin/feature/queuelog/fn.php
@@ -18,67 +18,72 @@
  */
 defined('_SECURE_') or die('Forbidden');
 
-function queuelog_get($line_per_page = '', $limit = '') {
+function queuelog_get($line_per_page = 0, $limit = 0)
+{
 	global $user_config;
-	$ret = array();
-	if ($user_config['status'] != 2) {
-		$user_query = "AND uid='" . $user_config['uid'] . "'";
-	}
-	if ($line_per_page) {
-		$line_per_page_query = "LIMIT $line_per_page";
-	}
-	if ($limit) {
-		$limit_query = "OFFSET $limit";
-	}
+
+	$ret = [];
+
+	$user_query = $user_config['status'] != 2 ? "AND uid='" . $user_config['uid'] . "'" : "";
+	$line_per_page_query = (int) $line_per_page > 0 ? "LIMIT " . (int) $line_per_page : "";
+	$limit_query = (int) $limit ? "OFFSET " . (int) $limit : "";
 	$db_query = "SELECT * FROM " . _DB_PREF_ . "_tblSMSOutgoing_queue WHERE (flag='0' OR flag='3') " . $user_query . " ORDER BY id " . $line_per_page_query . " " . $limit_query;
 	$db_result = dba_query($db_query);
 	while ($db_row = dba_fetch_array($db_result)) {
-		$c_id = $db_row['id'];
-		$db_row['count'] = dba_count(_DB_PREF_ . '_tblSMSOutgoing_queue_dst', array(
-			'flag' => 0,
-			'queue_id' => $c_id 
-		));
-		$ret[] = $db_row;
+		if ($c_id = $db_row['id']) {
+			$db_row['count'] = dba_count(_DB_PREF_ . '_tblSMSOutgoing_queue_dst', [
+				'flag' => 0,
+				'queue_id' => $c_id
+			]);
+			$ret[] = $db_row;
+		}
 	}
+
 	return $ret;
 }
 
-function queuelog_countall() {
+function queuelog_countall()
+{
 	global $user_config;
+
 	$ret = 0;
-	if ($user_config['status'] != 2) {
-		$user_query = "AND uid='" . $user_config['uid'] . "'";
-	}
+
+	$user_query = $user_config['status'] != 2 ? "AND uid='" . $user_config['uid'] . "'" : "";
 	$db_query = "SELECT count(*) AS count FROM " . _DB_PREF_ . "_tblSMSOutgoing_queue WHERE (flag='0' OR flag='3') " . $user_query;
 	$db_result = dba_query($db_query);
 	if ($db_row = dba_fetch_array($db_result)) {
 		$ret = $db_row['count'];
 	}
+
 	return $ret;
 }
 
-function queuelog_delete($queue) {
+function queuelog_delete($queue)
+{
 	global $user_config;
-	$ret = FALSE;
-	if ($user_config['status'] != 2) {
-		$user_query = "AND uid='" . $user_config['uid'] . "'";
+
+	$ret = false;
+
+	$user_query = $user_config['status'] != 2 ? "AND uid='" . $user_config['uid'] . "'" : "";
+	$db_query = "DELETE FROM " . _DB_PREF_ . "_tblSMSOutgoing_queue WHERE (flag='0' OR flag='3') AND queue_code=? " . $user_query;
+	if (dba_affected_rows($db_query, [$queue])) {
+		$ret = true;
 	}
-	$db_query = "DELETE FROM " . _DB_PREF_ . "_tblSMSOutgoing_queue WHERE (flag='0' OR flag='3') AND queue_code='" . $queue . "' " . $user_query;
-	if ($db_result = dba_affected_rows($db_query)) {
-		$ret = TRUE;
-	}
+
 	return $ret;
 }
 
-function queuelog_delete_all($queue) {
+function queuelog_delete_all($queue)
+{
 	global $user_config;
-	$ret = FALSE;
-	if ($user_config['status'] != 2) {
-		$user_query = "AND uid='" . $user_config['uid'] . "'";
-	}
+
+	$ret = false;
+
+	$user_query = $user_config['status'] != 2 ? "AND uid='" . $user_config['uid'] . "'" : "";
 	$db_query = "DELETE FROM " . _DB_PREF_ . "_tblSMSOutgoing_queue WHERE (flag='0' OR flag='3') " . $user_query;
-	if ($db_result = dba_affected_rows($db_query)) {
-		$ret = TRUE;
+	if (dba_affected_rows($db_query)) {
+		$ret = true;
 	}
+
 	return $ret;
 }
--- web/plugin/feature/queuelog/queuelog.php
+++ web/plugin/feature/queuelog/queuelog.php
@@ -25,16 +25,16 @@ if (!auth_isvalid()) {
 switch (_OP_) {
 	case "queuelog_list":
 		$nav = themes_nav($count, "index.php?app=main&inc=feature_queuelog&op=queuelog_list");
-		
+
 		$content = _dialog() . "
 			<h2>" . _('View SMS queue') . "</h2>";
-		
-		$count = queuelog_countall();
-		if ($count) {
+
+		if ($count = queuelog_countall()) {
 			$content .= "<p><a href=\"javascript: ConfirmURL('" . addslashes(_("Are you sure you want to delete ALL queues")) . " ?','" . _u('index.php?app=main&inc=feature_queuelog&op=queuelog_delete_all') . "')\">" . $icon_config['delete'] . _("Delete ALL queues") . " ($count)</a></p>";
 		}
-		
-		$content .= "<div align=center>" . $nav['form'] . "</div>
+
+		$content .= "
+			<div align=center>" . $nav['form'] . "</div>
 			<div class=table-responsive>
 			<table class=playsms-table-list>
 			<thead>
@@ -42,7 +42,7 @@ switch (_OP_) {
 		";
 		if (auth_isadmin()) {
 			$content .= "
-				<th width=20%>" . _('Queue Code') . "</th>
+				<th width=30%>" . _('Queue Code') . "</th>
 				<th width=15%>" . _('User') . "</th>
 			";
 		} else {
@@ -53,23 +53,25 @@ switch (_OP_) {
 		$content .= "
 				<th width=15%>" . _('Scheduled') . "</th>
 				<th width=10%>" . _('Count') . "</th>
-				<th width=30%>" . _('Message') . "</th>
-				<th width=10%>" . _('Action') . "</th>
+				<th width=25%>" . _('Message') . "</th>
+				<th width=5%>" . _('Action') . "</th>
 			</tr>
 			</thead>
 			<tbody>
 		";
 		$data = queuelog_get($nav['limit'], $nav['offset']);
+		$data = _display($data);
+		$c_count = is_array($data) ? count($data) : 0;
 		for ($c = count($data) - 1; $c >= 0; $c--) {
 			$c_queue_code = $data[$c]['queue_code'];
 			$c_datetime_scheduled = core_display_datetime($data[$c]['datetime_scheduled']);
 			$c_username = user_uid2username($data[$c]['uid']);
-			
+
 			// total number of SMS in queue
 			$c_count = $data[$c]['sms_count'];
-			
-			$c_message = stripslashes(_t($data[$c]['message']));
-			$c_action = "<a href=\"javascript: ConfirmURL('" . addslashes(_("Are you sure you want to delete queue")) . " " . $c_queue_code . " ?','" . _u('index.php?app=main&inc=feature_queuelog&op=queuelog_delete&queue=' . $c_queue_code) . "')\">" . $icon_config['delete'] . "</a>";
+
+			$c_message = $data[$c]['message'];
+			$c_action = "<a href=\"javascript: ConfirmURL('" . _("Are you sure you want to delete queue") . " " . $c_queue_code . " ?','" . _u('index.php?app=main&inc=feature_queuelog&op=queuelog_delete&queue=' . $c_queue_code) . "')\">" . $icon_config['delete'] . "</a>";
 			$content .= "
 				<tr>
 			";
@@ -98,6 +100,7 @@ switch (_OP_) {
 		";
 		_p($content);
 		break;
+
 	case "queuelog_delete":
 		if ($queue = $_REQUEST['queue']) {
 			if (queuelog_delete($queue)) {
@@ -106,12 +109,11 @@ switch (_OP_) {
 		}
 		header("Location: " . _u('index.php?app=main&inc=feature_queuelog&op=queuelog_list'));
 		exit();
-		break;
+
 	case "queuelog_delete_all":
 		if (queuelog_delete_all($queue)) {
 			$_SESSION['dialog']['info'][] = _('All queues have been removed');
 		}
 		header("Location: " . _u('index.php?app=main&inc=feature_queuelog&op=queuelog_list'));
 		exit();
-		break;
 }
