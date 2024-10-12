c6909c51c09caff8ef31bcd43cd8e7f6b6e2cdca
Anton Raharja <araharja@pm.me>
2024-10-12 00:14:33 +0000

    update callback in Example gateway

--- web/plugin/gateway/_example/callback.php
+++ web/plugin/gateway/_example/callback.php
@@ -44,37 +44,53 @@ if (is_array($requests)) {
 // Example API server only pushed these variables for DLR:
 //   id - SMS Log ID sent previously by us
 //   status - Delivery Report for the SMS Log ID sent previously by us
+//
+// read the correct variable names and their format from your API server documentation
 
 $smslog_id = isset($requests['id']) ? (int) $requests['id'] : 0;
 $status = isset($requests['status']) ? core_sanitize_alphanumeric($requests['status']) : '';
 
 // Handle DLR
 if ($smslog_id && $status) {
-
 	$db_query = "SELECT uid,smslog_id FROM " . _DB_PREF_ . "_tblSMSOutgoing WHERE smslog_id=? AND p_status=1 AND flag_deleted=0";
 	$db_result = dba_query($db_query, [$smslog_id]);
 	if ($db_row = dba_fetch_array($db_result)) {
 		$uid = (int) $db_row['uid'];
 		$smslog_id = (int) $db_row['smslog_id'];
 
-		$p_status = 0;
-		switch ($status) {
-			case 'DELIVRD':
-			case 'ESME_ROK':
-				$p_status = 3;
-				break; // delivered
-			default:
-				$p_status = 2;
-				break; // failed
+		// proceed when the previously sent SMS to Example provider/gateway found
+		if ($uid && $smslog_id) {
+			// p_status :
+			// 	 0 = pending
+			//   1 = sent
+			//   2 = failed
+			//   3 = delivered
+			//
+			// read the correct status values and their format from your API server documentation
+
+			$p_status = 0;
+			switch ($status) {
+				case 'DELIVRD':
+				case 'ESME_ROK':
+					$p_status = 3;
+					break; // delivered
+				default:
+					$p_status = 2;
+					break; // failed
+			}
+
+			// log it
+			_log("dlr uid:" . $uid . " smslog_id:" . $smslog_id . " status:" . $p_status, 2, "example callback");
+
+			// set delivery report
+			dlr($smslog_id, $uid, $p_status);
+
+			// respond to the API server
+			// read the correct way to respond back the API server in API server documentation
+			ob_end_clean();
+			echo "ACK " . $smslog_id;
+			exit();
 		}
-
-		_log("dlr uid:" . $uid . " smslog_id:" . $smslog_id . " status:" . $p_status, 2, "example callback");
-
-		dlr($smslog_id, $uid, $p_status);
-
-		ob_end_clean();
-		echo "ACK " . $smslog_id;
-		exit();
 	}
 }
 
@@ -83,6 +99,8 @@ if ($smslog_id && $status) {
 //   from - SMS sender number
 //   to - SMS receiver number
 //   message - Content or message of incoming SMS
+//
+// Read the correct variable names and their format from your API server documentation
 
 $ts = isset($requests['ts']) ? (int) $requests['ts'] : 0;
 $datetime = date($ts, $datetime_format);
@@ -92,10 +110,14 @@ $message = isset($requests['message']) ? $requests['message'] : '';
 
 // Handle incoming SMS (MO)
 if ($sender && $message) {
+	// log it
 	_log("incoming dt:" . $datetime . " from:" . $sender . " to:" . $receiver . " message:[" . $message . "]", 2, "example callback");
 
+	// save incoming SMS for further processing
 	$recvlog_id = recvsms($datetime, $sender, $message, $receiver, $smsc);
 
+	// respond to the API server
+	// read the correct way to respond back the API server in API server documentation
 	ob_end_clean();
 	echo "ACK " . $recvlog_id;
 	exit();
